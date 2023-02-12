75a26044834be9082a5df1ddc450e6d68d3ac9d4
Steven Ickman <stevenic@microsoft.com>
2023-02-12 11:37:35 -0800

    Added search command sample

---
+++ js/samples/messageExtensions/searchCommand/src/index.ts
@@ -0,0 +1,114 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+// Import required packages
+import { config } from 'dotenv';
+import * as path from 'path';
+import * as restify from 'restify';
+
+// Import required bot services.
+// See https://aka.ms/bot-services to learn more about the different parts of a bot.
+import {
+    CloudAdapter,
+    ConfigurationBotFrameworkAuthentication,
+    ConfigurationBotFrameworkAuthenticationOptions,
+    MemoryStorage,
+    ActivityTypes,
+    MessagingExtensionAttachment
+} from 'botbuilder';
+
+// Read botFilePath and botFileSecret from .env file.
+const ENV_FILE = path.join(__dirname, '..', '.env');
+config({ path: ENV_FILE });
+
+const botFrameworkAuthentication = new ConfigurationBotFrameworkAuthentication(process.env as ConfigurationBotFrameworkAuthenticationOptions);
+
+// Create adapter.
+// See https://aka.ms/about-bot-adapter to learn more about how bots work.
+const adapter = new CloudAdapter(botFrameworkAuthentication);
+
+// Create storage to use
+//const storage = new MemoryStorage();
+
+// Catch-all for errors.
+const onTurnErrorHandler = async ( context, error ) => {
+    // This check writes out errors to console log .vs. app insights.
+    // NOTE: In production environment, you should consider logging this to Azure
+    //       application insights.
+    console.error( `\n [onTurnError] unhandled error: ${ error }` );
+
+    // Send a trace activity, which will be displayed in Bot Framework Emulator
+    await context.sendTraceActivity(
+        'OnTurnError Trace',
+        `${ error }`,
+        'https://www.botframework.com/schemas/error',
+        'TurnError'
+    );
+
+    // Send a message to the user
+    await context.sendActivity( 'The bot encountered an error or bug.' );
+    await context.sendActivity( 'To continue to run this bot, please fix the bot source code.' );
+};
+
+// Set the onTurnError for the singleton CloudAdapter.
+adapter.onTurnError = onTurnErrorHandler;
+
+// Create HTTP server.
+const server = restify.createServer();
+server.use(restify.plugins.bodyParser());
+
+server.listen(process.env.port || process.env.PORT || 3978, () => {
+    console.log( `\n${ server.name } listening to ${ server.url }` );
+    console.log( '\nGet Bot Framework Emulator: https://aka.ms/botframework-emulator' );
+    console.log( '\nTo talk to your bot, open the emulator select "Open Bot"' );
+});
+
+import { AdaptiveCardSearchResult, Application } from 'botbuilder-m365';
+import { createNpmSearchResultCard, createNpmPackageCard } from './cards';
+import axios from 'axios';
+
+// Define storage and application
+const storage = new MemoryStorage();
+const app = new Application({
+    storage
+});
+
+// Listen for search actions
+app.messageExtensions.query('searchCmd', async (context, state, query) => {
+    const searchQuery = query.parameters['searchKeyword'] ?? '';
+    const count = query.count ?? 10;
+    const response = await axios.get(`http://registry.npmjs.com/-/v1/search?${new URLSearchParams({ text: searchQuery, size: count.toString() }).toString()}`);
+
+    // Format search results
+    const results: MessagingExtensionAttachment[] = [];
+    response?.data?.objects?.forEach(result => results.push(createNpmSearchResultCard(result)));
+
+    // Return results as a list
+    return {
+        type: 'result',
+        attachmentLayout: 'list',
+        attachments: results
+    };
+});
+
+// Listen for item tap
+app.messageExtensions.selectItem(async (context, state, item) => {
+    // Generate detailed result
+    const card = createNpmPackageCard(item);
+
+    // Return results
+    return {
+        type: 'result',
+        attachmentLayout: 'list',
+        attachments: [card]
+    };
+});
+
+// Listen for incoming server requests.
+server.post('/api/messages', async (req, res) => {
+    // Route received a request to adapter for processing
+    await adapter.process(req, res as any, async (context) => {
+        // Dispatch to application for routing
+        await app.run(context);
+    });
+});
