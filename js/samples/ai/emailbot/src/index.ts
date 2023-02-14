f695d3b34e99f49462ba9112a42c41e6adce5888
Paola Del Hierro Haro <p.del.hierro@hotmail.com>
2023-02-14 15:05:41 -0800

    email bot

---
+++ js/samples/ai/emailbot/src/index.ts
@@ -0,0 +1,163 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+// Import required packages
+import { config } from 'dotenv';
+import * as path from 'path';
+import * as restify from 'restify';
+
+// Import required bot services.
+// See https://aka.ms/bot-services to learn more about the different parts of a bot.
+import {
+    CloudAdapter,
+    ConfigurationBotFrameworkAuthentication,
+    ConfigurationBotFrameworkAuthenticationOptions,
+    MemoryStorage
+} from 'botbuilder';
+
+// Read botFilePath and botFileSecret from .env file.
+const ENV_FILE = path.join(__dirname, '..', '.env');
+config({ path: ENV_FILE });
+
+const botFrameworkAuthentication = new ConfigurationBotFrameworkAuthentication(
+    process.env as ConfigurationBotFrameworkAuthenticationOptions
+);
+
+// Create adapter.
+// See https://aka.ms/about-bot-adapter to learn more about how bots work.
+const adapter = new CloudAdapter(botFrameworkAuthentication);
+
+// Create storage to use
+//const storage = new MemoryStorage();
+
+// Catch-all for errors.
+const onTurnErrorHandler = async (context, error) => {
+    // This check writes out errors to console log .vs. app insights.
+    // NOTE: In production environment, you should consider logging this to Azure
+    //       application insights.
+    console.error(`\n [onTurnError] unhandled error: ${error}`);
+
+    // Send a trace activity, which will be displayed in Bot Framework Emulator
+    await context.sendTraceActivity(
+        'OnTurnError Trace',
+        `${error}`,
+        'https://www.botframework.com/schemas/error',
+        'TurnError'
+    );
+
+    // Send a message to the user
+    await context.sendActivity('The bot encountered an error or bug.');
+    await context.sendActivity('To continue to run this bot, please fix the bot source code.');
+};
+
+// Set the onTurnError for the singleton CloudAdapter.
+adapter.onTurnError = onTurnErrorHandler;
+
+// Create HTTP server.
+const server = restify.createServer();
+server.use(restify.plugins.bodyParser());
+
+server.listen(process.env.port || process.env.PORT || 3978, () => {
+    console.log(`\n${server.name} listening to ${server.url}`);
+    console.log('\nGet Bot Framework Emulator: https://aka.ms/botframework-emulator');
+    console.log('\nTo talk to your bot, open the emulator select "Open Bot"');
+});
+
+import { Application, DefaultTurnState, OpenAIPredictionEngine, AI, ConversationHistory } from 'botbuilder-m365';
+import * as responses from './responses';
+
+interface ConversationState {
+    lightsOn: boolean;
+}
+type ApplicationTurnState = DefaultTurnState<ConversationState>;
+
+// Create prediction engine
+const predictionEngine = new OpenAIPredictionEngine({
+    configuration: {
+        apiKey: process.env.OPENAI_API_KEY
+    },
+    prompt: path.join(__dirname, '../src/prompt.txt'),
+    promptConfig: {
+        model: 'text-davinci-003',
+        temperature: 0.0,
+        max_tokens: 2048,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0.6,
+        stop: [' Human:', ' AI:']
+    },
+    topicFilter: path.join(__dirname, '../src/topicFilter.txt'),
+    topicFilterConfig: {
+        model: 'text-davinci-003',
+        temperature: 0.0,
+        max_tokens: 128,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0.0,
+        stop: [' Human:', ' AI:']
+    },
+    logRequests: true
+});
+
+// Define storage and application
+const storage = new MemoryStorage();
+const app = new Application<ApplicationTurnState>({
+    storage,
+    predictionEngine
+});
+
+// Register action handlers
+app.ai.action('LightsOn', async (context, state) => {
+    state.conversation.value.lightsOn = true;
+    await context.sendActivity(`[lights on]`);
+    return true;
+});
+
+app.ai.action('LightsOff', async (context, state) => {
+    state.conversation.value.lightsOn = false;
+    await context.sendActivity(`[lights off]`);
+    return true;
+});
+
+app.ai.action('Pause', async (context, state, data) => {
+    const time = data.time ? parseInt(data.time) : 1000;
+    await context.sendActivity(`[pausing for ${time / 1000} seconds]`);
+    await new Promise((resolve) => setTimeout(resolve, time));
+    return true;
+});
+
+app.ai.action('LightStatus', async (context, state) => {
+    // Send the user a static response with the status of the lights.
+    const response = responses.lightStatus(state.conversation.value.lightsOn);
+    await context.sendActivity(response);
+
+    // Since we might be prompting the user with a followup question, we need to do
+    // some surgery on the {{conversation.history}} to append a THEN SAY command. This
+    // lets the model know we just asked the user a question and it can predict the
+    // next action based on their response.
+    ConversationHistory.appendToLastLine(state, ` THEN SAY ${response}`);
+
+    // End the current chain since we've manually just prompted the user for input.
+    return false;
+});
+
+// Register a handler to handle unknown actions that might be predicted
+app.ai.action(AI.UnknownActionName, async (context, state, data, action) => {
+    await context.sendActivity(responses.unknownAction(action));
+    return false;
+});
+
+// Register a handler to deal with a user asking something off topic
+app.ai.action(AI.OffTopicActionName, async (context, state) => {
+    await context.sendActivity(responses.offTopic());
+    return false;
+});
+
+// Listen for incoming server requests.
+server.post('/api/messages', async (req, res) => {
+    // Route received a request to adapter for processing
+    await adapter.process(req, res as any, async (context) => {
+        // Dispatch to application for routing
+        await app.run(context);
+    });
+});
