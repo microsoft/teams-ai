175bb2e1aaf17c9151a99c547cca6da96e979f6f
Corina Gum <>
2023-02-14 13:16:50 -0800

    Update echobot

---
+++ js/samples/01.messaging/01.a.echoBot/src/index.ts
@@ -0,0 +1,102 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+// Import required packages
+import { config } from 'dotenv';
+import * as path from 'path';
+import * as restify from 'restify';
+
+// Import required bot services.
+// See https://aka.ms/bot-services to learn more about the different parts of a bot.
+import {
+    CloudAdapter,
+    ConfigurationBotFrameworkAuthentication,
+    ConfigurationBotFrameworkAuthenticationOptions,
+    MemoryStorage,
+    ActivityTypes
+} from 'botbuilder';
+
+// Read botFilePath and botFileSecret from .env file.
+const ENV_FILE = path.join(__dirname, '..', '.env');
+config({ path: ENV_FILE });
+
+const botFrameworkAuthentication = new ConfigurationBotFrameworkAuthentication(process.env as ConfigurationBotFrameworkAuthenticationOptions);
+
+// Create adapter.
+// See https://aka.ms/about-bot-adapter to learn more about how bots work.
+const adapter = new CloudAdapter(botFrameworkAuthentication);
+
+// Create storage to use
+//const storage = new MemoryStorage();
+
+// Catch-all for errors.
+const onTurnErrorHandler = async ( context, error ) => {
+    // This check writes out errors to console log .vs. app insights.
+    // NOTE: In production environment, you should consider logging this to Azure
+    //       application insights.
+    console.error( `\n [onTurnError] unhandled error: ${ error }` );
+
+    // Send a trace activity, which will be displayed in Bot Framework Emulator
+    await context.sendTraceActivity(
+        'OnTurnError Trace',
+        `${ error }`,
+        'https://www.botframework.com/schemas/error',
+        'TurnError'
+    );
+
+    // Send a message to the user
+    await context.sendActivity( 'The bot encountered an error or bug.' );
+    await context.sendActivity( 'To continue to run this bot, please fix the bot source code.' );
+};
+
+// Set the onTurnError for the singleton CloudAdapter.
+adapter.onTurnError = onTurnErrorHandler;
+
+// Create HTTP server.
+const server = restify.createServer();
+server.use(restify.plugins.bodyParser());
+
+server.listen(process.env.port || process.env.PORT || 3978, () => {
+    console.log( `\n${ server.name } listening to ${ server.url }` );
+    console.log( '\nGet Bot Framework Emulator: https://aka.ms/botframework-emulator' );
+    console.log( '\nTo talk to your bot, open the emulator select "Open Bot"' );
+});
+
+import { Application, DefaultTurnState, OpenAIPredictionEngine, AI } from 'botbuilder-m365';
+
+interface ConversationState {
+    count: number;
+}
+type ApplicationTurnState = DefaultTurnState<ConversationState>;
+
+
+// Define storage and application
+const storage = new MemoryStorage();
+const app = new Application<ApplicationTurnState>({
+    storage
+});
+
+// Listen for user to say '/reset' and then delete conversation state
+app.message('/reset', async (context, state) => {
+    state.conversation.delete();
+    await context.sendActivity(`Ok I've deleted the current conversation state.`);
+});
+
+// Listen for ANY message to be received. MUST BE AFTER ANY OTHER MESSAGE HANDLERS
+app.activity(ActivityTypes.Message, async (context, state) => {
+    // Increment count state
+    let count = state.conversation.value.count ?? 0;
+    state.conversation.value.count = ++count;
+
+    // Echo back users request
+    await context.sendActivity(`[${count}] you said: ${context.activity.text}`);
+});
+
+// Listen for incoming server requests.
+server.post('/api/messages', async (req, res) => {
+    // Route received a request to adapter for processing
+    await adapter.process(req, res as any, async (context) => {
+        // Dispatch to application for routing
+        await app.run(context);
+    });
+});
