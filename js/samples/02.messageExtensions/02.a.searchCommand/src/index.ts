8ee2bbd8bf1db8ce913259b4bf7bc5663b095acc
Corina Gum <>
2023-02-14 13:18:24 -0800

    Move messageExtensions sample

--- js/samples/02.messageExtensions/02.a.searchCommand/src/index.ts
+++ js/samples/02.messageExtensions/02.a.searchCommand/src/index.ts
@@ -20,7 +20,9 @@ import {
 const ENV_FILE = path.join(__dirname, '..', '.env');
 config({ path: ENV_FILE });
 
-const botFrameworkAuthentication = new ConfigurationBotFrameworkAuthentication(process.env as ConfigurationBotFrameworkAuthenticationOptions);
+const botFrameworkAuthentication = new ConfigurationBotFrameworkAuthentication(
+    process.env as ConfigurationBotFrameworkAuthenticationOptions
+);
 
 // Create adapter.
 // See https://aka.ms/about-bot-adapter to learn more about how bots work.
@@ -30,23 +32,23 @@ const adapter = new CloudAdapter(botFrameworkAuthentication);
 //const storage = new MemoryStorage();
 
 // Catch-all for errors.
-const onTurnErrorHandler = async ( context, error ) => {
+const onTurnErrorHandler = async (context, error) => {
     // This check writes out errors to console log .vs. app insights.
     // NOTE: In production environment, you should consider logging this to Azure
     //       application insights.
-    console.error( `\n [onTurnError] unhandled error: ${ error }` );
+    console.error(`\n [onTurnError] unhandled error: ${error}`);
 
     // Send a trace activity, which will be displayed in Bot Framework Emulator
     await context.sendTraceActivity(
         'OnTurnError Trace',
-        `${ error }`,
+        `${error}`,
         'https://www.botframework.com/schemas/error',
         'TurnError'
     );
 
     // Send a message to the user
-    await context.sendActivity( 'The bot encountered an error or bug.' );
-    await context.sendActivity( 'To continue to run this bot, please fix the bot source code.' );
+    await context.sendActivity('The bot encountered an error or bug.');
+    await context.sendActivity('To continue to run this bot, please fix the bot source code.');
 };
 
 // Set the onTurnError for the singleton CloudAdapter.
@@ -57,9 +59,9 @@ const server = restify.createServer();
 server.use(restify.plugins.bodyParser());
 
 server.listen(process.env.port || process.env.PORT || 3978, () => {
-    console.log( `\n${ server.name } listening to ${ server.url }` );
-    console.log( '\nGet Bot Framework Emulator: https://aka.ms/botframework-emulator' );
-    console.log( '\nTo talk to your bot, open the emulator select "Open Bot"' );
+    console.log(`\n${server.name} listening to ${server.url}`);
+    console.log('\nGet Bot Framework Emulator: https://aka.ms/botframework-emulator');
+    console.log('\nTo talk to your bot, open the emulator select "Open Bot"');
 });
 
 import { Application } from 'botbuilder-m365';
@@ -76,11 +78,16 @@ const app = new Application({
 app.messageExtensions.query('searchCmd', async (context, state, query) => {
     const searchQuery = query.parameters['queryText'] ?? '';
     const count = query.count ?? 10;
-    const response = await axios.get(`http://registry.npmjs.com/-/v1/search?${new URLSearchParams({ text: searchQuery, size: count.toString() }).toString()}`);
+    const response = await axios.get(
+        `http://registry.npmjs.com/-/v1/search?${new URLSearchParams({
+            text: searchQuery,
+            size: count.toString()
+        }).toString()}`
+    );
 
     // Format search results
     const results: MessagingExtensionAttachment[] = [];
-    response?.data?.objects?.forEach(obj => results.push(createNpmSearchResultCard(obj.package)));
+    response?.data?.objects?.forEach((obj) => results.push(createNpmSearchResultCard(obj.package)));
 
     // Return results as a list
     return {
