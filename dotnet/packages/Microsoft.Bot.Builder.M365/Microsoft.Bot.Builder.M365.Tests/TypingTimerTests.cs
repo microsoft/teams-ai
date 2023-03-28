129c3acfe8582c6c75557a50ef3771f296f3c1be
Kavin Singh <kavinsingh@microsoft.com>
2023-03-28 08:18:20 -0700

    basic project set up, typing timer implementation

---
+++ dotnet/packages/Microsoft.Bot.Builder.M365/Microsoft.Bot.Builder.M365.Tests/TypingTimerTests.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Bot.Schema;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Bot.Builder.M365.Tests
+{
+    public class TypingTimerTests
+    {
+
+        [Fact]
+        public void StartTypingTimer_MessageActivityType()
+        {
+            // Arrange
+            var botAdapterStub = Mock.Of<BotAdapter>();
+            var turnContextMock = new Mock<TurnContext>(botAdapterStub, new Activity { Type = ActivityTypes.Message });
+
+            int timerDelay = 1000;
+            TypingTimer typingTimer = new TypingTimer(timerDelay);
+
+            // Act
+            typingTimer.StartTypingTimer(turnContextMock.Object);
+
+            // Assert
+            Assert.NotNull(typingTimer.timer);
+        }
+
+        [Fact]
+        public void StartTypingTimer_NotMessageActivityType()
+        {
+            // Arrange
+            var botAdapterStub = Mock.Of<BotAdapter>();
+            var notMessageActivityType = ActivityTypes.Invoke;
+            var turnContextMock = new Mock<TurnContext>(botAdapterStub, new Activity { Type = notMessageActivityType });
+
+            int timerDelay = 1000;
+            TypingTimer typingTimer = new TypingTimer(timerDelay);
+
+            // Act
+            typingTimer.StartTypingTimer(turnContextMock.Object);
+
+            // Assert
+            Assert.Null(typingTimer.timer);
+        }
+
+        [Fact]
+        public void StopTypingTimer_ShouldResetProperties()
+        {
+            // Arrange
+            var botAdapterStub = Mock.Of<BotAdapter>();
+            var turnContextMock = new Mock<TurnContext>(botAdapterStub, new Activity { Type = ActivityTypes.Message });
+
+            int timerDelay = 1000;
+            TypingTimer typingTimer = new TypingTimer(timerDelay);
+
+            // Act
+            typingTimer.StartTypingTimer(turnContextMock.Object);
+            typingTimer.StopTypingTimer();
+
+            // Assert
+            Assert.Null(typingTimer.timer);
+        }
+    }
+}
---
+++ dotnet/packages/Microsoft.Bot.Builder.M365/Microsoft.Bot.Builder.M365/TypingTimer.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.Bot.Schema;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Microsoft.Bot.Builder.M365
+{
+    /// <summary>
+    /// Encapsulates the logic for sending "typing" activity to the user.
+    /// </summary>
+    public class TypingTimer
+    {
+        public Timer? timer;
+        private int _delay;
+
+        public TypingTimer(int typingTimerDelay)
+        {
+            this._delay = typingTimerDelay;
+        }
+
+        /// <summary>
+        /// Manually start a timer to periodically send "typing" activity.
+        /// </summary>
+        /// <remarks>
+        /// The timer will automatically end once an outgoing activity has been sent. If the timer is already running or 
+        /// the current activity is not a "message" the call is ignored.
+        /// </remarks>
+        /// <param name="turnContext">The context for the current turn with the user.</param>
+        public void StartTypingTimer(ITurnContext turnContext)
+        {
+            if (turnContext.Activity.Type != ActivityTypes.Message || this.timer != null) return;
+
+            // Listen for outgoing activities
+            turnContext.OnSendActivities(TypingTimerSendActivitiesHandler);
+            
+            // Start periodically send "typing" activity
+            this.timer = new Timer(SendTypingActivity, turnContext, 0, _delay);
+        }
+
+        public void StopTypingTimer()
+        {
+            if (this.timer == null) return;
+            
+            this.timer.Dispose();
+            this.timer = null;
+        }
+
+        private async void SendTypingActivity(object state)
+        {
+            ITurnContext? turnContext = state as TurnContext;
+                
+            if (turnContext == null)
+            {
+                throw new Exception("Unexpected failure of casting object TurnContext");
+            }
+
+            try
+            {
+                await turnContext.SendActivityAsync(new Activity { Type = ActivityTypes.Typing });
+            } 
+            catch (Exception)
+            {
+                // Seeing a random proxy violation error from the context object. This is because
+                // we're in the middle of sending an activity on a background thread when the turn ends.
+                // The context object throws when we try to update "this.Responded = true". We can just
+                // eat the error but lets make sure our states cleaned up a bit.
+                this.timer = null;
+            }
+        }
+
+
+        private Task<ResourceResponse[]> TypingTimerSendActivitiesHandler(ITurnContext turnContext, List<Activity> activities, Func<Task<ResourceResponse[]>> next)
+        {
+            if (this.timer != null)
+            {
+                foreach (var activity in activities)
+                {
+                    if (activity.Type == ActivityTypes.Message)
+                    {
+                        this.StopTypingTimer();
+                        break;
+                    }
+                }
+            }
+
+            return next();
+        }
+
+    }
+}
