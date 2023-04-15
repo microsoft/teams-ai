1ba10e55a5328ae9e9d77409b9476ca212fc6840
Kavin Singh <kavinsingh@microsoft.com>
2023-04-15 13:11:39 -0700

    response parser, parse JSON method + unit tests

---
+++ dotnet/packages/Microsoft.Bot.Builder.M365/Microsoft.Bot.Builder.M365.Tests/ResponseParserTests.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.M365;
+using Newtonsoft.Json;
+
+namespace Microsoft.Bot.Builder.M365.Tests
+{
+    public class ResponseParserTests
+    {
+        [Theory]
+        [MemberData(nameof(ParseJsonTestData))]
+        public void Test_ParseJson_Simple(string text, List<JObject> expectedJSONs)
+        {
+            var actualObjects = ResponseParser.ParseJSON(text);
+
+            Assert.Equal(expectedJSONs, actualObjects);
+        }
+
+        public static IEnumerable<object[]> ParseJsonTestData()
+        {
+            yield return new object[] { "{}", new List<JObject> { new JObject() } };
+            yield return new object[] { "{}{}", new List<JObject> { new JObject(), new JObject() } };
+
+            yield return new object[] { "{'Name': 'Jose'}", new List<JObject> {
+                    new()
+                    {
+                        { "Name", "Jose" }
+                    }
+                }
+            };
+
+            yield return new object[] { "{'Name': 'Jose'} {'Name': 'Carlos'}", new List<JObject> {
+                    new()
+                    {
+                        { "Name", "Jose" }
+                    },
+                    new()
+                    {
+                        { "Name", "Carlos" }
+                    }
+                },
+            };
+
+            string planObjectJSON = "{ 'type':'plan','commands':[{ 'type':'DO','action':'<name>','entities':{ '<name>': '<value>' } },{ 'type':'SAY','response':'<response>'}]}";
+            JObject planObject = JsonConvert.DeserializeObject<JObject>(planObjectJSON);
+            yield return new object[] { planObjectJSON, new List<JObject> { planObject } };
+
+            string twoPlanObjectsJSON = "prefix string " + planObjectJSON + "other string" + planObjectJSON + "suffix string";
+            yield return new object[] { twoPlanObjectsJSON, new List<JObject> { planObject, planObject } };
+
+        }
+    }
+}
+
+
---
+++ dotnet/packages/Microsoft.Bot.Builder.M365/Microsoft.Bot.Builder.M365/ResponseParser.cs
@@ -0,0 +1,65 @@
+﻿using AdaptiveCards;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.Bot.Builder.M365
+{
+    public class ResponseParser
+    {
+        /// <summary>
+        /// Extract all the valid JSON strings within the input text and returns a list of objects.
+        /// </summary>
+        /// <param name="text">String that contains valid JSON object substrings</param>
+        /// <returns>An ordered list of JSON objects</returns>
+        public static List<JObject>? ParseJSON(string text)
+        {
+            int length = text.Length;
+
+            if (length < 2) return null;
+
+            var result = new List<JObject>();
+
+            int startIndex;
+            int endIndex = -1;
+            while (endIndex < length)
+            { 
+                // Find the first "{"
+                startIndex = text.IndexOf('{', endIndex + 1);
+                if (startIndex == -1) return result;
+
+                // Find the first "}" such that all the contents sandwiched between S & E is a valid JSON.
+                endIndex = startIndex;
+                while (endIndex < length)
+                {
+                    endIndex = text.IndexOf('}', endIndex + 1);
+                    if (endIndex == -1) return result;
+
+                    string possibleJSON = text.Substring(startIndex, endIndex - startIndex + 1);
+                    
+                    // Validate string to be a valid JSON
+                    try
+                    {
+                        JToken.Parse(possibleJSON);
+                    } catch (JsonReaderException)
+                    {
+                        continue;
+                    }
+                    
+                    // Will deserialize string even if it's not a valid JSON
+                    JObject? jsonObject = JsonConvert.DeserializeObject<JObject>(possibleJSON);
+
+                    if (jsonObject != null)
+                    {
+                        result.Add(jsonObject);
+                        break;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
