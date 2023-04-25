bfe0fa97577e4fda773f3bc38cd39a52e2bb7604
Kavin Singh <kavinsingh@microsoft.com>
2023-04-25 14:05:17 -0700

    changed "const" to "static readonly"

--- dotnet/packages/Microsoft.Bot.Builder.M365/Microsoft.Bot.Builder.M365/AI/AITypes.cs
+++ dotnet/packages/Microsoft.Bot.Builder.M365/Microsoft.Bot.Builder.M365/AI/AITypes.cs
@@ -6,8 +6,8 @@ namespace Microsoft.Bot.Builder.M365.AI
 {
     public class AITypes
     {
-        public const string Plan = "Plan";
-        public const string DoCommand = "DO";
-        public const string SayCommand = "SAY";
+        public static readonly string Plan = "Plan";
+        public static readonly string DoCommand = "DO";
+        public static readonly string SayCommand = "SAY";
     }
 }
