8dc1c8152b266c2552639eb6b0b58062d5fd93e1
Qianhao Dong <qidon@microsoft.com>
2023-07-10 14:14:56 +0800

    init attributes

---
+++ dotnet/packages/Microsoft.Bot.Builder.M365/Microsoft.Bot.Builder.M365/AI/Action/ActionParameterAttribute.cs
@@ -0,0 +1,48 @@
+﻿namespace Microsoft.Bot.Builder.M365.AI.Action
+{
+    public class ActionParameterAttribute : Attribute
+    {
+        /// <summary>
+        /// The attribute type that represents what the action parameter is.
+        /// </summary>
+        internal ActionParameterType ActionParameterType { get; set; }
+
+        /// <summary>
+        /// The real data type.
+        /// </summary>
+        internal Type? Type { get; set; }
+
+        internal ActionParameterAttribute(ActionParameterType actionParameterType, Type? type)
+        {
+            ActionParameterType = actionParameterType;
+            Type = type;
+        }
+    }
+
+    /// <summary>
+    /// Represent what the action parameter is.
+    /// </summary>
+    internal enum ActionParameterType
+    {
+        /// <summary>
+        /// The action parameter is TurnContext
+        /// </summary>
+        TurnContext,
+        /// <summary>
+        /// The action parameter is TurnState
+        /// </summary>
+        TurnState,
+        /// <summary>
+        /// The action parameter is data object
+        /// </summary>
+        Data,
+        /// <summary>
+        /// The action parameter is action name
+        /// </summary>
+        Name,
+        /// <summary>
+        /// The action parameter is not attributed, use default value
+        /// </summary>
+        Unknown,
+    }
+}
---
+++ dotnet/packages/Microsoft.Bot.Builder.M365/Microsoft.Bot.Builder.M365/AI/Action/ActionTurnStateAttribute.cs
@@ -0,0 +1,22 @@
+﻿namespace Microsoft.Bot.Builder.M365.AI.Action
+{
+    /// <summary>
+    /// Attribute to represent the <see cref="TurnState"/> parameter of an action method.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false)]
+    public sealed class ActionTurnStateAttribute : ActionParameterAttribute
+    {
+        /// <summary>
+        /// Create new <see cref="ActionTurnStateAttribute"/>.
+        /// </summary>
+        /// <param name="type">The actual type of <see cref="TurnState"/> parameter in action method.</param>
+        /// <exception cref="Exception">Throws if the input type is not a <see cref="TurnState"/>.</exception>
+        public ActionTurnStateAttribute(Type type) : base(ActionParameterType.TurnState, type)
+        {
+            if (!typeof(TurnState).IsAssignableFrom(type))
+            {
+                throw new Exception("ActionTurnStateAttribute input type should be a TurnState");
+            }
+        }
+    }
+}
