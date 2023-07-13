87c7f8b91aa3e5de961541fd2dd6197da10183a1
Kuojian Lu <kuojianlu@gmail.com>
2023-07-13 14:15:19 +0800

    fix build warning

--- dotnet/samples/TeamsChefBot/Config.cs
+++ dotnet/samples/TeamsChefBot/Config.cs
@@ -2,8 +2,8 @@
 {
     public class ConfigOptions
     {
-        public string BOT_ID { get; set; }
-        public string BOT_PASSWORD { get; set; }
-        public string OPENAI_API_KEY { get; set; }
+        public string? BOT_ID { get; set; }
+        public string? BOT_PASSWORD { get; set; }
+        public string? OPENAI_API_KEY { get; set; }
     }
 }
