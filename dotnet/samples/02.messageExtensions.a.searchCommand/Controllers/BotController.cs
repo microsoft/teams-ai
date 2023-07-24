3ea5cade2f1db20cff8468848d73a3df05ddee60
Qianhao Dong <qidon@microsoft.com>
2023-07-24 10:02:11 +0800

    [C# sample] 02.messageExtensions.a.searchCommand (#296)
    Part of https://github.com/microsoft/teams-ai/issues/292, to port the
    basic search command message extension sample.
    A bit different from the js sample - in this .net one, search for NuGet
    package instead of NPM package.
    Verified on both local and azure.

---
+++ dotnet/samples/02.messageExtensions.a.searchCommand/Controllers/BotController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Integration.AspNet.Core;
+
+namespace SearchCommand.Controllers
+{
+    [Route("api/messages")]
+    [ApiController]
+    public class BotController : ControllerBase
+    {
+        private readonly CloudAdapter _adapter;
+        private readonly IBot _bot;
+
+        public BotController(CloudAdapter adapter, IBot bot)
+        {
+            _adapter = adapter;
+            _bot = bot;
+        }
+
+        [HttpPost]
+        public async Task PostAsync(CancellationToken cancellationToken = default)
+        {
+            await _adapter.ProcessAsync
+            (
+                Request,
+                Response,
+                _bot,
+                cancellationToken
+            );
+        }
+    }
+}
