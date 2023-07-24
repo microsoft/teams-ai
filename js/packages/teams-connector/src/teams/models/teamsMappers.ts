80580800e57879f6119d879f47f5713083a74fa4
Steven Ickman <stevenic@microsoft.com>
2023-07-24 12:32:36 -0700

    Added teams-connector package

---
+++ js/packages/teams-connector/src/teams/models/teamsMappers.ts
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+export { ErrorResponse, ErrorModel, InnerHttpError } from '../../connectorApi/models/mappers';
+
+export {
+    ConversationList,
+    ChannelInfo,
+    MessageActionsPayloadConversation,
+    Meeting,
+    TeamDetails,
+    TeamsChannelAccount,
+    TeamsMeetingDetails,
+    TeamsMeetingInfo,
+    TeamsMeetingParticipant,
+    TeamsMeetingNotification,
+    TeamsMeetingNotificationInfo,
+    TeamsMeetingNotificationSurface,
+    TeamsMeetingNotificationChannelData,
+    TeamsMeetingOnBehalfOf,
+    TeamsMeetingNotificationRecipientFailureInfo,
+    TeamsMeetingNotificationRecipientFailureInfos,
+    TaskModuleContinueResponse,
+    TaskModuleTaskInfo,
+    Attachment,
+} from './mappers';
