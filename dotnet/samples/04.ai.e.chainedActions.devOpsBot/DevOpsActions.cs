c11aeed732953a21684b456471f14c28f6007fb4
Qianhao Dong <qidon@microsoft.com>
2023-07-25 13:44:50 +0800

    add devops bot sample

---
+++ dotnet/samples/04.ai.e.chainedActions.devOpsBot/DevOpsActions.cs
@@ -0,0 +1,172 @@
+﻿using Microsoft.Bot.Builder;
+using Microsoft.TeamsAI;
+using Microsoft.TeamsAI.AI.Action;
+
+using DevOpsBot.Model;
+
+namespace DevOpsBot
+{
+    public class DevOpsActions
+    {
+        private readonly Application<DevOpsState, DevOpsStateManager> _application;
+
+        public DevOpsActions(Application<DevOpsState, DevOpsStateManager> application)
+        {
+            _application = application;
+        }
+
+        [Action("CreateWI")]
+        public async Task<bool> CreateWI([ActionTurnContext] ITurnContext turnContext, [ActionTurnState] DevOpsState turnState, [ActionEntities] EntityData data)
+        {
+            _ = turnContext ?? throw new ArgumentNullException(nameof(turnContext));
+            _ = turnState ?? throw new ArgumentNullException(nameof(turnState));
+            _ = data ?? throw new ArgumentNullException(nameof(data));
+
+            int id = CreateNewWorkItem(turnState, data);
+            await turnContext.SendActivityAsync($"New work item created with ID: {id} and assigned to: {data.AssignedTo}").ConfigureAwait(false);
+            return false;
+        }
+
+        [Action("AssignWI")]
+        public bool AssignWI([ActionTurnState] DevOpsState turnState, [ActionEntities] EntityData data)
+        {
+            _ = turnState ?? throw new ArgumentNullException(nameof(turnState));
+            _ = data ?? throw new ArgumentNullException(nameof(data));
+
+            AssignWorkItem(turnState, data);
+            return true;
+        }
+
+        [Action("UpdateWI")]
+        public bool UpdateWI([ActionTurnState] DevOpsState turnState, [ActionEntities] EntityData data)
+        {
+            _ = turnState ?? throw new ArgumentNullException(nameof(turnState));
+            _ = data ?? throw new ArgumentNullException(nameof(data));
+
+            UpdateWorkItem(turnState, data);
+            return true;
+        }
+
+        [Action("TriageWI")]
+        public bool TriageWI([ActionTurnState] DevOpsState turnState, [ActionEntities] EntityData data)
+        {
+            _ = turnState ?? throw new ArgumentNullException(nameof(turnState));
+            _ = data ?? throw new ArgumentNullException(nameof(data));
+
+            TriageWorkItem(turnState, data);
+            return true;
+        }
+
+        [Action("Summarize")]
+        public async Task<bool> Summarize([ActionTurnContext] ITurnContext turnContext, [ActionTurnState] DevOpsState turnState)
+        {
+            _ = turnContext ?? throw new ArgumentNullException(nameof(turnContext));
+            _ = turnState ?? throw new ArgumentNullException(nameof(turnState));
+
+            EntityData[] workItems = turnState.Conversation!.WorkItems;
+            if (workItems.Length > 0)
+            {
+                // Chain into a new summarization prompt
+                await _application.AI.ChainAsync(turnContext, turnState, "Summarize").ConfigureAwait(false);
+            }
+            else
+            {
+                await turnContext.SendActivityAsync(ResponseBuilder.NoListFound()).ConfigureAwait(false);
+            }
+
+            // End the current chain
+            return false;
+        }
+
+        [Action(DefaultActionTypes.UnknownActionName)]
+        public async Task<bool> UnknownAction([ActionTurnContext] ITurnContext turnContext, [ActionName] string action)
+        {
+            _ = turnContext ?? throw new ArgumentNullException(nameof(turnContext));
+
+            await turnContext.SendActivityAsync(ResponseBuilder.UnknownAction(action)).ConfigureAwait(false);
+            return false;
+        }
+
+        /// <summary>
+        /// This method is used to create new work item.
+        /// </summary>
+        /// <param name="turnState">The application turn state.</param>
+        /// <param name="workItemInfo">Data containing the work item information.</param>
+        /// <returns>The ID of the newly created work item.</returns>
+        private static int CreateNewWorkItem(DevOpsState turnState, EntityData workItemInfo)
+        {
+            if (workItemInfo.Id == 0)
+            {
+                workItemInfo.Id = turnState.Conversation!.WorkItems.Length + 1;
+            }
+            workItemInfo.Status = "Proposed";
+            turnState.Conversation!.WorkItems = turnState.Conversation!.WorkItems.Concat(new[] { workItemInfo }).ToArray();
+            return workItemInfo.Id;
+        }
+
+        /// <summary>
+        /// This method is used to assign a work item to a person.
+        /// </summary>
+        /// <param name="turnState">The application turn state.</param>
+        /// <param name="workItemInfo">Data containing the work item information.</param>
+        private static void AssignWorkItem(DevOpsState turnState, EntityData workItemInfo)
+        {
+            EntityData[] workItems = turnState.Conversation!.WorkItems;
+            if (workItemInfo.Id != 0)
+            {
+                EntityData? target = workItems.FirstOrDefault(item => item.Id == workItemInfo.Id);
+                if (target != null)
+                {
+                    target.AssignedTo = workItemInfo.AssignedTo;
+
+                    // Set back to state to be persisted
+                    turnState.Conversation!.WorkItems = workItems;
+                }
+            }
+        }
+
+        /// <summary>
+        /// This method is used to triage work item.
+        /// </summary>
+        /// <param name="turnState">The application turn state.</param>
+        /// <param name="workItemInfo">Data containing the work item information.</param>
+        private static void TriageWorkItem(DevOpsState turnState, EntityData workItemInfo)
+        {
+            EntityData[] workItems = turnState.Conversation!.WorkItems;
+            if (workItemInfo.Id != 0)
+            {
+                EntityData? target = workItems.FirstOrDefault(item => item.Id == workItemInfo.Id);
+                if (target != null)
+                {
+                    target.Status = workItemInfo.Status;
+
+                    // Set back to state to be persisted
+                    turnState.Conversation!.WorkItems = workItems;
+                }
+            }
+        }
+
+        /// <summary>
+        /// This method is used to update the existing work item.
+        /// </summary>
+        /// <param name="turnState">The application turn state.</param>
+        /// <param name="workItemInfo">Data containing the work item information.</param>
+        private static void UpdateWorkItem(DevOpsState turnState, EntityData workItemInfo)
+        {
+            EntityData[] workItems = turnState.Conversation!.WorkItems;
+            if (workItemInfo.Id != 0)
+            {
+                EntityData? target = workItems.FirstOrDefault(item => item.Id == workItemInfo.Id);
+                if (target != null)
+                {
+                    target.Title = workItemInfo.Title ?? target.Title;
+                    target.AssignedTo = workItemInfo.AssignedTo ?? target.AssignedTo;
+                    target.Status = workItemInfo.Status ?? target.Status;
+
+                    // Set back to state to be persisted
+                    turnState.Conversation!.WorkItems = workItems;
+                }
+            }
+        }
+    }
+}
---
+++ dotnet/samples/04.ai.e.chainedActions.devOpsBot/Program.cs
@@ -0,0 +1,144 @@
+﻿using DevOpsBot;
+using DevOpsBot.Model;
+
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using Microsoft.Bot.Connector.Authentication;
+using Microsoft.TeamsAI;
+using Microsoft.TeamsAI.AI;
+using Microsoft.TeamsAI.AI.Moderator;
+using Microsoft.TeamsAI.AI.Planner;
+using Microsoft.TeamsAI.AI.Prompt;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddHttpClient("WebClient", client => client.Timeout = TimeSpan.FromSeconds(600));
+builder.Services.AddHttpContextAccessor();
+
+// Prepare Configuration for ConfigurationBotFrameworkAuthentication
+ConfigOptions config = builder.Configuration.Get<ConfigOptions>()!;
+builder.Configuration["MicrosoftAppType"] = "MultiTenant";
+builder.Configuration["MicrosoftAppId"] = config.BOT_ID;
+builder.Configuration["MicrosoftAppPassword"] = config.BOT_PASSWORD;
+
+// Create the Bot Framework Authentication to be used with the Bot Adapter.
+builder.Services.AddSingleton<BotFrameworkAuthentication, ConfigurationBotFrameworkAuthentication>();
+
+// Create the Cloud Adapter with error handling enabled.
+// Note: some classes expect a BotAdapter and some expect a BotFrameworkHttpAdapter, so
+// register the same adapter instance for all types.
+builder.Services.AddSingleton<CloudAdapter, AdapterWithErrorHandler>();
+builder.Services.AddSingleton<IBotFrameworkHttpAdapter>(sp => sp.GetService<CloudAdapter>()!);
+builder.Services.AddSingleton<BotAdapter>(sp => sp.GetService<CloudAdapter>()!);
+
+// Create singleton instances for bot application
+builder.Services.AddSingleton<IStorage, MemoryStorage>();
+
+#region Use OpenAI
+// Use OpenAI
+if (config.OpenAI == null || string.IsNullOrEmpty(config.OpenAI.ApiKey))
+{
+    throw new ArgumentException("Missing OpenAI configuration.");
+}
+builder.Services.AddSingleton<OpenAIPlannerOptions>(_ => new(config.OpenAI.ApiKey, "gpt-3.5-turbo"));
+builder.Services.AddSingleton<OpenAIModeratorOptions>(_ => new(config.OpenAI.ApiKey, ModerationType.Both));
+
+// Create the bot as a transient. In this case the ASP Controller is expecting an IBot.
+builder.Services.AddTransient<IBot>(sp =>
+{
+    // Create loggers
+    ILoggerFactory loggerFactory = sp.GetService<ILoggerFactory>()!;
+
+    // Get HttpClient
+    HttpClient moderatorHttpClient = sp.GetService<IHttpClientFactory>()!.CreateClient("WebClient");
+
+    // Create OpenAIPlanner
+    IPlanner<DevOpsState> planner = new OpenAIPlanner<DevOpsState>(
+        sp.GetService<OpenAIPlannerOptions>()!,
+        loggerFactory.CreateLogger<OpenAIPlanner<DevOpsState>>());
+
+    // Create OpenAIModerator
+    IModerator<DevOpsState> moderator = new OpenAIModerator<DevOpsState>(
+        sp.GetService<OpenAIModeratorOptions>()!,
+        loggerFactory.CreateLogger<OpenAIModerator<DevOpsState>>(),
+        moderatorHttpClient);
+
+    // Create Application
+    AIOptions<DevOpsState> aiOptions = new(
+        planner: planner,
+        promptManager: new PromptManager<DevOpsState>("./Prompts"),
+        prompt: "ChatGPT",
+        moderator: moderator);
+    ApplicationOptions<DevOpsState, DevOpsStateManager> ApplicationOptions = new()
+    {
+        TurnStateManager = new DevOpsStateManager(),
+        Storage = sp.GetService<IStorage>(),
+        AI = aiOptions
+    };
+    return new TeamsDevOpsBot(ApplicationOptions);
+});
+#endregion
+
+#region Use Azure OpenAI and Azure Content Safety
+/** // Following code is for using Azure OpenAI and Azure Content Safety
+if (config.Azure == null
+    || string.IsNullOrEmpty(config.Azure.OpenAIApiKey)
+    || string.IsNullOrEmpty(config.Azure.OpenAIEndpoint)
+    || string.IsNullOrEmpty(config.Azure.ContentSafetyApiKey)
+    || string.IsNullOrEmpty(config.Azure.ContentSafetyEndpoint))
+{
+    throw new ArgumentException("Missing Azure configuration.");
+}
+builder.Services.AddSingleton<AzureOpenAIPlannerOptions>(_ => new(config.Azure.OpenAIApiKey, "gpt-3.5-turbo", config.Azure.OpenAIEndpoint));
+builder.Services.AddSingleton<AzureContentSafetyModeratorOptions>(_ => new(config.Azure.ContentSafetyApiKey, config.Azure.ContentSafetyEndpoint, ModerationType.Both));
+
+// Create the bot as a transient. In this case the ASP Controller is expecting an IBot.
+builder.Services.AddTransient<IBot>(sp =>
+{
+    // Create loggers
+    ILoggerFactory loggerFactory = sp.GetService<ILoggerFactory>()!;
+
+    // Get HttpClient
+    HttpClient moderatorHttpClient = sp.GetService<IHttpClientFactory>()!.CreateClient("WebClient");
+
+    // Create AzureOpenAIPlanner
+    IPlanner<DevOpsState> planner = new AzureOpenAIPlanner<DevOpsState>(
+        sp.GetService<AzureOpenAIPlannerOptions>()!,
+        loggerFactory.CreateLogger<AzureOpenAIPlanner<DevOpsState>>());
+
+    // Create AzureContentSafetyModerator
+    IModerator<DevOpsState> moderator = new AzureContentSafetyModerator<DevOpsState>(
+        sp.GetService<AzureContentSafetyModeratorOptions>()!,
+        loggerFactory.CreateLogger<AzureContentSafetyModerator<DevOpsState>>(),
+        moderatorHttpClient);
+
+    // Create Application
+    AIOptions<DevOpsState> aiOptions = new(
+        planner: planner,
+        promptManager: new PromptManager<DevOpsState>("./Prompts"),
+        prompt: "ChatGPT",
+        moderator: moderator);
+    ApplicationOptions<DevOpsState, DevOpsStateManager> ApplicationOptions = new()
+    {
+        TurnStateManager = new DevOpsStateManager(),
+        Storage = sp.GetService<IStorage>(),
+        AI = aiOptions,
+    };
+    return new TeamsDevOpsBot(ApplicationOptions);
+});
+**/
+#endregion
+
+WebApplication app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseStaticFiles();
+app.UseRouting();
+app.MapControllers();
+
+app.Run();
