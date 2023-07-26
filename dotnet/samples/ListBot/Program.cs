6000dd37843c650c6b4035803cfbb18b5c2ffd4e
Kuojian Lu <kuojianlu@gmail.com>
2023-07-26 15:25:07 +0800

    [C# sample] add list bot sample

---
+++ dotnet/samples/ListBot/Program.cs
@@ -0,0 +1,125 @@
+﻿using ListBot;
+using ListBot.Model;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using Microsoft.Bot.Connector.Authentication;
+using Microsoft.TeamsAI;
+using Microsoft.TeamsAI.AI;
+using Microsoft.TeamsAI.AI.Planner;
+using Microsoft.TeamsAI.AI.Prompt;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddHttpClient("WebClient", client => client.Timeout = TimeSpan.FromSeconds(600));
+builder.Services.AddHttpContextAccessor();
+
+// Prepare Configuration for ConfigurationBotFrameworkAuthentication
+var config = builder.Configuration.Get<ConfigOptions>()!;
+builder.Configuration["MicrosoftAppType"] = "MultiTenant";
+builder.Configuration["MicrosoftAppId"] = config.BOT_ID;
+builder.Configuration["MicrosoftAppPassword"] = config.BOT_PASSWORD;
+
+// Create the Bot Framework Authentication to be used with the Bot Adapter.
+builder.Services.AddSingleton<BotFrameworkAuthentication, ConfigurationBotFrameworkAuthentication>();
+
+// Create the Cloud Adapter with error handling enabled.
+// Note: some classes expect a BotAdapter and some expect a BotFrameworkHttpAdapter, so
+// register the same adapter instance for all types.
+builder.Services.AddSingleton<CloudAdapter, AdapterWithErrorHandler>();
+builder.Services.AddSingleton<IBotFrameworkHttpAdapter>(sp => sp.GetService<CloudAdapter>()!);
+builder.Services.AddSingleton<BotAdapter>(sp => sp.GetService<CloudAdapter>()!);
+
+builder.Services.AddSingleton<IStorage, MemoryStorage>();
+
+#region Use OpenAI
+if (config.OpenAI == null || string.IsNullOrEmpty(config.OpenAI.ApiKey))
+{
+    throw new Exception("Missing OpenAI configuration.");
+}
+
+builder.Services.AddSingleton(_ => new OpenAIPlannerOptions(config.OpenAI.ApiKey, "gpt-3.5-turbo")
+{
+    LogRequests = true
+});
+
+// Create the Application.
+builder.Services.AddTransient<IBot, ListBotApplication>(sp =>
+{
+    ILoggerFactory loggerFactory = sp.GetService<ILoggerFactory>()!;
+
+    PromptManager<ListState> promptManager = new("./Prompts");
+
+    OpenAIPlanner<ListState> planner = new(sp.GetService<OpenAIPlannerOptions>()!, loggerFactory.CreateLogger<OpenAIPlanner<ListState>>());
+
+    ApplicationOptions<ListState, ListStateManager> applicationOptions = new()
+    {
+        AI = new AIOptions<ListState>(planner, promptManager)
+        {
+            Prompt = "ChatGPT",
+            History = new AIHistoryOptions()
+            {
+                AssistantHistoryType = AssistantHistoryType.Text
+            }
+        },
+        Storage = sp.GetService<IStorage>()
+    };
+
+    return new ListBotApplication(applicationOptions);
+});
+#endregion
+
+#region Use Azure OpenAI
+/**
+if (config.Azure == null
+    || string.IsNullOrEmpty(config.Azure.OpenAIApiKey)
+    || string.IsNullOrEmpty(config.Azure.OpenAIEndpoint))
+{
+    throw new Exception("Missing Azure configuration.");
+}
+
+builder.Services.AddSingleton(_ => new AzureOpenAIPlannerOptions(config.Azure.OpenAIApiKey, "gpt-35-turbo", config.Azure.OpenAIEndpoint)
+{
+    LogRequests = true
+});
+
+// Create the Application.
+builder.Services.AddTransient<IBot, ListBotApplication>(sp =>
+{
+    ILoggerFactory loggerFactory = sp.GetService<ILoggerFactory>()!;
+
+    PromptManager<ListState> promptManager = new("./Prompts");
+
+    AzureOpenAIPlanner<ListState> planner = new(sp.GetService<AzureOpenAIPlannerOptions>()!, loggerFactory.CreateLogger<AzureOpenAIPlanner<ListState>>());
+
+    ApplicationOptions<ListState, ListStateManager> applicationOptions = new()
+    {
+        AI = new AIOptions<ListState>(planner, promptManager)
+        {
+            Prompt = "ChatGPT",
+            History = new AIHistoryOptions()
+            {
+                TrackHistory = false,
+            }
+        },
+        Storage = sp.GetService<IStorage>(),
+        StartTypingTimer = false
+    };
+
+    return new ListBotApplication(applicationOptions);
+});
+**/
+#endregion
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseStaticFiles();
+app.UseRouting();
+app.MapControllers();
+
+app.Run();
