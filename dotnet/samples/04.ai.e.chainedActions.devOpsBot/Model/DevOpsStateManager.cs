36e1ab75036e1b5d6c5d9c35db25fc5bc2c56f5c
Qianhao Dong <qidon@microsoft.com>
2023-07-28 09:18:09 +0800

    [C# sample] dev ops bot sample (#374)
    For https://github.com/microsoft/teams-ai/issues/367
    Add C# DevOpsBot sample.

---
+++ dotnet/samples/04.ai.e.chainedActions.devOpsBot/Model/DevOpsStateManager.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.TeamsAI.State;
+
+namespace DevOpsBot.Model
+{
+    /// <summary>
+    /// Shorter the class name since turn state is strongly typed.
+    /// </summary>
+    public class DevOpsStateManager : TurnStateManager<DevOpsState, DevOpsConversationState, StateBase, TempState>
+    {
+    }
+}
