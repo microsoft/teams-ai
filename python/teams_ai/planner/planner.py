bb02dd2801e6138f60c203567afdc1ba183c0337
Chaoyi Yuan <blackchoey@gmail.com>
2023-08-08 23:59:36 +0800

    feat: add interface for planner and moderator (#398)
    The new interface for planner and moderator after moves prompt
    management to planner. This is follow up of #202.
    
    The `AI` class will call the planner and moderator in following ways:
    ``` python
    async def chain(...):
        prompt_name = options.prompt # assumes user provides a prompt name to simplify the code
        user_input = turn_context.activity.text
        plan = await options.moderator.reviewPrompt(user_input)
        if plan is None:
            plan = await options.planner.generate_plan(turn_state, prompt_name)
            plan = await options.moderator.reviewPlan(plan)
        
        # process generated plan
    ```
    
    The differences comparing with existing interface in js SDK:
    1. For `Planner.geneate_plan`
    1. ~~Does not accept a `TurnContext` parameter any more, because this
    wasn't used in the implementation. I'm curious about the user scenario
    that requires `TurnContext`. Please add comments about the user
    scenarios and I'm happy to add it back.~~ Adds this back based on the
    potential scenario shared by Qianhao: it can be used to obtain Teams
    specific information when writing custom planner.
    2. The `prompt` parameter can now be either a prompt name, or a prompt
    template.
    3. The `options` parameter type is changed to `AIHistoryOptions` instead
    of a big `ConfiguredAIOptions`, because the current implementation only
    uses them. This is helpful for people to understand what options will
    take effect in the planner.
    2. Removews `Planner.complete_prompt`, per #206 
    3. For `Moderator.review_prompt`
        1. The `prompt` parameter is now just `user_input`, per #203 
    2. Does not accept `TurnContext`, `TurnState` and `ConfiguredAIOptions`
    parameter any more, because the moderator now can only reviews user
    input and seems no user scenarios that requires these parameter. Please
    add comments about the user scenarios and I'm happy to add them back.
    4. For `Moderator.review_plan`
    1. Does not accept `TurnContext` and `TurnState` parameter any more. I'm
    curious about the user scenario that requires `TurnContext`. Please add
    comments about the user scenarios and I'm happy to add them back.
    5. I also do not use generics for `TurnState` parameter in the
    interface, to reduce the need of type declaration when developers write
    code. Because planner and moderator is used by our code internally,
    developers actually do not care what's the type of `TurnState`. They
    just need to tell Application class the type of `TurnState` because they
    need to register handlers using the class and strong type makes the dev
    experience better. The updated coding experience to initialize Teams-AI
    classes would be:
        ``` python
    planner = OpenAIPlanner(...) # instead of: planner =
    OpenAIPlanner[ApplicationState](...)
    moderator = OpenAIModerator(...) # instead of: planner =
    OpenAIModerator[ApplicationState](...)
        app = Application[ApplicationState](...)
        ```
    
    Related work items: #395 #386

---
+++ python/teams_ai/planner/planner.py
@@ -0,0 +1,40 @@
+"""
+Copyright (c) Microsoft Corporation. All rights reserved.
+Licensed under the MIT License.
+"""
+
+from abc import ABC, abstractclassmethod
+from typing import Any, Callable, Union
+from teams_ai.turn_state import TurnState
+
+class Planner(ABC):
+    """
+    interface implemented by all planners
+    """
+
+    @abstractclassmethod
+    async def generate_plan(self, turn_context:TurnContext, state: TurnState, prompt_name_or_template: Union[str, PromptTemplate], *, history_options: AIHistoryOptions) -> Plan:
+        """
+        Generates a plan based on the given turn state and prompt name or template.
+
+        Args:
+            turn_context (TurnContext): The turn context for current turn of conversation
+            state (TurnState): The current turn state.
+            prompt_name_or_template (Union[str, PromptTemplate]): The name of the prompt or a prompt template to use.
+            history_options (AIHistoryOptions): The options for the AI history.
+
+        Returns:
+            Plan: The generated plan.
+        """
+    
+
+    @abstractclassmethod
+    async def add_function(self, name: str, handler: Callable[[TurnContext, TurnState], Any], *, allow_overrides=False) -> None:
+        """
+        Adds a custom function to be used when rendering prompt templates.
+
+        Args:
+            name (str): The name of the function.
+            handler (Callable[[TurnContext, TurnState], Any]): The business logic for the function.
+            allow_overrides (bool, optional): Whether to allow overriding an existing function. Defaults to False.
+        """
