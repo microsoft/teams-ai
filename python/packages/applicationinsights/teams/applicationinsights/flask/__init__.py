d15702999172b5c1d613639bb377a19e276d4de2
Alex Acebo <aacebowork@gmail.com>
2023-08-08 13:11:05 -0700

    for now remove `applicationinsights ` which is causing issues

--- python/packages/applicationinsights/teams/applicationinsights/flask/__init__.py
+++
@@ -1,7 +0,0 @@
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License.
-"""Flask Application Insights package."""
-
-from .flask_telemetry_middleware import BotTelemetryMiddleware
-
-__all__ = ["BotTelemetryMiddleware"]
