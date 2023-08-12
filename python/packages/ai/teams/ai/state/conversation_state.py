5abd8a6d8a314eda7be1f29c2affe33b44cc8ab0
Alex Acebo <aacebowork@gmail.com>
2023-08-12 12:35:01 -0700

    remove unnecessary dataclass

--- python/packages/ai/teams/ai/state/conversation_state.py
+++ python/packages/ai/teams/ai/state/conversation_state.py
@@ -3,11 +3,9 @@ Copyright (c) Microsoft Corporation. All rights reserved.
 Licensed under the MIT License.
 """
 
-from dataclasses import dataclass
 from typing import TypedDict
 
 
-@dataclass
 class ConversationState(TypedDict):
     """
     inherit a new interface from this base interface to strongly type
