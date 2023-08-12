0a549767bb96694800b5b7316a1ccaf45414c8a3
Alex Acebo <aacebowork@gmail.com>
2023-08-12 12:13:43 -0700

    remove sample tests

--- python/samples/echo/scripts/test.py
+++ python/samples/echo/scripts/test.py
@@ -3,8 +3,6 @@ Copyright (c) Microsoft Corporation. All rights reserved.
 Licensed under the MIT License.
 """
 
-import subprocess
-
 
 def test():
-    subprocess.run(["poetry", "run", "pytest"], check=True)
+    return True
