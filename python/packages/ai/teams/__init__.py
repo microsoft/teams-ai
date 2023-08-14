4c4c846b42127ede4cbe3cad8923ccaff227be71
Alex Acebo <aacebowork@gmail.com>
2023-08-14 11:56:43 -0700

    run fmt

--- python/packages/ai/teams/__init__.py
+++ python/packages/ai/teams/__init__.py
@@ -3,8 +3,8 @@ Copyright (c) Microsoft Corporation. All rights reserved.
 Licensed under the MIT License.
 """
 
+from .activity_type import ActivityType
 from .ai import *
 from .app import Application
 from .app_error import ApplicationError
 from .app_options import ApplicationOptions
-from .activity_type import ActivityType
--- python/packages/ai/teams/ai/__init__.py
+++ python/packages/ai/teams/ai/__init__.py
@@ -3,10 +3,10 @@ Copyright (c) Microsoft Corporation. All rights reserved.
 Licensed under the MIT License.
 """
 
+from .action import *
 from .ai import AI
 from .ai_error import AIError
 from .ai_options import AIOptions
 from .history_options import AIHistoryOptions
 from .prompts import *
 from .state import *
-from .action import *
