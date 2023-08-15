20aff3c5a607ca267657d15789319cb3c0f9536d
Alex Acebo <aacebowork@gmail.com>
2023-08-15 09:39:52 -0700

    add explicit type annotations

--- python/packages/ai/teams/ai/ai_history_options.py
+++ python/packages/ai/teams/ai/ai_history_options.py
@@ -14,25 +14,25 @@ class AssistantHistoryType(str, Enum):
 
 @dataclass
 class AIHistoryOptions:
-    track_history = True
+    track_history: bool = True
     "Whether the AI system should track conversation history. `Default: True`"
 
-    max_turns = 3
+    max_turns: int = 3
     "The maximum number of turns to remember. `Default: 3`"
 
-    max_tokens = 1000
+    max_tokens: int = 1000
     "The maximum number of tokens worth of history to add to the prompt. `Default: 1000`"
 
-    line_separator = "\n"
+    line_separator: str = "\n"
     "The line separator to use when concatenating history. `Default: '\n'`"
 
-    user_prefix = "User:"
+    user_prefix: str = "User:"
     'The prefix to use for user history. `Default: "User:"`'
 
-    assistant_prefix = "Assistant:"
+    assistant_prefix: str = "Assistant:"
     'The prefix to use for assistant history. `Default: "Assistant:"`'
 
-    assistant_history_type = AssistantHistoryType.PLAN_OBJECT
+    assistant_history_type: AssistantHistoryType = AssistantHistoryType.PLAN_OBJECT
     """
     Whether the conversation history should include the plan object returned by the model or
     just the text of any SAY commands.
