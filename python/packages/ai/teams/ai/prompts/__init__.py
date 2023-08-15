5c0a8ed798240ed31f8052e3851b1e1865655d6b
Alex Acebo <aacebowork@gmail.com>
2023-08-15 09:46:17 -0700

    explicit export types at file level

--- python/packages/ai/teams/ai/prompts/__init__.py
+++ python/packages/ai/teams/ai/prompts/__init__.py
@@ -3,7 +3,8 @@ Copyright (c) Microsoft Corporation. All rights reserved.
 Licensed under the MIT License.
 """
 
-from .completion_config import *
-from .prompt_manager import *
-from .prompt_manager_error import *
-from .prompt_template_config import *
+from .completion_config import CompletionConfig
+from .prompt_manager import PromptManager
+from .prompt_manager_error import PromptManagerError
+from .prompt_template import PromptTemplate
+from .prompt_template_config import PromptTemplateConfig
