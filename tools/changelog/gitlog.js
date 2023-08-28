02055d56ea208bb453bfaee203edf4014f26b3d1
Corina Gum <>
2023-08-28 14:30:38 -0700

    Update gitlog script to write three separate changelogs (per language) and add invoke script

--- tools/changelog/gitlog.js
+++ tools/changelog/gitlog.js
@@ -22,21 +22,17 @@ if (!existsSync(join(__dirname, gitLogFromRepo))) {
 
 // Determine the changelogFile
 let changelogFile;
-if (gitLogFromRepo.startsWith("dotnet")) {
-  changelogFile = optionalOutputName || "dotnet.md";
-} else if (gitLogFromRepo.startsWith("js")) {
-  changelogFile = optionalOutputName || "js.md";
-} else if (gitLogFromRepo.startsWith("python")) {
-  changelogFile = optionalOutputName || "python.md";
-}
+  changelogFile = optionalOutputName || "CHANGELOG.md";
 
-// Run the following per file
-// git log --show-pulls --pretty=tformat:"%s" --since=2023-08-13 > dotnet.txt
-// git log --show-pulls --pretty=tformat:"%s" --since=2023-08-07 > js.txt
-// git log --show-pulls --pretty=tformat:"%s" --since=2023-08-10 > python.txt
-// then run node gitlog.js dotnet.txt
+
+// NOTE: update the --since date to the date of the last release.
+// Run the 'createChangelog.sh' script, or run:
+// git log --show-pulls --pretty=tformat:"%s" --since=2023-08-13 > changelog.txt | node gitlog.js changelog.txt
 
 const preparedStream = createWriteStream(changelogFile);
+const js = [];
+const dotnet = [];
+const python = [];
 (async function processLineByLine() {
   try {
     const rl = createInterface({
@@ -52,19 +48,41 @@ const preparedStream = createWriteStream(changelogFile);
       const matches = /(?<message>.*)\s\(#(?<num>\d*)\)$/gim.exec(line);
       if (matches) {
         const { message, num } = matches.groups;
-        preparedStream.write(`- ${message} [[PR ${num}]](https://github.com/microsoft/teams-ai/pull/${num})\n`);
+        const line = `- ${message} [[PR ${num}]](https://github.com/microsoft/teams-ai/pull/${num})\n`;
+         if (message.startsWith("[JS]")) {
+          js.push(line)
+         } else if (message.startsWith("[C#]")) {
+          dotnet.push(line)
+         }
+         else if (message.startsWith("[PY]")) {
+          python.push(line);
+         } else {
+          // The commit message does not start with a tag, or it starts with [repo], which can be applied to all changelogs.
+          js.push(line);
+          dotnet.push(line);
+          python.push(line);
+         }
       } else {
         // This second check is for PRs that were not squashed and merged.
         // E.g. "Merge pull request #1411 from <fork/branch>"
         const otherFormatMatches = /^(?<message>Merge pull request #(?<num>\d*) from.*)/gim.exec(line);
         if (otherFormatMatches) {
           const { message, num } = otherFormatMatches.groups;
-          preparedStream.write(`- ${message} [[PR ${num}]](https://github.com/microsoft/teams-ai/pull/${num})\n`);
+          const otherLine = (`- ${message} [[PR ${num}]](https://github.com/microsoft/teams-ai/pull/${num})\n`);
+          js.push(otherLine);
+          dotnet.push(otherLine);
+          python.push(otherLine);
         }
       }
     });
 
     await once(rl, "close");
+    preparedStream.write("# JavaScript SDK\n");
+    js.forEach(line => preparedStream.write(line));
+    preparedStream.write("\n# .NET SDK\n");
+    dotnet.forEach(line => preparedStream.write(line));
+    preparedStream.write("\n# Python SDK\n");
+    python.forEach(line => preparedStream.write(line));
     console.log(`Changelog "${changelogFile}" created.`);
   } catch (err) {
     const errMsgPrefix = `An error occurred during the creation of the changelog.
