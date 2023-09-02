4a1e927f4cf0f9062f501cf5246e0cab9074bec9
Alex Acebo <aacebowork@gmail.com>
2023-09-02 12:15:44 -0700

    update turn state

--- python/packages/ai/teams/ai/state/turn_state.py
+++ python/packages/ai/teams/ai/state/turn_state.py
@@ -23,7 +23,7 @@ class TurnState(ABC):
     user: UserState
     temp: TempState = field(default_factory=TempState)
 
-    async def save(self, storage: Optional[Storage]) -> None:
+    async def save(self, storage: Optional[Storage] = None) -> None:
         await self.conversation.save(storage)
         await self.user.save(storage)
 
