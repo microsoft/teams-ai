7b1f3fa8ed654fb2be41c4434330ebbb74918951
Alex Acebo <aacebowork@gmail.com>
2023-09-12 10:46:39 -0700

    add missing extensions

--- python/packages/ai/teams/message_extensions.py
+++ python/packages/ai/teams/message_extensions.py
@@ -4,12 +4,23 @@ Licensed under the MIT License.
 """
 
 import re
-from typing import Awaitable, Callable, Generic, List, Literal, Pattern, TypeVar, Union
+from typing import (
+    Any,
+    Awaitable,
+    Callable,
+    Generic,
+    List,
+    Literal,
+    Pattern,
+    TypeVar,
+    Union,
+)
 
 from botbuilder.core import InvokeResponse, TurnContext
 from botbuilder.schema import Activity, ActivityTypes
 from botbuilder.schema.teams import (
     MessagingExtensionActionResponse,
+    MessagingExtensionQuery,
     MessagingExtensionResponse,
     MessagingExtensionResult,
     TaskModuleContinueResponse,
@@ -32,10 +43,60 @@ class MessageExtensions(Generic[StateT]):
     def __init__(self, routes: List[Route[StateT]]) -> None:
         self._routes = routes
 
+    def query(self, command_id: Union[str, Pattern[str]]):
+        """
+        Registers a handler that implements a Search based Message Extension.
+
+        ```python
+        # Use this method as a decorator
+        @app.message_extensions.query("test")
+        async def on_query(context: TurnContext, state: TurnState, url: str):
+            return MessagingExtensionResult()
+
+        # Pass a function to this method
+        app.message_extensions.query("test")(on_query)
+        ```
+
+        #### Args:
+        - `command_id`: a string or regex pattern that matches against the activities
+        `command_id`
+        """
+
+        def __selector__(context: TurnContext):
+            if (
+                context.activity.type != ActivityTypes.invoke
+                or context.activity.name != "composeExtension/query"
+                or not context.activity.value
+                or not self._activity_with_command_id(context.activity, command_id)
+            ):
+                return False
+
+            return True
+
+        def __call__(
+            func: Callable[
+                [TurnContext, StateT, MessagingExtensionQuery],
+                Awaitable[MessagingExtensionResult],
+            ]
+        ):
+            async def __invoke__(context: TurnContext, state: StateT):
+                query = context.activity.value
+
+                if not query or not isinstance(query, MessagingExtensionQuery):
+                    return False
+
+                res = await func(context, state, query)
+                await self._invoke_response(context, res)
+                return True
+
+            self._routes.append(Route[StateT](__selector__, __invoke__, True))
+            return func
+
+        return __call__
+
     def query_link(self, command_id: Union[str, Pattern[str]]):
         """
-        Registers a handler to process an action of a message that's being
-        previewed by the user prior to sending.
+        Registers a handler that implements a Link Unfurling based Message Extension.
 
         ```python
         # Use this method as a decorator
@@ -82,7 +143,7 @@ class MessageExtensions(Generic[StateT]):
                 await self._invoke_response(context, res)
                 return True
 
-            self._routes.append(Route[StateT](__selector__, __invoke__))
+            self._routes.append(Route[StateT](__selector__, __invoke__, True))
             return func
 
         return __call__
@@ -133,7 +194,7 @@ class MessageExtensions(Generic[StateT]):
                 await self._invoke_response(context, res)
                 return True
 
-            self._routes.append(Route[StateT](__selector__, __invoke__))
+            self._routes.append(Route[StateT](__selector__, __invoke__, True))
             return func
 
         return __call__
@@ -205,15 +266,15 @@ class MessageExtensions(Generic[StateT]):
                 await self._invoke_action_response(context, res)
                 return True
 
-            self._routes.append(Route[StateT](__selector__, __invoke__))
+            self._routes.append(Route[StateT](__selector__, __invoke__, True))
             return func
 
         return __call__
 
     def fetch_task(self, command_id: Union[str, Pattern[str]]):
         """
-        Registers a handler to process an action of a message that's being
-        previewed by the user prior to sending.
+        Registers a handler to process the initial fetch task for an
+        Action based message extension.
 
         ```python
         # Use this method as a decorator
@@ -252,7 +313,95 @@ class MessageExtensions(Generic[StateT]):
                 await self._invoke_task_response(context, res)
                 return True
 
-            self._routes.append(Route[StateT](__selector__, __invoke__))
+            self._routes.append(Route[StateT](__selector__, __invoke__, True))
+            return func
+
+        return __call__
+
+    def select_item(self):
+        """
+        Registers a handler that implements the logic to handle the
+        tap actions for items returned by a Search based message extension.
+
+        ```python
+        # Use this method as a decorator
+        @app.message_extensions.select_item
+        async def on_select_item(context: TurnContext, state: TurnState, item: Any):
+            return MessagingExtensionResult()
+
+        # Pass a function to this method
+        app.message_extensions.select_item()(on_select_item)
+        ```
+        """
+
+        def __selector__(context: TurnContext):
+            if (
+                context.activity.type != ActivityTypes.invoke
+                or context.activity.name != "composeExtension/selectItem"
+            ):
+                return False
+
+            return True
+
+        def __call__(
+            func: Callable[
+                [TurnContext, StateT, Any],
+                Awaitable[MessagingExtensionResult],
+            ]
+        ):
+            async def __invoke__(context: TurnContext, state: StateT):
+                res = await func(context, state, context.activity.value)
+                await self._invoke_action_response(context, res)
+                return True
+
+            self._routes.append(Route[StateT](__selector__, __invoke__, True))
+            return func
+
+        return __call__
+
+    def submit_action(self, command_id: Union[str, Pattern[str]]):
+        """
+        Registers a handler that implements the submit action for an
+        Action based Message Extension.
+
+        ```python
+        # Use this method as a decorator
+        @app.message_extensions.submit_action("test")
+        async def on_submit_action(context: TurnContext, state: TurnState, value: Any):
+            return TaskModuleTaskInfo()
+
+        # Pass a function to this method
+        app.message_extensions.submit_action("test")(on_submit_action)
+        ```
+
+        #### Args:
+        - `command_id`: a string or regex pattern that matches against the activities
+        `command_id`
+        """
+
+        def __selector__(context: TurnContext):
+            if (
+                context.activity.type != ActivityTypes.invoke
+                or context.activity.name != "composeExtension/submitAction"
+                or not context.activity.value
+                or not self._activity_with_command_id(context.activity, command_id)
+            ):
+                return False
+
+            return True
+
+        def __call__(
+            func: Callable[
+                [TurnContext, StateT, Any],
+                Awaitable[Union[MessagingExtensionResult, TaskModuleTaskInfo, str, None]],
+            ]
+        ):
+            async def __invoke__(context: TurnContext, state: StateT):
+                res = await func(context, state, context.activity.value)
+                await self._invoke_action_response(context, res)
+                return True
+
+            self._routes.append(Route[StateT](__selector__, __invoke__, True))
             return func
 
         return __call__
