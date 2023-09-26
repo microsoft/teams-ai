ecba47f4d1d4728bd1ca654c0455412c60bb00a3
Alex Acebo <aacebowork@gmail.com>
2023-09-26 12:31:47 -0700

    add azure client and disable moderation in azure planner

--- python/packages/ai/teams/ai/models/azure_openai/planner.py
+++ python/packages/ai/teams/ai/models/azure_openai/planner.py
@@ -3,21 +3,47 @@ Copyright (c) Microsoft Corporation. All rights reserved.
 Licensed under the MIT License.
 """
 
+from typing import Optional
+
 from semantic_kernel.connectors.ai.open_ai import (
     AzureChatCompletion,
     AzureTextCompletion,
 )
 
+from botbuilder.core import TurnContext
+
+from teams.ai.state import TurnState
+from teams.ai.prompts import PromptTemplate
 from teams.ai.models.openai import OpenAIPlanner
+from teams.ai.planner import Plan
 
+from .client import AzureOpenAIClient
 from .planner_options import AzureOpenAIPlannerOptions
-
 
 class AzureOpenAIPlanner(OpenAIPlanner):
     _options: AzureOpenAIPlannerOptions
+    _client: AzureOpenAIClient
 
     def __init__(self, options: AzureOpenAIPlannerOptions):
         super().__init__(options)
+        self._options = options
+        self._client = AzureOpenAIClient(self._options.api_key, base_url=options.endpoint)
+
+    async def review_prompt(
+        self,
+        _context: TurnContext,
+        _state: TurnState,
+        _prompt: PromptTemplate,
+    ) -> Optional[Plan]:
+        return None
+
+    async def review_plan(
+        self,
+        _context: TurnContext,
+        _state: TurnState,
+        plan: Plan,
+    ) -> Plan:
+        return plan
 
     def _add_text_completion_service(self) -> None:
         self._sk.add_text_completion_service(
