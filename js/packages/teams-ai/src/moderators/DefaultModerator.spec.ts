4b3f94bc1f866733124a72a87f138aa041d5dd3c
Steven Ickman <stevenic@microsoft.com>
2023-10-12 15:35:45 -0700

    Update DefaultModerator.spec.ts

--- js/packages/teams-ai/src/moderators/DefaultModerator.spec.ts
+++ js/packages/teams-ai/src/moderators/DefaultModerator.spec.ts
@@ -1,48 +1,35 @@
 import { strict as assert } from 'assert';
 import { DefaultModerator } from './DefaultModerator';
-import { ActivityTypes, TestAdapter, TurnContext, MemoryStorage } from 'botbuilder-core';
-import { TurnState, TurnStateEntry } from '../TurnState';
-import { ConversationHistory } from '../ConversationHistory';
+import { TestAdapter } from 'botbuilder';
+import { TestTurnState } from '../TestTurnState';
 import { Plan, PredictedDoCommand } from '../planners/Planner';
 
 describe('DefaultModerator', () => {
     const adapter = new TestAdapter();
-
-    const createTurnContextAndState = async (): Promise<[TurnContext, TurnState]> => {
-        const mockTurnContext = new TurnContext(adapter, { text: 'test', type: ActivityTypes.Message });
-        const storage = new MemoryStorage();
-        const state = new TurnState();
-        await state.load(mockTurnContext, storage);
-        return [mockTurnContext, state];
+    const expectedPlan: Plan = {
+        type: 'plan',
+        commands: [{} as PredictedDoCommand]
     };
 
-    describe('reviewPrompt()', () => {
+    describe('reviewInput()', () => {
         it('should return undefined', async () => {
-            const [mockTurnContext, state] = await createTurnContextAndState();
-
-            const moderator = new DefaultModerator();
-
-            const plan = await moderator.reviewInput(
-                mockTurnContext,
-                state,
-            );
-            assert.equal(plan, undefined);
+            await adapter.sendTextToBot('test', async (context) => {
+                const state = await TestTurnState.create(context);
+                const moderator = new DefaultModerator();
+                const plan = await moderator.reviewInput(context, state);
+                assert.equal(plan, undefined);
+            });
         });
     });
 
-    describe('reviewPrompt()', () => {
+    describe('reviewOutput()', () => {
         it('should return the provided plan', async () => {
-            const [mockTurnContext, state] = await createTurnContextAndState();
-
-            const expectedPlan: Plan = {
-                type: 'plan',
-                commands: [{} as PredictedDoCommand]
-            };
-
-            const moderator = new DefaultModerator();
-
-            const plan = await moderator.reviewOutput(mockTurnContext, state, expectedPlan);
-            assert.deepEqual(plan, expectedPlan);
+            await adapter.sendTextToBot('test', async (context) => {
+                const state = await TestTurnState.create(context);
+                const moderator = new DefaultModerator();
+                const plan = await moderator.reviewOutput(context, state, expectedPlan);
+                assert.deepEqual(plan, expectedPlan);
+            });
         });
     });
 });
