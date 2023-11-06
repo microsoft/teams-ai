a235c1ca209747834271444af1a029e8b5394121
Corina <14900841+corinagum@users.noreply.github.com>
2023-11-06 14:07:01 -0800

    [JS] fix: #247 #635 Add fallthrough for empty result Task Module Fetch and tests (#772)
    ## Linked issues
    
    closes: #635
    closes: #247 
    
    ## Details
    
    - A behavior that was missing from Task Modules was handling submit
    "continue" scenario when the handler does not return a
    `TaskModuleResponse` or string. This ensures that the expected response
    (empty 200) is sent even if there is nothing new to render/send.
    - Modify so that Teams events check for channelId using `Channels` enum,
    not 'msteams' string
    - This included minor refactor for Message Extension and Application as
    well
    - Add Task Module tests to get the file to 80% coverage
    
    ## Attestation Checklist
    
    - [x] My code follows the style guidelines of this project
    
    - I have checked for/fixed spelling, linting, and other errors
    - I have commented my code for clarity
    - I have made corresponding changes to the documentation (we use
    [TypeDoc](https://typedoc.org/) to document our code)
    - My changes generate no new warnings
    - I have added tests that validates my changes, and provides sufficient
    test coverage. I have tested with:
      - Local testing
      - E2E testing in Teams
    - New and existing unit tests pass locally with my changes
    
    ### Additional information
    
    Thank you @th1nkful for the original
    [PR](https://github.com/microsoft/teams-ai/pull/636), which I modified
    to match Bot Framework protocol :)
    
    ---------
    
    Co-authored-by: Corina Gum <>

---
+++ js/packages/teams-ai/src/TaskModules.spec.ts
@@ -0,0 +1,188 @@
+import { strict as assert } from 'assert';
+import { Application } from './Application';
+import { createTestInvoke } from './TestUtilities';
+import {
+    ActivityTypes,
+    Channels,
+    INVOKE_RESPONSE_KEY,
+    TaskModuleContinueResponse,
+    TaskModuleMessageResponse,
+    TestAdapter,
+    TurnContext
+} from 'botbuilder';
+import { TaskModules } from './TaskModules';
+
+describe('TaskModules', () => {
+    const adapter = new TestAdapter();
+    const FETCH_INVOKE_NAME = `task/fetch`;
+    const SUBMIT_INVOKE_NAME = `task/submit`;
+
+    let mockApp: Application;
+    beforeEach(() => {
+        mockApp = new Application({ adapter });
+    });
+
+    const createExpected200Response = (taskModuleResponse: TaskModuleContinueResponse | TaskModuleMessageResponse) => {
+        if (taskModuleResponse.type != 'continue' && taskModuleResponse.type != 'message') {
+            throw new Error(
+                `Test setup failed. TaskModuleResponse.type must be either 'continue' or 'message', not ${taskModuleResponse.type}`
+            );
+        }
+
+        if (typeof taskModuleResponse.value != 'string' && typeof taskModuleResponse.value != 'object') {
+            throw new Error(
+                `Test setup faiiled. TaskModuleResponse.value type must be either a string or an object, not ${typeof taskModuleResponse.value}`
+            );
+        }
+        return {
+            status: 200,
+            body: { task: taskModuleResponse }
+        };
+    };
+
+    it('should exist when Application is instantiated', () => {
+        assert.notEqual(mockApp.taskModules, undefined);
+        assert.equal(mockApp.taskModules instanceof TaskModules, true);
+    });
+
+    describe(FETCH_INVOKE_NAME, () => {
+        const TEST_VERB = 'testVerb';
+        const happyPathTestData = [
+            {
+                testCase: 'should return a 200 and TaskModuleMessageResponse for a returned string',
+                testVerb: TEST_VERB,
+                testActivity: createTestInvoke(FETCH_INVOKE_NAME, {
+                    data: { verb: TEST_VERB }
+                }),
+                handlerReturnValue: 'message',
+                expectedResult: createExpected200Response({ type: 'message', value: 'message' })
+            }
+        ];
+
+        for (const { testCase, testVerb, testActivity, expectedResult, handlerReturnValue } of happyPathTestData) {
+            testActivity.channelId = Channels.Msteams;
+            it(testCase, async () => {
+                mockApp.taskModules.fetch(testVerb, async (_context, _state, _data) => {
+                    return handlerReturnValue;
+                });
+
+                await adapter.processActivity(testActivity, async (context) => {
+                    await mockApp.run(context);
+                    const response = context.turnState.get(INVOKE_RESPONSE_KEY);
+                    assert.deepEqual(response.value, expectedResult);
+                });
+            });
+        }
+
+        it('fetch() with custom RouteSelector happy path', async () => {
+            const expectedVerbValue = 'verbValue';
+            const testActivity = createTestInvoke(FETCH_INVOKE_NAME, {
+                data: { verb: expectedVerbValue }
+            });
+            testActivity.channelId = Channels.Msteams;
+            const messageResult = 'messageResult';
+            mockApp.taskModules.fetch(expectedVerbValue, async (_context, _state, _data) => {
+                return messageResult;
+            });
+
+            await adapter.processActivity(testActivity, async (context) => {
+                await mockApp.run(context);
+                const response = context.turnState.get(INVOKE_RESPONSE_KEY);
+                assert.deepEqual(response.value, {
+                    status: 200,
+                    body: { task: { type: 'message', value: messageResult } }
+                });
+            });
+        });
+
+        it('fetch() with custom RouteSelector handler result is falsy', async () => {
+            const expectedVerbValue = 'verbValue';
+            const testActivity = createTestInvoke(FETCH_INVOKE_NAME, {
+                data: { verb: expectedVerbValue }
+            });
+            testActivity.channelId = Channels.Msteams;
+            mockApp.taskModules.fetch(expectedVerbValue, async (_context, _state, _data) => {
+                return {};
+            });
+
+            await adapter.processActivity(testActivity, async (context) => {
+                await mockApp.run(context);
+                const response = context.turnState.get(INVOKE_RESPONSE_KEY);
+                assert.deepEqual(response.value, {
+                    status: 200,
+                    body: { task: { type: 'continue', value: {} } }
+                });
+            });
+        });
+
+        it('fetch() should throw an error when activity.name is incorrect', async () => {
+            const testActivity = { channelId: Channels.Msteams, type: ActivityTypes.Invoke, name: 'incorrectName' };
+            const badRouteSelector = (context: TurnContext) => {
+                return Promise.resolve(true);
+            };
+
+            mockApp.taskModules.fetch(badRouteSelector, async (_context, _state, _data) => {
+                return 'testFailed'; // the test should not reach this point
+            });
+            await assert.rejects(async () => {
+                await adapter.processActivity(testActivity, async (context) => {
+                    await mockApp.run(context);
+                });
+            });
+        });
+        it('fetch() with custom RouteSelector unhappy path', async () => {
+            const testActivity = { channelId: Channels.Msteams, type: ActivityTypes.Invoke, name: 'incorrectName' };
+            const badRouteSelector = (context: TurnContext) => {
+                return Promise.resolve(true);
+            };
+
+            mockApp.taskModules.fetch(badRouteSelector, async (context, _state, _data) => {
+                return Promise.resolve('');
+            });
+            await assert.rejects(async () => {
+                await adapter.processActivity(testActivity, async (context) => {
+                    await mockApp.run(context);
+                });
+            });
+        });
+    });
+
+    describe(SUBMIT_INVOKE_NAME, () => {
+        it('submit() with custom RouteSelector happy path', async () => {
+            const expectedVerbValue = 'verbValue';
+            const testActivity = createTestInvoke(SUBMIT_INVOKE_NAME, {
+                data: { verb: expectedVerbValue }
+            });
+            testActivity.channelId = Channels.Msteams;
+            const messageResult = 'messageResult';
+            mockApp.taskModules.submit(expectedVerbValue, async (_context, _state, _data) => {
+                return messageResult;
+            });
+
+            await adapter.processActivity(testActivity, async (context) => {
+                await mockApp.run(context);
+                const response = context.turnState.get(INVOKE_RESPONSE_KEY);
+                assert.deepEqual(response.value, {
+                    status: 200,
+                    body: { task: { type: 'message', value: messageResult } }
+                });
+            });
+        });
+
+        it('submit() with custom RouteSelector unhappy path', async () => {
+            const testActivity = { channelId: Channels.Msteams, type: ActivityTypes.Invoke, name: 'incorrectName' };
+            const badRouteSelector = (context: TurnContext) => {
+                return Promise.resolve(true);
+            };
+
+            mockApp.taskModules.submit(badRouteSelector, async (_context, _state, _data) => {
+                return Promise.resolve(undefined);
+            });
+            await assert.rejects(async () => {
+                await adapter.processActivity(testActivity, async (context) => {
+                    await mockApp.run(context);
+                });
+            });
+        });
+    });
+});
