3b82342ac04d57abe008502b27a46bf7a30922ad
Alex Acebo <aacebowork@gmail.com>
2023-11-07 09:08:13 -0800

    add tokenizer interface

---
+++ dotnet/packages/Microsoft.TeamsAI/Microsoft.TeamsAI/AI/Tokenizers/ITokenizer.cs
@@ -0,0 +1,15 @@
+﻿namespace Microsoft.Teams.AI.AI.Tokenizers
+{
+    /// <summary>
+    /// Tokenizer
+    /// </summary>
+    public interface ITokenizer
+    {
+        /// <summary>
+        /// Encode
+        /// </summary>
+        /// <param name="text">text to encode</param>
+        /// <returns>encoded bytes</returns>
+        public List<int> Encode(string text);
+    }
+}
