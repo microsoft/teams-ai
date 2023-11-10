90bc3901af8d0123960fa0c40ffd3ca191e43990
limamicro <89859513+limamicro@users.noreply.github.com>
2023-11-10 09:38:55 -0800

    [C#] refactor: implement validator interface (#804)
    ## Linked issues
    https://github.com/microsoft/teams-ai/issues/722
    closes: #722
    
    ## Details
    
    Provide a list of your changes here. If you are fixing a bug, please
    provide steps to reproduce the bug.
    
    #### Change details
    
    > Describe your changes, with screenshots and code snippets as
    appropriate
    
    **code snippets**:
    
    **screenshots**:
    
    ## Attestation Checklist
    
    - [ ] My code follows the style guidelines of this project
    
    - I have checked for/fixed spelling, linting, and other errors
    - I have commented my code for clarity
    - I have made corresponding changes to the documentation (we use
    [TypeDoc](https://typedoc.org/) to document our code)
    - My changes generate no new warnings
    - I have added tests that validates my changes, and provides sufficient
    test coverage. I have tested with:
      - Local testing
      - E2E testing in Teams
    - New and existing unit tests pass locally with my changes
    
    ### Additional information
    
    > Feel free to add other relevant information below

---
+++ dotnet/packages/Microsoft.TeamsAI/Microsoft.TeamsAI/AI/Prompt/PromptResponse.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Bot.Schema;
+using Microsoft.Teams.AI.AI.Models;
+
+namespace Microsoft.Teams.AI.AI.Prompt
+{
+    /// <summary>
+    /// Response returned by a `PromptCompletionClient`.
+    /// </summary>
+    public class PromptResponse
+    {
+        /// <summary>
+        /// Status of the prompt response.
+        /// </summary>
+        public PromptResponseStatus Status { get; set; }
+
+        /// <summary>
+        /// Message returned.
+        /// </summary>
+        public ChatMessage? Message { get; set; }
+
+        /// <summary>
+        /// Error returned.
+        /// </summary>
+        public Error? Error { get; set; }
+    }
+
+    /// <summary>
+    /// Status of the prompt response.
+    /// </summary>
+    public enum PromptResponseStatus
+    {
+        /// <summary>
+        /// Success - The prompt was successfully completed.
+        /// </summary>
+        Success,
+        /// <summary>
+        /// Error - An error occurred while completing the prompt.
+        /// </summary>
+        Error,
+        /// <summary>
+        /// RateLimited - The request was rate limited.
+        /// </summary>
+        RateLimited,
+        /// <summary>
+        /// InvalidResponse - The response was invalid.
+        /// </summary>
+        InvalidResponse,
+        /// <summary>
+        /// TooLong - The rendered prompt exceeded the `max_input_tokens` limit.
+        /// </summary>
+        TooLong
+    }
+}
---
+++ dotnet/packages/Microsoft.TeamsAI/Microsoft.TeamsAI/AI/Validators/IPromptResponseValidator.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Bot.Builder;
+using Microsoft.Teams.AI.AI.Prompt;
+using Microsoft.Teams.AI.AI.Tokenizers;
+using Microsoft.Teams.AI.Memory;
+
+namespace Microsoft.Teams.AI.AI.Validators
+{
+    /// <summary>
+    /// A validator that can be used to validate prompt responses.
+    /// </summary>
+    public interface IPromptResponseValidator<TValue>
+    {
+        /// <summary>
+        /// Validates a response to a prompt.
+        /// </summary>
+        /// <param name="context">Context for the current turn of conversation with the user.</param>
+        /// <param name="memory">An interface for accessing state values.</param>
+        /// <param name="tokenizer">Tokenizer to use for encoding and decoding text.</param>
+        /// <param name="response">Response to validate.</param>
+        /// <param name="remainingAttempts">Number of remaining attempts to validate the response.</param>
+        /// <returns></returns>
+        public Task<Validation<TValue>> ValidateResponse(TurnContext context, IMemory memory, ITokenizer tokenizer, PromptResponse response, int remainingAttempts);
+    }
+}
