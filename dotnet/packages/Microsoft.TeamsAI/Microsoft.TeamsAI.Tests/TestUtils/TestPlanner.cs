fb81b750fe150de02dc54e21a77a0fc3cc021281
Kuojian Lu <kuojianlu@gmail.com>
2023-11-12 20:45:40 -0600

    [C#] refactor: update planner interface (#797)
    ## Linked issues
    
    closes: #715 
    
    ## Details
    
    Provide a list of your changes here. If you are fixing a bug, please
    provide steps to reproduce the bug.
    
    #### Change details
    
    - update IPlanner interface
    - remove ChainAsync, CompletePromptAsync, CreateSemanticFunction,
    _ConfigureOptions from AI class because they are deprecated and break
    the new planner interface
    
    ## Attestation Checklist
    
    - [ ] My code follows the style guidelines of this project
    
    - I have checked for/fixed spelling, linting, and other errors
    - I have commented my code for clarity
    - I have made corresponding changes to the documentation (we use
    [TypeDoc](https://typedoc.org/) to document our code)
    - My changes generate no new warnings
    - I have added tests that validates my changes, and provides sufficient
    test coverage. I have tested with:
      - Local testing
      - E2E testing in Teams
    - New and existing unit tests pass locally with my changes
    
    ### Additional information
    
    > Feel free to add other relevant information below

--- dotnet/packages/Microsoft.TeamsAI/Microsoft.TeamsAI.Tests/TestUtils/TestPlanner.cs
+++ dotnet/packages/Microsoft.TeamsAI/Microsoft.TeamsAI.Tests/TestUtils/TestPlanner.cs
@@ -1,37 +1,29 @@
-﻿using System.Reflection;
-
-using Microsoft.Bot.Builder;
+﻿using Microsoft.Bot.Builder;
 using Microsoft.Teams.AI.AI;
 using Microsoft.Teams.AI.AI.Planner;
-using Microsoft.Teams.AI.AI.Prompt;
 
 namespace Microsoft.Teams.AI.Tests.TestUtils
 {
     public class TestPlanner : IPlanner<TestTurnState>
     {
-        public IList<string> Record { get; } = new List<string>();
-
-        public string CompletePromptResult { get; set; } = "Default";
-
-        public Plan GeneratePlanResult { get; set; } = new Plan
+        public Plan BeginPlan { get; set; } = new Plan
         {
             Commands = new List<IPredictedCommand>
             {
-                new PredictedDoCommand("Test-DO"),
                 new PredictedSayCommand("Test-SAY")
             }
         };
 
-        public Task<string> CompletePromptAsync(ITurnContext turnContext, TestTurnState turnState, PromptTemplate promptTemplate, AIOptions<TestTurnState> options, CancellationToken cancellationToken)
+        public Plan ContinuePlan { get; set; } = new Plan();
+
+        public Task<Plan> BeginTaskAsync(ITurnContext turnContext, TestTurnState turnState, AI<TestTurnState> ai, CancellationToken cancellationToken)
         {
-            Record.Add(MethodBase.GetCurrentMethod()!.Name);
-            return Task.FromResult(CompletePromptResult);
+            return Task.FromResult(BeginPlan);
         }
 
-        public Task<Plan> GeneratePlanAsync(ITurnContext turnContext, TestTurnState turnState, PromptTemplate promptTemplate, AIOptions<TestTurnState> options, CancellationToken cancellationToken)
+        public Task<Plan> ContinueTaskAsync(ITurnContext turnContext, TestTurnState turnState, AI<TestTurnState> ai, CancellationToken cancellationToken)
         {
-            Record.Add(MethodBase.GetCurrentMethod()!.Name);
-            return Task.FromResult(GeneratePlanResult);
+            return Task.FromResult(ContinuePlan);
         }
     }
 }
