5403738dcec314bcbd2de68f17c5dac77c74121b
Alex Acebo <aacebowork@gmail.com>
2023-11-14 14:22:08 -0800

    [C#] feat: add DataSources (#811)
    ## Linked issues
    
    closes: #724 
    
    ## Details
    
    - add `TextDataSource`
    - add tests for `DataSourceSection`

---
+++ dotnet/packages/Microsoft.TeamsAI/Microsoft.TeamsAI.Tests/AITests/PromptsTests/SectionsTests/DataSourceSectionTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Bot.Builder;
+using Microsoft.Teams.AI.AI.DataSources;
+using Microsoft.Teams.AI.AI.Prompts;
+using Microsoft.Teams.AI.AI.Prompts.Sections;
+using Microsoft.Teams.AI.AI.Tokenizers;
+using Moq;
+
+namespace Microsoft.Teams.AI.Tests.AITests.PromptsTests.SectionsTests
+{
+    public class DataSourceSectionTests
+    {
+        [Fact]
+        public async void Test_RenderAsTextAsync_ShouldRender()
+        {
+            TextDataSource dataSource = new("test", "my text to use");
+            DataSourceSection section = new(dataSource);
+            Mock<ITurnContext> context = new();
+            Memory.Memory memory = new();
+            GPTTokenizer tokenizer = new();
+            PromptManager manager = new();
+            RenderedPromptSection<string> rendered = await section.RenderAsTextAsync(context.Object, memory, manager, tokenizer, 10);
+
+            Assert.Equal("my text to use", rendered.Output);
+            Assert.Equal(4, rendered.Length);
+        }
+
+        [Fact]
+        public async void Test_RenderAsTextAsync_ShouldTruncate()
+        {
+            TextDataSource dataSource = new("test", "my text to use");
+            DataSourceSection section = new(dataSource);
+            Mock<ITurnContext> context = new();
+            Memory.Memory memory = new();
+            GPTTokenizer tokenizer = new();
+            PromptManager manager = new();
+            RenderedPromptSection<string> rendered = await section.RenderAsTextAsync(context.Object, memory, manager, tokenizer, 3);
+
+            Assert.Equal("my text to", rendered.Output);
+            Assert.Equal(3, rendered.Length);
+        }
+    }
+}
