79a2adbaecd3312c42379b8e0a868e2fb3438788
Yunny Chung <sunchung@microsoft.com>
2023-11-16 13:28:04 -0800

    Port Embeddings logic to c# SDK.

---
+++ dotnet/packages/Microsoft.TeamsAI/Microsoft.TeamsAI/AI/Embeddings/EmbeddingsResponse.cs
@@ -0,0 +1,44 @@
+﻿namespace Microsoft.Teams.AI.AI.Embeddings
+{
+    /// <summary>
+    /// Response returned by a `EmbeddingsClient`.
+    /// </summary>
+    public class EmbeddingsResponse
+    {
+        /// <summary>
+        /// Status of the embeddings response.
+        /// </summary>
+        public EmbeddingsResponseStatus Status { get; set; }
+
+        /// <summary>
+        /// Optional. Embeddings for the given inputs.
+        /// </summary>
+        public IList<ReadOnlyMemory<float>>? Output { get; set; }
+
+        /// <summary>
+        /// Optional. Message when status is not equal to `success`.
+        /// </summary>
+        public string? Message { get; set; }
+    }
+
+    /// <summary>
+    /// Status of the embeddings response.
+    /// </summary>
+    public enum EmbeddingsResponseStatus
+    {
+        /// <summary>
+        /// The embeddings were successfully created.
+        /// </summary>
+        Success,
+
+        /// <summary>
+        /// An error occurred while creating the embeddings.
+        /// </summary>
+        Failure,
+
+        /// <summary>
+        /// The request was rate limited.
+        /// </summary>
+        RateLimited,
+    }
+}
---
+++ dotnet/packages/Microsoft.TeamsAI/Microsoft.TeamsAI/AI/Embeddings/IEmbeddings.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Teams.AI.State;
+
+namespace Microsoft.Teams.AI.AI.Embeddings
+{
+    /// <summary>
+    /// Interface for Embeddings.
+    /// </summary>
+    public interface IEmbeddings<TState> where TState : ITurnState<StateBase, StateBase, TempState>
+    {
+        /// <summary>
+        ///     Creates embeddings for the given inputs using the OpenAI API.
+        /// </summary>
+        /// <param name="inputs">Text inputs to create embeddings for.</param>
+        /// <returns>A `EmbeddingsResponse` with a status and the generated embeddings or a message when an error occurs.</returns>
+        ///
+        Task<EmbeddingsResponse> CreateEmbeddings(IList<string> inputs);
+    }
+}
