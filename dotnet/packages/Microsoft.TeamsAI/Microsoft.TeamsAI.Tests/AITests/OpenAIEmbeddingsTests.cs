1128e56ac5b5ff2afeb20330dbd8996e4d74bdb2
Yunny Chung - MSFT <46677982+YunnyChung@users.noreply.github.com>
2023-11-16 15:31:55 -0800

    [C#] feat: Implement Embeddings and unittests (#822)
    ## Linked issues
    closes: #720 
    
    ## Details
    
    Provide a list of your changes here. If you are fixing a bug, please
    provide steps to reproduce the bug.
    
    
    #### Change details
    
    > Describe your changes, with screenshots and code snippets as
    appropriate
    
    - Introduce relevant model classes for Embeddings Response (ex:
    EmbeddingsResponse, OpenAIEmbeddingsOptions,
    AzureOpenAIEmbeddingsOptions)
    - Implement the main business logic to call [OpenAI Embeddings API
    ](https://github.com/Azure/azure-sdk-for-net/blob/5aef41fb02c7759a17f6c5e7a8c123a7120ccc00/sdk/openai/Azure.AI.OpenAI/tests/OpenAIInferenceTests.cs#L79)via
    Azure OpenAI SDK in OpenAIEmbeddgins.cs
    - Implement unittests.
    
    ## Attestation Checklist
    - [ ] My code follows the style guidelines of this project
    - I have checked for/fixed spelling, linting, and other errors
    - I have commented my code for clarity
    - I have made corresponding changes to the documentation (we use
    [TypeDoc](https://typedoc.org/) to document our code)
    - My changes generate no new warnings
    - I have added tests that validates my changes, and provides sufficient
    test coverage. I have tested with:
      - Local testing
      - E2E testing in Teams
    - New and existing unit tests pass locally with my changes
    
    ### Additional information
    
    > Feel free to add other relevant information below
    
    ---------
    
    Co-authored-by: Yunny Chung <sunchung@microsoft.com>

---
+++ dotnet/packages/Microsoft.TeamsAI/Microsoft.TeamsAI.Tests/AITests/OpenAIEmbeddingsTests.cs
@@ -0,0 +1,137 @@
+﻿using Azure;
+using Microsoft.Bot.Builder;
+using Microsoft.Teams.AI.Tests.TestUtils;
+using Moq;
+using System.Reflection;
+using Microsoft.Teams.AI.AI.Embeddings;
+using Azure.AI.OpenAI;
+using Microsoft.Teams.AI.Exceptions;
+
+#pragma warning disable CS8604 // Possible null reference argument.
+namespace Microsoft.Teams.AI.Tests.AITests
+{
+    public class OpenAIEmbeddingsTests
+    {
+        [Fact]
+        public async void Test_OpenAI_CreateEmbeddings_ReturnEmbeddings()
+        {
+            // Arrange
+            var apiKey = "randomApiKey";
+            var model = "randomModelId";
+
+            var options = new OpenAIEmbeddingsOptions(apiKey, model);
+            var turnContextMock = new Mock<ITurnContext>();
+            var turnStateMock = new Mock<TestTurnState>();
+            var openAiEmbeddings = new OpenAIEmbeddings<TestTurnState, OpenAIEmbeddingsOptions>(options);
+
+            IList<string> inputs = new List<string> { "test" };
+            var clientMock = new Mock<OpenAIClient>(It.IsAny<string>());
+            IEnumerable<EmbeddingItem> data = new List<EmbeddingItem>()
+            {
+                AzureOpenAIModelFactory.EmbeddingItem()
+            };
+            EmbeddingsUsage usage = AzureOpenAIModelFactory.EmbeddingsUsage();
+            Embeddings embeddingsResult = AzureOpenAIModelFactory.Embeddings(data, usage);
+            Response? response = null;
+            clientMock.Setup(client => client.GetEmbeddingsAsync(It.IsAny<EmbeddingsOptions>(), It.IsAny<CancellationToken>())).ReturnsAsync(Response.FromValue(embeddingsResult, response));
+            openAiEmbeddings.GetType().GetField("_client", BindingFlags.Instance | BindingFlags.NonPublic)!.SetValue(openAiEmbeddings, clientMock.Object);
+
+            // Act
+            var result = await openAiEmbeddings.CreateEmbeddings(inputs);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.NotNull(result.Output);
+            Assert.Equal(result.Status, EmbeddingsResponseStatus.Success);
+        }
+
+        [Fact]
+        public async void Test_AzureOpenAI_CreateEmbeddings_ReturnEmbeddings()
+        {
+            // Arrange
+            var apiKey = "randomApiKey";
+            var model = "randomModelId";
+            var endpoint = "https://test.cognitiveservices.azure.com";
+            var options = new AzureOpenAIEmbeddingsOptions(apiKey, model, endpoint);
+
+            var turnContextMock = new Mock<ITurnContext>();
+            var turnStateMock = new Mock<TestTurnState>();
+            var openAiEmbeddings = new OpenAIEmbeddings<TestTurnState, AzureOpenAIEmbeddingsOptions>(options);
+
+            IList<string> inputs = new List<string> { "test" };
+            IEnumerable<EmbeddingItem> data = new List<EmbeddingItem>()
+            {
+                AzureOpenAIModelFactory.EmbeddingItem()
+            };
+            EmbeddingsUsage usage = AzureOpenAIModelFactory.EmbeddingsUsage();
+            Embeddings embeddingsResult = AzureOpenAIModelFactory.Embeddings(data, usage);
+            Response? response = null;
+            var clientMock = new Mock<OpenAIClient>(It.IsAny<string>());
+            clientMock.Setup(client => client.GetEmbeddingsAsync(It.IsAny<EmbeddingsOptions>(), It.IsAny<CancellationToken>())).ReturnsAsync(Response.FromValue(embeddingsResult, response));
+            openAiEmbeddings.GetType().GetField("_client", BindingFlags.Instance | BindingFlags.NonPublic)!.SetValue(openAiEmbeddings, clientMock.Object);
+
+            // Act
+            var result = await openAiEmbeddings.CreateEmbeddings(inputs);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.NotNull(result.Output);
+            Assert.Equal(result.Status, EmbeddingsResponseStatus.Success);
+        }
+
+        [Theory]
+        [InlineData(429, "too many requests", EmbeddingsResponseStatus.RateLimited)]
+        [InlineData(502, "service error", EmbeddingsResponseStatus.Failure)]
+        public async void Test_CreateEmbeddings_ThrowRequestFailedException(int statusCode, string errorMsg, EmbeddingsResponseStatus responseStatus)
+        {
+            // Arrange
+            var apiKey = "randomApiKey";
+            var model = "randomModelId";
+
+            var options = new OpenAIEmbeddingsOptions(apiKey, model);
+            var turnContextMock = new Mock<ITurnContext>();
+            var turnStateMock = new Mock<TestTurnState>();
+            var openAiEmbeddings = new OpenAIEmbeddings<TestTurnState, OpenAIEmbeddingsOptions>(options);
+
+            IList<string> inputs = new List<string> { "test" };
+            var exception = new RequestFailedException(statusCode, errorMsg);
+            var clientMock = new Mock<OpenAIClient>(It.IsAny<string>());
+            clientMock.Setup(client => client.GetEmbeddingsAsync(It.IsAny<EmbeddingsOptions>(), It.IsAny<CancellationToken>())).ThrowsAsync(exception);
+            openAiEmbeddings.GetType().GetField("_client", BindingFlags.Instance | BindingFlags.NonPublic)!.SetValue(openAiEmbeddings, clientMock.Object);
+
+            // Act
+            var result = await openAiEmbeddings.CreateEmbeddings(inputs);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Null(result.Output);
+            Assert.Equal(result.Status, responseStatus);
+        }
+
+        [Fact]
+        public async void Test_CreateEmbeddings_ThrowException()
+        {
+            // Arrange
+            var apiKey = "randomApiKey";
+            var model = "randomModelId";
+
+            var options = new OpenAIEmbeddingsOptions(apiKey, model);
+            var turnContextMock = new Mock<ITurnContext>();
+            var turnStateMock = new Mock<TestTurnState>();
+            var openAiEmbeddings = new OpenAIEmbeddings<TestTurnState, OpenAIEmbeddingsOptions>(options);
+
+            IList<string> inputs = new List<string> { "test" };
+            var exception = new InvalidOperationException("other exception");
+            var clientMock = new Mock<OpenAIClient>(It.IsAny<string>());
+            clientMock.Setup(client => client.GetEmbeddingsAsync(It.IsAny<EmbeddingsOptions>(), It.IsAny<CancellationToken>())).ThrowsAsync(exception);
+            openAiEmbeddings.GetType().GetField("_client", BindingFlags.Instance | BindingFlags.NonPublic)!.SetValue(openAiEmbeddings, clientMock.Object);
+
+            // Act
+            var result = await Assert.ThrowsAsync<TeamsAIException>(async () => await openAiEmbeddings.CreateEmbeddings(inputs));
+
+            // Assert
+            Assert.Equal("Error while executing openAI Embeddings execution: other exception", result.Message);
+        }
+    }
+}
+#pragma warning restore CS8604 // Possible null reference argument.
