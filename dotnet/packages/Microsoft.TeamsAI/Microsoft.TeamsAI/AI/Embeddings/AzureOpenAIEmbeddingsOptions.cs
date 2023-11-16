8cb33d567204993717cd1960fffd2b3e39b86d31
Yunny Chung - MSFT <46677982+YunnyChung@users.noreply.github.com>
2023-11-16 15:31:55 -0800

    [C#] feat: Implement Embeddings and unittests (#822)
    ## Linked issues
    closes: #720 
    
    ## Details
    
    Provide a list of your changes here. If you are fixing a bug, please
    provide steps to reproduce the bug.
    
    
    #### Change details
    
    > Describe your changes, with screenshots and code snippets as
    appropriate
    
    - Introduce relevant model classes for Embeddings Response (ex:
    EmbeddingsResponse, OpenAIEmbeddingsOptions,
    AzureOpenAIEmbeddingsOptions)
    - Implement the main business logic to call [OpenAI Embeddings API
    ](https://github.com/Azure/azure-sdk-for-net/blob/5aef41fb02c7759a17f6c5e7a8c123a7120ccc00/sdk/openai/Azure.AI.OpenAI/tests/OpenAIInferenceTests.cs#L79)via
    Azure OpenAI SDK in OpenAIEmbeddgins.cs
    - Implement unittests.
    
    ## Attestation Checklist
    - [ ] My code follows the style guidelines of this project
    - I have checked for/fixed spelling, linting, and other errors
    - I have commented my code for clarity
    - I have made corresponding changes to the documentation (we use
    [TypeDoc](https://typedoc.org/) to document our code)
    - My changes generate no new warnings
    - I have added tests that validates my changes, and provides sufficient
    test coverage. I have tested with:
      - Local testing
      - E2E testing in Teams
    - New and existing unit tests pass locally with my changes
    
    ### Additional information
    
    > Feel free to add other relevant information below
    
    ---------
    
    Co-authored-by: Yunny Chung <sunchung@microsoft.com>

---
+++ dotnet/packages/Microsoft.TeamsAI/Microsoft.TeamsAI/AI/Embeddings/AzureOpenAIEmbeddingsOptions.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Teams.AI.Utilities;
+
+namespace Microsoft.Teams.AI.AI.Embeddings
+{
+    /// <summary>
+    /// Additional options needed to use the Azure OpenAI service.
+    /// </summary>
+    /// <remarks>
+    /// The Azure OpenAI API version is set to latest by default.
+    /// </remarks>
+    public class AzureOpenAIEmbeddingsOptions : OpenAIEmbeddingsOptions
+    {
+        /// <summary>
+        /// Endpoint for your Azure OpenAI deployment.
+        /// </summary>
+        public new string Endpoint { get; set; }
+
+        /// <summary>
+        /// Create an instance of the AzureOpenAIEmbeddingsOptions class.
+        /// </summary>
+        /// <param name="apiKey">OpenAI API key.</param>
+        /// <param name="model">The model to use for embeddings. This should be the model deployment name, not the model</param>
+        /// <param name="endpoint">Endpoint for your Azure OpenAI deployment.</param>
+        public AzureOpenAIEmbeddingsOptions(string apiKey, string model, string endpoint) : base(apiKey, model)
+        {
+            Verify.ParamNotNull(endpoint);
+
+            Endpoint = endpoint;
+        }
+    }
+}
