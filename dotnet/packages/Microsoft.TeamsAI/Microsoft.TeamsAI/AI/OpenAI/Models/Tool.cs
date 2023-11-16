a52f7a9b38dc66e692f68728778e6fa08ea90d88
Qianhao Dong <qidon@microsoft.com>
2023-11-16 12:08:31 +0800

    [C#] feat: call OpenAI's assistant and thread APIs (#819)
    ## Linked issues
    
    #minor (avoid closing issue)
    closes: #798
    
    ## Details
    
    Since OpenAI does not officially have .NET SDK, this PR adds OpenAI
    Assistant and Thread API calls to `OpenAIClient`
    
    #### Change details
    
    - Call OpenAI's Assistant and Thread APIs
    - Seperate into partial `OpenAIClient` class
    - Only add APIs that will be called in `AssistantPlanner`
    
    ## Attestation Checklist
    
    - [X] My code follows the style guidelines of this project
    
    - I have checked for/fixed spelling, linting, and other errors
    - I have commented my code for clarity
    - I have made corresponding changes to the documentation (we use
    [TypeDoc](https://typedoc.org/) to document our code)
    - My changes generate no new warnings
    - I have added tests that validates my changes, and provides sufficient
    test coverage. I have tested with:
      - Local testing
      - E2E testing in Teams
    - New and existing unit tests pass locally with my changes
    
    ### Additional information
    
    > Feel free to add other relevant information below

---
+++ dotnet/packages/Microsoft.TeamsAI/Microsoft.TeamsAI/AI/OpenAI/Models/Tool.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Microsoft.Teams.AI.AI.OpenAI.Models
+{
+    internal class Tool
+    {
+        [JsonPropertyName("type")]
+        public string Type { get; set; } = string.Empty;
+
+        [JsonPropertyName("function")]
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public Function? Function { get; set; }
+    }
+
+    internal class Function
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; } = string.Empty;
+
+        [JsonPropertyName("parameters")]
+        public Dictionary<string, object> Parameters { get; set; } = new Dictionary<string, object>();
+
+        [JsonPropertyName("description")]
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public string? Description { get; set; }
+    }
+}
