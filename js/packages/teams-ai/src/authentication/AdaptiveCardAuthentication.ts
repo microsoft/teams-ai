ce83194bb8eaa2bfb9de847fad42906c88bb5a2c
Kavin Singh <kavinsingh@microsoft.com>
2023-11-15 16:51:04 -0800

    adaptive card auth + sample

---
+++ js/packages/teams-ai/src/authentication/AdaptiveCardAuthentication.ts
@@ -0,0 +1,152 @@
+import { ActivityTypes, InvokeResponse, TokenResponse, TurnContext } from 'botbuilder';
+import * as UserTokenAccess from './UserTokenAccess';
+import { OAuthPromptSettings } from 'botbuilder-dialogs';
+import { AuthError, OAuthSettings } from './Authentication';
+import { ACTION_INVOKE_NAME } from '../AdaptiveCards';
+
+/**
+ * @internal
+ */
+interface AdaptiveCardLoginInRequest {
+    statusCode: number;
+    type: 'application/vnd.microsoft.activity.loginRequest';
+    value: {
+        text: string;
+        connectionName: string;
+        tokenExchangeResource?: {
+            id: string;
+            uri: string;
+            providerId?: string;
+        };
+        buttons: {
+            title: string;
+            text: string;
+            type: string;
+            value: string;
+        }[];
+    };
+}
+
+/**
+ * @internal
+ */
+export class AdaptiveCardAuthentication {
+    public async authenticate(context: TurnContext, settings: OAuthSettings): Promise<string | undefined> {
+        const authObj = context.activity.value.authentication;
+
+        // Token Exchange
+        if (authObj && authObj.token) {
+            // Message extension token exchange invoke activity
+            const isTokenExchangable = await this.isTokenExchangeable(context, settings);
+            if (!isTokenExchangable) {
+                await context.sendActivity({
+                    value: {
+                        body: {
+                            statusCode: 412,
+                            type: 'application/vnd.microsoft.error.preconditionFailed',
+                            value: {
+                                code: '412',
+                                message: 'Failed to exchange token'
+                            }
+                        },
+                        status: 200
+                    } as InvokeResponse,
+                    type: ActivityTypes.InvokeResponse
+                });
+
+                return undefined;
+            } else {
+                const tokenExchangeResponse = await this.exchangeToken(context, settings);
+                if (tokenExchangeResponse && tokenExchangeResponse.token) {
+                    return tokenExchangeResponse.token;
+                }
+            }
+        }
+
+        const value = context.activity.value;
+
+        // When the Bot Service Auth flow completes, the query.State will contain a magic code used for verification.
+        const magicCode = value.state && Number.isInteger(Number(value.state)) ? value.state : '';
+
+        const tokenResponse = await UserTokenAccess.getUserToken(context, settings, magicCode);
+
+        if (this.isValidActivity(context) && (!tokenResponse || !tokenResponse.token)) {
+            // There is no token, so the user has not signed in yet.
+            // Retrieve the OAuth sign in link to use in the card.
+
+            const signInResource = await UserTokenAccess.getSignInResource(context, settings);
+            const signInLink = signInResource.signInLink;
+
+            if (!signInLink) {
+                throw new AuthError('OAuthPrompt Authentication failed. No signin link found.');
+            }
+
+            const response: AdaptiveCardLoginInRequest = {
+                statusCode: 401,
+                type: 'application/vnd.microsoft.activity.loginRequest',
+                value: {
+                    text: 'Please sign in',
+                    connectionName: settings.connectionName,
+                    buttons: [
+                        {
+                            title: 'Sign-In',
+                            text: 'Sign-In',
+                            type: 'signin',
+                            value: signInLink
+                        }
+                    ]
+                }
+            };
+
+            if (settings.tokenExchangeUri) {
+                const botId = context.activity.recipient.id;
+                response.value.tokenExchangeResource = {
+                    id: botId,
+                    uri: settings.tokenExchangeUri
+                };
+            }
+
+            // Queue up invoke response
+            await context.sendActivity({
+                value: { body: response, status: 200 } as InvokeResponse,
+                type: ActivityTypes.InvokeResponse
+            });
+
+            return;
+        }
+
+        return tokenResponse.token;
+    }
+
+    public isValidActivity(context: TurnContext): boolean {
+        return context.activity.type == ActivityTypes.Invoke && context.activity.name == ACTION_INVOKE_NAME;
+    }
+
+    public async isTokenExchangeable(context: TurnContext, settings: OAuthPromptSettings): Promise<boolean> {
+        let tokenExchangeResponse;
+        try {
+            tokenExchangeResponse = await this.exchangeToken(context, settings);
+        } catch (err) {
+            // Ignore Exceptions
+            // If token exchange failed for any reason, tokenExchangeResponse above stays null, and hence we send back a failure invoke response to the caller.
+            console.log('tokenExchange error: ' + err);
+        }
+        if (!tokenExchangeResponse || !tokenExchangeResponse.token) {
+            return false;
+        }
+        return true;
+    }
+
+    public async exchangeToken(
+        context: TurnContext,
+        settings: OAuthPromptSettings
+    ): Promise<TokenResponse | undefined> {
+        const tokenExchangeRequest = context.activity.value.authentication;
+
+        if (!tokenExchangeRequest || !tokenExchangeRequest.token) {
+            return;
+        }
+
+        return await UserTokenAccess.exchangeToken(context, settings, tokenExchangeRequest);
+    }
+}
---
+++ js/samples/06.auth.adaptiveCardSSO/src/index.ts
@@ -0,0 +1,147 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+// Import required packages
+import { config } from 'dotenv';
+import * as path from 'path';
+import * as restify from 'restify';
+
+// Import required bot services.
+// See https://aka.ms/bot-services to learn more about the different parts of a bot.
+import {
+    ActivityTypes,
+    CardFactory,
+    CloudAdapter,
+    ConfigurationBotFrameworkAuthentication,
+    ConfigurationBotFrameworkAuthenticationOptions,
+    MemoryStorage,
+    MessagingExtensionAttachment,
+    MessagingExtensionResult,
+    TaskModuleTaskInfo,
+    TurnContext
+} from 'botbuilder';
+
+// Read botFilePath and botFileSecret from .env file.
+const ENV_FILE = path.join(__dirname, '..', '.env');
+config({ path: ENV_FILE });
+
+const botFrameworkAuthentication = new ConfigurationBotFrameworkAuthentication(
+    process.env as ConfigurationBotFrameworkAuthenticationOptions
+);
+
+// Create adapter.
+// See https://aka.ms/about-bot-adapter to learn more about how bots work.
+const adapter = new CloudAdapter(botFrameworkAuthentication);
+
+// Catch-all for errors.
+const onTurnErrorHandler = async (context: any, error: any) => {
+    // This check writes out errors to console log .vs. app insights.
+    // NOTE: In production environment, you should consider logging this to Azure
+    //       application insights.
+    console.error(`\n [onTurnError] unhandled error: ${error.toString()}`);
+
+    // Send a trace activity, which will be displayed in Bot Framework Emulator
+    await context.sendTraceActivity(
+        'OnTurnError Trace',
+        `${error.toString()}`,
+        'https://www.botframework.com/schemas/error',
+        'TurnError'
+    );
+
+    // Send a message to the user
+    await context.sendActivity('The bot encountered an error or bug.');
+    await context.sendActivity('To continue to run this bot, please fix the bot source code.');
+};
+
+// Set the onTurnError for the singleton CloudAdapter.
+adapter.onTurnError = onTurnErrorHandler;
+
+// Create HTTP server.
+const server = restify.createServer();
+server.use(restify.plugins.bodyParser());
+
+server.listen(process.env.port || process.env.PORT || 3978, () => {
+    console.log(`\n${server.name} listening to ${server.url}`);
+    console.log('\nGet Bot Framework Emulator: https://aka.ms/botframework-emulator');
+    console.log('\nTo test your bot in Teams, sideload the app manifest.json within Teams Apps.');
+});
+
+import { ApplicationBuilder, TurnState } from '@microsoft/teams-ai';
+import { createUserProfileCard, createViewProfileCard } from './cards';
+import { GraphClient } from './graphClient';
+
+// Define storage and application
+const storage = new MemoryStorage();
+const app = new ApplicationBuilder()
+    .withStorage(storage)
+    .withAuthentication(adapter, {
+        settings: {
+            graph: {
+                connectionName: process.env.ConnectionName ?? '',
+                title: 'Sign in',
+                text: 'Please sign in to use the bot.',
+                endOnInvalidMessage: true,
+                tokenExchangeUri: process.env.TokenExchangeUri ?? ''
+            }
+        },
+        autoSignIn: (context: TurnContext) => {
+            // Disable auto sign in for message activities
+            if (context.activity.type == ActivityTypes.Message) {
+                return Promise.resolve(false);
+            }
+            return Promise.resolve(true);
+        }
+    })
+    .build();
+
+// Handle message activities
+app.activity(ActivityTypes.Message, async (context: TurnContext, _state: TurnState) => {
+    const initialCard = createViewProfileCard();
+
+    await context.sendActivity({ attachments: [initialCard] });
+});
+
+// Handle sign in adaptive card button click
+app.adaptiveCards.actionExecute('signin', async (_context: TurnContext, state: TurnState) => {
+    const token = state.temp.authTokens['graph'];
+    if (!token) {
+        throw new Error('No auth token found in state. Authentication failed.');
+    }
+
+    const user = await getUserDetailsFromGraph(token);
+    const profileCard = createUserProfileCard(user.displayName, user.profilePhoto);
+
+    return profileCard.content;
+});
+
+// Handle sign out adaptive card button click
+app.adaptiveCards.actionExecute('signout', async (context: TurnContext, state: TurnState) => {
+    await app.authentication.signOutUser(context, state);
+
+    const initialCard = createViewProfileCard();
+
+    return initialCard.content;
+});
+
+/**
+ * Get the user details from Graph
+ * @param {string} token The token to use to get the user details
+ * @returns {Promise<{ displayName: string; profilePhoto: string }>} A promise that resolves to the user details
+ */
+async function getUserDetailsFromGraph(token: string): Promise<{ displayName: string; profilePhoto: string }> {
+    // The user is signed in, so use the token to create a Graph Clilent and show profile
+    const graphClient = new GraphClient(token);
+    const profile = await graphClient.GetMyProfile();
+    const profilePhoto = await graphClient.GetPhotoAsync();
+    return { displayName: profile.displayName, profilePhoto: profilePhoto };
+}
+
+// Listen for incoming server requests.
+server.post('/api/messages', async (req, res) => {
+    // Route received a request to adapter for processing
+    await adapter.process(req, res as any, async (context) => {
+        // Dispatch to application for routing
+        await app.run(context);
+    });
+});
