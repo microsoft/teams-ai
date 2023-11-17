6656d4f303d55e536538da796a90efb201ba77dc
Kuojian Lu <kuojianlu@gmail.com>
2023-11-18 02:37:41 +0800

    [C#] refactor: add OpenAIModel (#828)
    ## Linked issues
    
    closes: #723 
    
    ## Details
    
    #### Change details
    
    - BaseOpenAIModelOptions
    - OpenAIModelOptions
    - AzureOpenAIModelOptions
    - IPromptCompletionModel
    - OpenAIModel
    
    Followings are different from JS part:
    - use Azure.AI.OpenAI instead of http requests
    - remove requestConfig from BaseOpenAIModelOptions, but instead pass
    HttpClient in OpenAIModel constructor
    - remove endpoint from OpenAIModelOptions because it is not supported by
    Azure.AI.OpenAI
    
    TODOs in PRs afterwards:
    - support logging colorized contents
    - unit test
    
    ## Attestation Checklist
    
    - [ ] My code follows the style guidelines of this project
    
    - I have checked for/fixed spelling, linting, and other errors
    - I have commented my code for clarity
    - I have made corresponding changes to the documentation (we use
    [TypeDoc](https://typedoc.org/) to document our code)
    - My changes generate no new warnings
    - I have added tests that validates my changes, and provides sufficient
    test coverage. I have tested with:
      - Local testing
      - E2E testing in Teams
    - New and existing unit tests pass locally with my changes
    
    ### Additional information
    
    > Feel free to add other relevant information below

---
+++ dotnet/packages/Microsoft.TeamsAI/Microsoft.TeamsAI/AI/Models/AddHeaderRequestPolicy.cs
@@ -0,0 +1,25 @@
+﻿using Azure.Core.Pipeline;
+using Azure.Core;
+
+namespace Microsoft.Teams.AI.AI.Models
+{
+    /// <summary>
+    /// Helper class to inject headers into Azure SDK HTTP pipeline.
+    /// </summary>
+    internal class AddHeaderRequestPolicy : HttpPipelineSynchronousPolicy
+    {
+        private readonly string _headerName;
+        private readonly string _headerValue;
+
+        public AddHeaderRequestPolicy(string headerName, string headerValue)
+        {
+            this._headerName = headerName;
+            this._headerValue = headerValue;
+        }
+
+        public override void OnSendingRequest(HttpMessage message)
+        {
+            message.Request.Headers.Add(this._headerName, this._headerValue);
+        }
+    }
+}
---
+++ dotnet/packages/Microsoft.TeamsAI/Microsoft.TeamsAI/AI/Models/OpenAIModelOptions.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Teams.AI.Utilities;
+using static Microsoft.Teams.AI.AI.Prompts.PromptTemplate.PromptTemplateConfiguration.CompletionConfiguration;
+
+namespace Microsoft.Teams.AI.AI.Models
+{
+    /// <summary>
+    /// Options for configuring an `OpenAIModel` to call an OpenAI hosted model.
+    /// </summary>
+    public class OpenAIModelOptions : BaseOpenAIModelOptions
+    {
+        /// <summary>
+        /// API key to use when calling the OpenAI API.
+        /// </summary>
+        /// <remarks>
+        /// A new API key can be created at https://platform.openai.com/account/api-keys.
+        /// </remarks>
+        public string ApiKey { get; set; }
+
+        /// <summary>
+        /// Default model to use for completion.
+        /// </summary>
+        /// <remarks>
+        /// For Azure OpenAI this is the name of the deployment to use.
+        /// </remarks>
+        public string DefaultModel { get; set; }
+
+        /// <summary>
+        /// Optional. Organization to use when calling the OpenAI API.
+        /// </summary>
+        public string? Organization { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OpenAIModelOptions"/> class.
+        /// </summary>
+        /// <param name="apiKey">API key to use when calling the OpenAI API.</param>
+        /// <param name="defaultModel">Default model to use for completion.</param>
+        /// <param name="organization">Organization to use when calling the OpenAI API.</param>
+        /// <param name="completionType">Type of completion to use for the default model.</param>
+        /// <param name="logRequests">Whether to log requests to the console.</param>
+        /// <param name="retryPolicy">Retry policy to use when calling the OpenAI API.</param>
+        /// <param name="useSystemMessages"></param>
+        public OpenAIModelOptions(
+            string apiKey,
+            string defaultModel,
+            string? organization = null,
+            CompletionType completionType = CompletionType.Chat,
+            bool logRequests = false,
+            List<int>? retryPolicy = null,
+            bool useSystemMessages = false) : base(completionType, logRequests, retryPolicy, useSystemMessages)
+        {
+            Verify.ParamNotNull(apiKey);
+            Verify.ParamNotNull(defaultModel);
+
+            this.ApiKey = apiKey;
+            this.DefaultModel = defaultModel;
+            this.Organization = organization;
+        }
+    }
+}
