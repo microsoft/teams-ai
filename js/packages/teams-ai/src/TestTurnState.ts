3301ccd057685e16cb93b2d0afc91d0358613b49
Steven Ickman <stevenic@microsoft.com>
2023-11-17 09:41:31 -0800

    Updated turn state docs

--- js/packages/teams-ai/src/TestTurnState.ts
+++ js/packages/teams-ai/src/TestTurnState.ts
@@ -1,11 +1,23 @@
 import { TurnState } from "./TurnState";
 import { MemoryStorage, TurnContext } from "botbuilder";
 
+/**
+ * A test version of the TurnState class used by unit tests.
+ */
 export class TestTurnState extends TurnState {
+    /**
+     * @private
+     */
     private constructor() {
         super();
     }
 
+    /**
+     * Creates a new `TestTurnState` instance.
+     * @param context Context for the current turn of conversation with the user.
+     * @param testState Optional. State to initialize the new instance with.
+     * @returns Created `TestTurnState` instance.
+     */
     public static async create(context: TurnContext, testState?: {user?: Record<string, any>; conversation?: Record<string, any>; temp?: Record<string, any>;}): Promise<TestTurnState> {
         const storage = new MemoryStorage();
         const state = new TestTurnState();
