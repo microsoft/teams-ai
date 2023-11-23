9c6cb0600a71a85898588e911013a32f1e386adc
Qianhao Dong <qidon@microsoft.com>
2023-11-23 10:36:08 +0800

    feat: math bot

--- dotnet/samples/06.assistants.a.mathBot/Program.cs
+++ dotnet/samples/06.assistants.a.mathBot/Program.cs
@@ -3,10 +3,7 @@ using Microsoft.Bot.Builder.Integration.AspNet.Core;
 using Microsoft.Bot.Connector.Authentication;
 using Microsoft.Teams.AI;
 using Microsoft.Teams.AI.AI;
-using Microsoft.Teams.AI.AI.Moderator;
-using Microsoft.Teams.AI.AI.Planner;
-using Microsoft.Teams.AI.AI.Prompt;
-using Microsoft.Teams.AI.State;
+using Microsoft.Teams.AI.AI.Planners;
 using MathBot;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -15,8 +12,37 @@ builder.Services.AddControllers();
 builder.Services.AddHttpClient("WebClient", client => client.Timeout = TimeSpan.FromSeconds(600));
 builder.Services.AddHttpContextAccessor();
 
-// Prepare Configuration for ConfigurationBotFrameworkAuthentication
+// Load configuration
 var config = builder.Configuration.Get<ConfigOptions>()!;
+if (config.OpenAI == null || string.IsNullOrEmpty(config.OpenAI.ApiKey))
+{
+    throw new Exception("Missing OpenAI configuration.");
+}
+
+// Missing Assistant ID, create new assistant
+if (string.IsNullOrEmpty(config.OpenAI.AssistantId))
+{
+    string newAssistantId = AssistantsPlanner<AssistantsState>.CreateAssistantAsync(config.OpenAI.ApiKey, null, new()
+    {
+        Name = "Math Tutor",
+        Instructions = "You are a personal math tutor. Write and run code to answer math questions.",
+        Tools = new()
+        {
+            new()
+            {
+                Type = "code_interpreter"
+            }
+        },
+        Model = "gpt-4-1106-preview"
+    }).Result.Id;
+    Console.WriteLine($"Created a new assistant with an ID of: ${newAssistantId}");
+    Console.WriteLine("Copy and save above ID, and set `OpenAI:AssistantId` in appsettings.Development.json.");
+    Console.WriteLine("Press any key to exit.");
+    Console.ReadLine();
+    Environment.Exit(0);
+}
+
+// Prepare Configuration for ConfigurationBotFrameworkAuthentication
 builder.Configuration["MicrosoftAppType"] = "MultiTenant";
 builder.Configuration["MicrosoftAppId"] = config.BOT_ID;
 builder.Configuration["MicrosoftAppPassword"] = config.BOT_PASSWORD;
@@ -32,49 +58,27 @@ builder.Services.AddSingleton<IBotFrameworkHttpAdapter>(sp => sp.GetService<Clou
 builder.Services.AddSingleton<BotAdapter>(sp => sp.GetService<CloudAdapter>()!);
 
 builder.Services.AddSingleton<IStorage, MemoryStorage>();
-
-if (config.OpenAI == null || string.IsNullOrEmpty(config.OpenAI.ApiKey))
-{
-    throw new Exception("Missing OpenAI configuration.");
-}
-
-builder.Services.AddSingleton(_ => new OpenAIPlannerOptions(config.OpenAI.ApiKey, "text-davinci-003")
-{
-    LogRequests = true
-});
-builder.Services.AddSingleton(_ => new OpenAIModeratorOptions(config.OpenAI.ApiKey, ModerationType.Both));
+builder.Services.AddSingleton(_ => new AssistantsPlannerOptions(config.OpenAI.ApiKey, config.OpenAI.AssistantId));
 
 // Create the Application.
 builder.Services.AddTransient<IBot>(sp =>
 {
     ILoggerFactory loggerFactory = sp.GetService<ILoggerFactory>()!;
-
-    IPromptManager<TurnState> promptManager = new PromptManager<TurnState>("./Prompts");
-    IPlanner<TurnState> planner = new OpenAIPlanner<TurnState>(sp.GetService<OpenAIPlannerOptions>()!, loggerFactory);
-    IModerator<TurnState> moderator = new OpenAIModerator<TurnState>(sp.GetService<OpenAIModeratorOptions>()!, loggerFactory);
-
-    ApplicationOptions<TurnState, TurnStateManager> applicationOptions = new()
+    IPlanner<AssistantsState> planner = new AssistantsPlanner<AssistantsState>(sp.GetService<AssistantsPlannerOptions>()!, loggerFactory);
+    ApplicationOptions<AssistantsState> applicationOptions = new()
     {
-        AI = new AIOptions<TurnState>(planner, promptManager)
-        {
-            Moderator = moderator,
-            Prompt = "Chat",
-            History = new AIHistoryOptions()
-            {
-                AssistantHistoryType = AssistantHistoryType.Text
-            }
-        },
+        AI = new AIOptions<AssistantsState>(planner),
         Storage = sp.GetService<IStorage>(),
         LoggerFactory = loggerFactory
     };
 
-    Application<TurnState, TurnStateManager> app = new(applicationOptions);
+    Application<AssistantsState> app = new(applicationOptions);
 
     // Register AI actions
     app.AI.ImportActions(new ActionHandlers());
 
-    // Listen for user to say "/history".
-    app.OnMessage("/history", ActivityHandlers.HistoryMessageHandler);
+    // Listen for user to say "/reset".
+    app.OnMessage("/reset", ActivityHandlers.ResetMessageHandler);
 
     return app;
 });
