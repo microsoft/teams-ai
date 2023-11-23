9337838fd0792a321a3420bb6b9d64bfad7c30c8
Chaoyi Yuan <chyuan@microsoft.com>
2023-11-23 10:09:07 +0800

    feat: define a new SelectorAsync delegate for auth options to avoid confusion

--- dotnet/packages/Microsoft.TeamsAI/Microsoft.TeamsAI/Application/Authentication/AuthenticationOptions.cs
+++ dotnet/packages/Microsoft.TeamsAI/Microsoft.TeamsAI/Application/Authentication/AuthenticationOptions.cs
@@ -1,5 +1,17 @@
-﻿namespace Microsoft.Teams.AI.Application.Authentication
+﻿using Microsoft.Bot.Builder;
+
+namespace Microsoft.Teams.AI.Application.Authentication
 {
+
+    /// <summary>
+    /// Function for determining whether authentication should be enabled for an activity.
+    /// </summary>
+    /// <param name="turnContext">Context for the current turn of conversation with the user.</param>
+    /// <param name="cancellationToken">A cancellation token that can be used by other objects
+    /// or threads to receive notice of cancellation.</param>
+    /// <returns>True if authentication should be enabled. Otherwise, False.</returns>
+    public delegate Task<bool> SelectorAsync(ITurnContext turnContext, CancellationToken cancellationToken);
+
     /// <summary>
     /// Options for authentication.
     /// </summary>
@@ -20,7 +32,7 @@
         /// Indicates whether the bot should start the sign in flow when the user sends a message to the bot or triggers a message extension.
         /// If the selector returns false, the bot will not start the sign in flow before routing the activity to the bot logic.
         /// </summary>
-        public RouteSelector? AutoSignIn { get; set; }
+        public SelectorAsync? AutoSignIn { get; set; }
 
         /// <summary>
         /// Initializes a new instance of the <see cref="AuthenticationOptions"/> class.
@@ -28,7 +40,7 @@
         /// <param name="authentications">The authentication classes to sign-in and sign-out users.</param>
         /// <param name="default">Describes the authentication class the bot should use if the user does not specify a authentication class name.</param>
         /// <param name="autoSignIn">Indicates whether the bot should start the sign in flow when the user sends a message to the bot or triggers a message extension.</param>
-        public AuthenticationOptions(Dictionary<string, IAuthentication> authentications, string? @default, RouteSelector? autoSignIn)
+        public AuthenticationOptions(Dictionary<string, IAuthentication> authentications, string? @default, SelectorAsync? autoSignIn)
         {
             this.Authentications = authentications;
             this.Default = @default;
