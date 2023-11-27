be1459b288e6b7ce1366d2adf51e1fad9883b0b2
Kuojian Lu <kuojianlu@gmail.com>
2023-11-28 07:24:38 +0800

    [C#] refactor: update devops sample (#900)
    ## Linked issues
    
    closes: #898 
    
    ## Details
    
    Provide a list of your changes here. If you are fixing a bug, please
    provide steps to reproduce the bug.
    
    #### Change details
    
    > Describe your changes, with screenshots and code snippets as
    appropriate
    
    **code snippets**:
    
    **screenshots**:
    
    ## Attestation Checklist
    
    - [x] My code follows the style guidelines of this project
    
    - I have checked for/fixed spelling, linting, and other errors
    - I have commented my code for clarity
    - I have made corresponding changes to the documentation (we use
    [TypeDoc](https://typedoc.org/) to document our code)
    - My changes generate no new warnings
    - I have added tests that validates my changes, and provides sufficient
    test coverage. I have tested with:
      - Local testing
      - E2E testing in Teams
    - New and existing unit tests pass locally with my changes
    
    ### Additional information
    
    > Feel free to add other relevant information below

---
+++ dotnet/samples/04.ai.e.chainedActions.devOpsBot/Model/UserUpdate.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace DevOpsBot.Model
+{
+    /// <summary>
+    /// The strongly typed user update parameter data
+    /// </summary>
+    public class UserUpdate
+    {
+        [JsonPropertyName("added")]
+        public string[]? Added { get; set; } = Array.Empty<string>();
+
+        [JsonPropertyName("removed")]
+        public string[]? Removed { get; set; } = Array.Empty<string>();
+    }
+}
