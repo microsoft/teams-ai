a3e267bad7f99fdebb55628c1d292dcc72505c5a
Kavin Singh <kavinsingh@microsoft.com>
2023-11-29 12:33:37 -0800

    fix twenty questions bot

--- dotnet/samples/04.e.twentyQuestions/GameStateManager.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.Teams.AI.State;
-
-namespace TwentyQuestions
-{
-    /// <summary>
-    /// Shorter the class name since turn state is strongly typed.
-    /// </summary>
-    public class GameStateManager : TurnStateManager<GameState, ConversationState, StateBase, TempState> { }
-}
