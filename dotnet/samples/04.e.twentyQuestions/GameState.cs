035e49e296310b5284d4552c1c055e3ed4c37119
singhk97 <115390646+singhk97@users.noreply.github.com>
2023-11-29 15:52:00 -0800

    [C#] chore: fixed dotnet samples (#933)
    ## Linked issues
    
    closes: #875 #881 #923
    
    ## Details
    
    _Teams chef bot_ and _Twenty questions bot_ samples were broken. This PR
    updates them to consume the latest version of the SDK (i.e most recent
    commit to `breaking-changes-2`).
    
    ## Attestation Checklist
    
    - [x] My code follows the style guidelines of this project
    
    - I have checked for/fixed spelling, linting, and other errors
    - I have commented my code for clarity
    - I have made corresponding changes to the documentation (we use
    [TypeDoc](https://typedoc.org/) to document our code)
    - My changes generate no new warnings
    - I have added tests that validates my changes, and provides sufficient
    test coverage. I have tested with:
      - Local testing
      - E2E testing in Teams
    - New and existing unit tests pass locally with my changes

--- dotnet/samples/04.e.twentyQuestions/GameState.cs
+++ dotnet/samples/04.e.twentyQuestions/GameState.cs
@@ -5,18 +5,53 @@ namespace TwentyQuestions
     /// <summary>
     /// Extend the turn state by configuring custom strongly typed state classes.
     /// </summary>
-    public class GameState : TurnState<ConversationState, StateBase, TempState>
+    public class GameState : TurnState
     {
+        public GameState()
+        {
+            ScopeDefaults[CONVERSATION_SCOPE] = new ConversationState();
+        }
+
+        /// <summary>
+        /// Stores all the conversation-related state.
+        /// </summary>
+        public new ConversationState Conversation
+        {
+            get
+            {
+                TurnStateEntry? scope = GetScope(CONVERSATION_SCOPE);
+
+                if (scope == null)
+                {
+                    throw new ArgumentException("TurnState hasn't been loaded. Call LoadStateAsync() first.");
+                }
+
+                return (ConversationState)scope.Value!;
+            }
+            set
+            {
+                TurnStateEntry? scope = GetScope(CONVERSATION_SCOPE);
+
+                if (scope == null)
+                {
+                    throw new ArgumentException("TurnState hasn't been loaded. Call LoadStateAsync() first.");
+                }
+
+                scope.Replace(value!);
+            }
+        }
     }
 
     /// <summary>
     /// This class adds custom properties to the turn state which will be accessible in the activity handler methods.
     /// </summary>
-    public class ConversationState : StateBase
+    public class ConversationState : Record
     {
-        private const string _secretWordKey = "secretWordKey";
-        private const string _guessCountKey = "guessCountKey";
-        private const string _remainingGuessesKey = "remainingGuessesKey";
+        // The keys can be referenced in the prompt using the ${{conversation.<key>}} syntax.
+        // For example, ${{conversation.secretWord}}
+        private const string _secretWordKey = "secretWord";
+        private const string _guessCountKey = "guessCount";
+        private const string _remainingGuessesKey = "remainingGuesses";
 
         public string? SecretWord
         {
--- dotnet/samples/04.e.twentyQuestions/Program.cs
+++ dotnet/samples/04.e.twentyQuestions/Program.cs
@@ -3,10 +3,9 @@ using Microsoft.Bot.Builder.Integration.AspNet.Core;
 using Microsoft.Bot.Connector.Authentication;
 using Microsoft.Bot.Schema;
 using Microsoft.Teams.AI;
-using Microsoft.Teams.AI.AI;
-using Microsoft.Teams.AI.AI.Moderator;
-using Microsoft.Teams.AI.AI.Planner;
-using Microsoft.Teams.AI.AI.Prompt;
+using Microsoft.Teams.AI.AI.Models;
+using Microsoft.Teams.AI.AI.Planners;
+using Microsoft.Teams.AI.AI.Prompts;
 using TwentyQuestions;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -34,18 +33,25 @@ builder.Services.AddSingleton<BotAdapter>(sp => sp.GetService<CloudAdapter>()!);
 // Create singleton instances for bot application
 builder.Services.AddSingleton<IStorage, MemoryStorage>();
 
-#region Use Azure OpenAI and Azure Content Safety
-// Following code is for using Azure OpenAI and Azure Content Safety
-if (config.Azure == null
-    || string.IsNullOrEmpty(config.Azure.OpenAIApiKey)
-    || string.IsNullOrEmpty(config.Azure.OpenAIEndpoint)
-    || string.IsNullOrEmpty(config.Azure.ContentSafetyApiKey)
-    || string.IsNullOrEmpty(config.Azure.ContentSafetyEndpoint))
+OpenAIModel? model = null;
+
+if (!string.IsNullOrEmpty(config.OpenAI?.ApiKey))
 {
-    throw new Exception("Missing Azure configuration.");
+    model = new(new OpenAIModelOptions(config.OpenAI.ApiKey, "gpt-3.5-turbo"));
 }
-builder.Services.AddSingleton<AzureOpenAIPlannerOptions>(_ => new(config.Azure.OpenAIApiKey, "text-davinci-003", config.Azure.OpenAIEndpoint));
-builder.Services.AddSingleton<AzureContentSafetyModeratorOptions>(_ => new(config.Azure.ContentSafetyApiKey, config.Azure.ContentSafetyEndpoint, ModerationType.Both));
+else if (!string.IsNullOrEmpty(config.Azure?.OpenAIApiKey) && !string.IsNullOrEmpty(config.Azure.OpenAIEndpoint))
+{
+    model = new(new AzureOpenAIModelOptions(
+        config.Azure.OpenAIApiKey,
+        "gpt-35-turbo",
+        config.Azure.OpenAIEndpoint
+    ));
+}
+
+if (model == null)
+{
+    throw new Exception("please configure settings for either OpenAI or Azure");
+}
 
 // Create the bot as a transient. In this case the ASP Controller is expecting an IBot.
 builder.Services.AddTransient<IBot>(sp =>
@@ -53,29 +59,31 @@ builder.Services.AddTransient<IBot>(sp =>
     // Create loggers
     ILoggerFactory loggerFactory = sp.GetService<ILoggerFactory>()!;
 
-    // Create AzureOpenAIPlanner
-    IPlanner<GameState> planner = new AzureOpenAIPlanner<GameState>(
-        sp.GetService<AzureOpenAIPlannerOptions>()!,
-        loggerFactory);
-
-    // Create AzureContentSafetyModerator
-    IModerator<GameState> moderator = new AzureContentSafetyModerator<GameState>(sp.GetService<AzureContentSafetyModeratorOptions>()!);
-
-    // Setup Application
-    AIHistoryOptions aiHistoryOptions = new()
+    // Create Prompt Manager
+    PromptManager prompts = new(new()
     {
-        AssistantHistoryType = AssistantHistoryType.Text
-    };
-    AIOptions<GameState> aiOptions = new(
-        planner: planner,
-        promptManager: new PromptManager<GameState>("./Prompts"),
-        moderator: moderator,
-        prompt: "Chat",
-        history: aiHistoryOptions);
-    var applicationBuilder = new ApplicationBuilder<GameState, GameStateManager>()
-    .WithAIOptions(aiOptions)
+        PromptFolder = "./Prompts"
+    });
+
+    // Create ActionPlanner
+    ActionPlanner<GameState> planner = new(
+        options: new(
+            model: model,
+            prompts: prompts,
+            defaultPrompt: async (context, state, planner) =>
+            {
+                PromptTemplate template = prompts.GetPrompt("sequence");
+                return await Task.FromResult(template);
+            }
+        )
+        { LogRepairs = true },
+        loggerFactory: loggerFactory
+    );
+
+    var applicationBuilder = new ApplicationBuilder<GameState>()
+    .WithAIOptions(new(planner))
     .WithLoggerFactory(loggerFactory)
-    .WithTurnStateManager(new GameStateManager());
+    .WithTurnStateFactory(() => new GameState());
 
     // Set storage options
     IStorage? storage = sp.GetService<IStorage>();
@@ -85,81 +93,15 @@ builder.Services.AddTransient<IBot>(sp =>
     }
 
     // Create Application
-    Application<GameState, GameStateManager> app = applicationBuilder.Build();
+    Application<GameState> app = applicationBuilder.Build();
 
-    GameBotHandlers handlers = new(app);
-
-    // register turn and activity handlers
-    app.OnActivity(ActivityTypes.Message, handlers.OnMessageActivityAsync);
-
-    return app;
-});
-#endregion
-
-#region Use OpenAI
-/** // Use OpenAI
-if (config.OpenAI == null || string.IsNullOrEmpty(config.OpenAI.ApiKey))
-{
-    throw new Exception("Missing OpenAI configuration.");
-}
-builder.Services.AddSingleton<OpenAIPlannerOptions>(_ => new(config.OpenAI.ApiKey, "text-davinci-003"));
-builder.Services.AddSingleton<OpenAIModeratorOptions>(_ => new(config.OpenAI.ApiKey, ModerationType.Both));
-
-// Create the bot as a transient. In this case the ASP Controller is expecting an IBot.
-builder.Services.AddTransient<IBot>(sp =>
-{
-    // Create loggers
-    ILoggerFactory loggerFactory = sp.GetService<ILoggerFactory>()!;
-
-    // Get HttpClient
-    HttpClient moderatorHttpClient = sp.GetService<IHttpClientFactory>()!.CreateClient("WebClient");
-
-    // Create OpenAIPlanner
-    IPlanner<GameState> planner = new OpenAIPlanner<GameState>(
-        sp.GetService<OpenAIPlannerOptions>()!,
-        loggerFactory);
-
-    // Create OpenAIModerator
-    IModerator<GameState> moderator = new OpenAIModerator<GameState>(
-        sp.GetService<OpenAIModeratorOptions>()!,
-        loggerFactory,
-        moderatorHttpClient);
-
-    // Setup Application
-    AIHistoryOptions aiHistoryOptions = new()
-    {
-        AssistantHistoryType = AssistantHistoryType.Text
-    };
-    AIOptions<GameState> aiOptions = new(
-        planner: planner,
-        promptManager: new PromptManager<GameState>("./Prompts"),
-        moderator: moderator,
-        prompt: "Chat",
-        history: aiHistoryOptions);
-    var applicationBuilder = new ApplicationBuilder<GameState, GameStateManager>()
-    .WithAIOptions(aiOptions)
-    .WithLoggerFactory(loggerFactory)
-    .WithTurnStateManager(new GameStateManager());
-
-    // Set storage options
-    IStorage? storage = sp.GetService<IStorage>();
-    if (storage != null)
-    {
-        applicationBuilder.WithStorage(storage);
-    }
-
-    // Create Application
-    Application<GameState, GameStateManager> app = applicationBuilder.Build();
-
-    GameBotHandlers handlers = new(app);
+    GameBotHandlers handlers = new(planner);
 
     // register turn and activity handlers
     app.OnActivity(ActivityTypes.Message, handlers.OnMessageActivityAsync);
 
     return app;
 });
-**/
-#endregion
 
 var app = builder.Build();
 
