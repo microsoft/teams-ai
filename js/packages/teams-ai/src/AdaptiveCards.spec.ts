48726f27c0cc7e2bb3548d06e21fcaba2f1bf878
Kavin Singh <kavinsingh@microsoft.com>
2023-12-08 16:36:20 -0800

    search tests

--- js/packages/teams-ai/src/AdaptiveCards.spec.ts
+++ js/packages/teams-ai/src/AdaptiveCards.spec.ts
@@ -1,7 +1,7 @@
 import { ActivityTypes, InvokeResponse, TestAdapter, TurnContext } from 'botbuilder';
-import { Application, RouteSelector } from './Application';
+import { Application, Query, RouteSelector } from './Application';
 import * as sinon from 'sinon';
-import { AdaptiveCard, AdaptiveCards, TurnState } from '.';
+import { AdaptiveCard, AdaptiveCardSearchResult, AdaptiveCards, AdaptiveCardsSearchParams, TurnState } from '.';
 import assert from 'assert';
 
 describe.only('AdaptiveCards', () => {
@@ -269,7 +269,7 @@ describe.only('AdaptiveCards', () => {
         });
     });
 
-    describe.only('actionSubmit()', () => {
+    describe('actionSubmit()', () => {
         const testVerb = 'testVerb';
         const testHandler = (context: TurnContext, state: TurnState, data: Record<string, any>) => Promise.resolve();
 
@@ -343,7 +343,7 @@ describe.only('AdaptiveCards', () => {
                     adaptiveCards.actionSubmit(verb, testHandler);
                 });
 
-                it('activity is valid invoke type and verb matches', async () => {
+                it('activity is valid action submit type and verb matches', async () => {
                     // a valid action submit type is a message activity with a value property and no text.
                     const activity = {
                         type: 'message',
@@ -358,7 +358,7 @@ describe.only('AdaptiveCards', () => {
                     assert((await selector(context)) == true);
                 });
 
-                it('activity is valid invoke type and verbs are not equal', async () => {
+                it('activity is valid action submit type and verbs are not equal', async () => {
                     // a valid action submit type is a message activity with a value property.
                     const activity = {
                         type: 'message',
@@ -427,6 +427,229 @@ describe.only('AdaptiveCards', () => {
 
                 assert(testHandlerStub.calledOnce);
                 assert(testHandlerStub.calledWith(context, state, activity.value));
+            });
+        });
+    });
+
+    describe('search()', () => {
+        const testDataset = 'testDataSet';
+        const testHandler = (
+            context: TurnContext,
+            state: TurnState,
+            query: Query<AdaptiveCardsSearchParams>
+        ): Promise<AdaptiveCardSearchResult[]> =>
+            Promise.resolve([
+                {
+                    title: 'title',
+                    value: 'value'
+                }
+            ]);
+
+        it('should call application addRoute', () => {
+            adaptiveCards.search(testDataset, testHandler);
+
+            assert(addRouteStub.calledOnce);
+        });
+
+        describe('createSearchSelector()', () => {
+            it('dataset is a function', () => {
+                const dataset: RouteSelector = () => Promise.resolve(true);
+
+                adaptiveCards.search(dataset, testHandler);
+
+                assert(selector === dataset);
+            });
+
+            describe('dataset is a regular expression', () => {
+                const datasetRegex = new RegExp('test');
+
+                beforeEach(() => {
+                    adaptiveCards.search(datasetRegex, testHandler);
+                });
+
+                it('incomming activity is valid application/search type and should match regex', async () => {
+                    const activity = {
+                        type: 'invoke',
+                        name: 'application/search',
+                        value: {
+                            dataset: 'test'
+                        }
+                    };
+                    const context = createTurnContext(activity);
+
+                    // the selector should be testing if `value.dataset` matches the regex `datasetRegex`.
+                    assert((await selector(context)) == true);
+                });
+
+                it('incomming activity is valid application/search type and should not match regex ', async () => {
+                    const activity = {
+                        type: 'invoke',
+                        name: 'application/search',
+                        value: {
+                            dataset: 'DoesNotMatchRegex'
+                        }
+                    };
+                    const context = createTurnContext(activity);
+
+                    // the selector should be testing if `value.dataset` matches the regex `datasetRegex`.
+                    assert((await selector(context)) == false);
+                });
+
+                it('incomming activity is invalid', async () => {
+                    const activity = {
+                        type: 'NotInvoke'
+                    };
+
+                    const context = createTurnContext(activity);
+
+                    assert((await selector(context)) == false);
+                });
+            });
+
+            describe('dataset is a string', () => {
+                const dataset = 'test';
+
+                beforeEach(() => {
+                    adaptiveCards.search(dataset, testHandler);
+                });
+
+                it('activity is valid application/search type and dataset matches', async () => {
+                    const activity = {
+                        type: 'invoke',
+                        name: 'application/search',
+                        value: {
+                            dataset: 'test'
+                        }
+                    };
+                    const context = createTurnContext(activity);
+
+                    assert((await selector(context)) == true);
+                });
+
+                it('activity is valid application/search type and datasets are not equal', async () => {
+                    const activity = {
+                        type: 'invoke',
+                        name: 'application/search',
+                        value: {
+                            dataset: 'test'
+                        }
+                    };
+                    const context = createTurnContext(activity);
+
+                    assert((await selector(context)) == false);
+                });
+
+                it('activity is invalid', async () => {
+                    const activity = {
+                        type: 'NotApplicationSearch'
+                    };
+
+                    const context = createTurnContext(activity);
+
+                    assert((await selector(context)) == false);
+                });
+            });
+        });
+
+        describe('handler creation logic', () => {
+            it('should create handler', () => {
+                adaptiveCards.search(testDataset, testHandler);
+
+                assert(typeof handler === 'function');
+            });
+
+            it('should throw error if incomming activity is not valid', async () => {
+                adaptiveCards.search(testDataset, testHandler);
+
+                const activity = {
+                    type: 'invalidActivityType'
+                };
+
+                const context = createTurnContext(activity);
+                const state = new TurnState();
+                const errorMsg = `Unexpected AdaptiveCards.search() triggered for activity type: invalidActivityType`;
+
+                assert.rejects(async () => await handler(context, state), errorMsg);
+            });
+
+            it('should call the test handler with the correct parameters', async () => {
+                const testHandlerStub = sinon.spy(testHandler);
+
+                adaptiveCards.search(testDataset, testHandlerStub);
+
+                const query = {
+                    count: 1,
+                    skip: 1,
+                    parameters: {
+                        queryText: 'queryText',
+                        dataset: 'testDataSet'
+                    }
+                };
+
+                const activity = {
+                    type: 'invoke',
+                    name: 'application/search',
+                    value: {
+                        queryOptions: {
+                            top: query.count,
+                            skip: query.skip
+                        },
+                        queryText: query.parameters.queryText,
+                        dataset: query.parameters.dataset
+                    }
+                };
+
+                const context = createTurnContext(activity);
+                const state = new TurnState();
+
+                // this is the handler that is registered as an app route.
+                await handler(context, state);
+
+                assert(testHandlerStub.calledOnce);
+                assert(testHandlerStub.calledWith(context, state, query));
+            });
+
+            it('should send an invoke search response with handler result.', async () => {
+                const returnedSearchResult = [
+                    {
+                        title: 'title',
+                        value: 'value'
+                    }
+                ];
+
+                const testHandler = (
+                    context: TurnContext,
+                    state: TurnState,
+                    query: Query<AdaptiveCardsSearchParams>
+                ): Promise<AdaptiveCardSearchResult[]> => Promise.resolve(returnedSearchResult);
+
+                adaptiveCards.search(testDataset, testHandler);
+
+                const activity = {
+                    type: 'invoke',
+                    name: 'application/search'
+                };
+
+                const context = createTurnContext(activity);
+                const contextSendActivityStub = sinon.stub(context, 'sendActivity');
+                const state = new TurnState();
+
+                // this is the handler that is registered as an app route.
+                await handler(context, state);
+
+                const response = {
+                    type: 'application/vnd.microsoft.search.searchResponse',
+                    value: {
+                        results: returnedSearchResult
+                    }
+                };
+
+                assert(
+                    contextSendActivityStub.calledOnceWith({
+                        value: { body: response, status: 200 } as InvokeResponse,
+                        type: ActivityTypes.InvokeResponse
+                    })
+                );
             });
         });
     });
