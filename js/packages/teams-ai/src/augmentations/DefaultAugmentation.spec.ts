5ed6ac919a52b7c9ad0c3e07db94aa385eeeee9c
Lily Du <lilydu@microsoft.com>
2023-12-13 13:27:21 -0800

    [JS] chore: DefaultAugmentation and SequenceAugmentation unit tests  (#1056)
    ## Linked issues
    
    closes: #54 
    
    ## Details
    
    Unit tests added for:
    
    - [x]  SequenceAugmentation.ts
    - [x]  DefaultAugmentation.ts
    
    ## Attestation Checklist
    
    - [x] My code follows the style guidelines of this project
    
    - I have checked for/fixed spelling, linting, and other errors
    - I have commented my code for clarity
    - I have made corresponding changes to the documentation (we use
    [TypeDoc](https://typedoc.org/) to document our code)
    - My changes generate no new warnings
    - I have added tests that validates my changes, and provides sufficient
    test coverage. I have tested with:
      - Local testing
      - E2E testing in Teams
    - New and existing unit tests pass locally with my changes

---
+++ js/packages/teams-ai/src/augmentations/DefaultAugmentation.spec.ts
@@ -0,0 +1,102 @@
+import assert from 'assert';
+import { DefaultAugmentation } from './DefaultAugmentation';
+import { TestAdapter } from 'botbuilder';
+import { TestTurnState } from '../TestTurnState';
+import { GPT3Tokenizer } from '../tokenizers';
+import { PromptResponse } from '../models';
+
+describe('DefaultAugmentation', () => {
+    const adapter = new TestAdapter();
+    const tokenizer = new GPT3Tokenizer();
+    const valid_content = JSON.stringify({
+        type: 'plan',
+        commands: [
+            {
+                type: 'DO',
+                action: 'test',
+                parameters: {
+                    foo: 'bar'
+                }
+            }
+        ]
+    });
+    const valid_SAY = JSON.stringify({
+        type: 'plan',
+        commands: [
+            {
+                type: 'SAY',
+                response: 'hello world'
+            }
+        ]
+    });
+
+    describe('constructor', () => {
+        it('should create a DefaultAugmentation', () => {
+            const augmentation = new DefaultAugmentation();
+            assert.notEqual(augmentation, undefined);
+        });
+    });
+
+    describe('createPromptSection', () => {
+        it('should render an undefined prompt section', async () => {
+            await adapter.sendTextToBot('test', async (context) => {
+                const augmentation = new DefaultAugmentation();
+                const section = await augmentation.createPromptSection();
+                assert.equal(section, undefined);
+            });
+        });
+    });
+
+    describe('validateResponse', () => {
+        it('should pass a valid response', async () => {
+            await adapter.sendTextToBot('test', async (context) => {
+                const state = await TestTurnState.create(context);
+                const augmentation = new DefaultAugmentation();
+                const response = await augmentation.validateResponse(
+                    context,
+                    state,
+                    tokenizer,
+                    { status: 'success', message: { role: 'assistant', content: valid_content } },
+                    3
+                );
+                assert.notEqual(response, undefined);
+                assert.equal(response.type, 'Validation');
+                assert.equal(response.valid, true);
+            });
+        });
+    });
+
+    describe('createPlanFromResponse', () => {
+        it('should create a plan with a single SAY command containing the models response', async () => {
+            await adapter.sendTextToBot('test', async (context) => {
+                const state = await TestTurnState.create(context);
+                const augmentation = new DefaultAugmentation();
+
+                // Validate response
+                const response: PromptResponse<string> = {
+                    status: 'success',
+                    message: { role: 'assistant', content: valid_SAY }
+                };
+                const validation = await augmentation.validateResponse(context, state, tokenizer, response, 3);
+                assert.equal(validation.type, 'Validation');
+                assert.equal(validation.valid, true);
+
+                // Create plan from response
+                const plan = await augmentation.createPlanFromResponse(context, state, {
+                    status: 'success',
+                    message: { role: 'assistant', content: validation.value }
+                });
+                assert.notEqual(plan, undefined);
+                assert.deepEqual(plan, {
+                    type: 'plan',
+                    commands: [
+                        {
+                            type: 'SAY',
+                            response: ''
+                        }
+                    ]
+                });
+            });
+        });
+    });
+});
