feae86b7a680632d40d1ca201fe5e2f9fdf2aa42
Kuojian Lu <kuojianlu@gmail.com>
2024-01-22 15:14:24 +0800

    add more

--- dotnet/packages/Microsoft.TeamsAI/Microsoft.TeamsAI.Tests/AITests/Models/ChatRoleTests.cs
+++ dotnet/packages/Microsoft.TeamsAI/Microsoft.TeamsAI.Tests/AITests/Models/ChatRoleTests.cs
@@ -18,6 +18,25 @@ namespace Microsoft.Teams.AI.Tests.AITests.Models
         }
 
         [Fact]
+        public void Test_OperatorEquality()
+        {
+            // Arrange
+            var chatRole1 = new ChatRole("user");
+            var chatRole2 = ChatRole.User;
+            var chatRole3 = ChatRole.Assistant;
+
+            // Act
+            var result1 = chatRole1 == chatRole2;
+            var result2 = chatRole1 == chatRole3;
+            var result3 = chatRole2 != chatRole3;
+
+            // Assert
+            Assert.True(result1);
+            Assert.False(result2);
+            Assert.True(result3);
+        }
+
+        [Fact]
         public void Test_Equals()
         {
             // Arrange
@@ -45,6 +64,29 @@ namespace Microsoft.Teams.AI.Tests.AITests.Models
 
             // Assert
             Assert.Equal("test", result.ToString());
+        }
+
+        [Fact]
+        public void Test_ImplicitConversion()
+        {
+            // Arrange
+            ChatRole chatRole = "user";
+
+            // Assert
+            Assert.Equal("user", chatRole.Value);
+        }
+
+        [Fact]
+        public void Test_GetHashCode()
+        {
+            // Arrange
+            var chatRole = ChatRole.Function;
+
+            // Act
+            var result = chatRole.GetHashCode();
+
+            // Assert
+            Assert.Equal("function".GetHashCode(), result);
         }
     }
 }
