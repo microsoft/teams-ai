1883aa9e302f6467160bd3203260ce8a71f50bc0
Kuojian Lu <kuojianlu@gmail.com>
2024-02-01 05:52:10 +0800

    [C#] refactor: refactor embeddings to sync with JS sdk (#1227)
    ## Linked issues
    
    closes: #1210 #907 
    
    ## Details
    
    Provide a list of your changes here. If you are fixing a bug, please
    provide steps to reproduce the bug.
    
    #### Change details
    
    - refactor embeddings to sync with the JS part and fix #1210
    - add integration tests for embeddings #907 
    
    ## Attestation Checklist
    
    - [x] My code follows the style guidelines of this project
    
    - I have checked for/fixed spelling, linting, and other errors
    - I have commented my code for clarity
    - I have made corresponding changes to the documentation (updating the
    doc strings in the code is sufficient)
    - My changes generate no new warnings
    - I have added tests that validates my changes, and provides sufficient
    test coverage. I have tested with:
      - Local testing
      - E2E testing in Teams
    - New and existing unit tests pass locally with my changes
    
    ### Additional information
    
    > Feel free to add other relevant information below

--- dotnet/packages/Microsoft.TeamsAI/Microsoft.TeamsAI.Tests/IntegrationTests/AzureOpenAIConfiguration.cs
+++ dotnet/packages/Microsoft.TeamsAI/Microsoft.TeamsAI.Tests/IntegrationTests/AzureOpenAIConfiguration.cs
@@ -5,12 +5,14 @@
         public string ModelId { get; set; } = string.Empty;
         public string ApiKey { get; set; } = string.Empty;
         public string? ChatModelId { get; set; }
+        public string? EmbeddingModelId { get; set; }
         public string Endpoint { get; set; } = string.Empty;
 
-        public AzureOpenAIConfiguration(string modelId, string? chatModelId, string apiKey, string endpoint)
+        public AzureOpenAIConfiguration(string modelId, string? chatModelId, string? embeddingModelId, string apiKey, string endpoint)
         {
             ModelId = modelId;
             ChatModelId = chatModelId;
+            EmbeddingModelId = embeddingModelId;
             ApiKey = apiKey;
             Endpoint = endpoint;
         }
---
+++ dotnet/packages/Microsoft.TeamsAI/Microsoft.TeamsAI/AI/Embeddings/BaseOpenAIEmbeddingsOptions.cs
@@ -0,0 +1,26 @@
+﻿namespace Microsoft.Teams.AI.AI.Embeddings
+{
+    /// <summary>
+    /// Base embeddings options common to both OpenAI and Azure OpenAI services.
+    /// </summary>
+    public class BaseOpenAIEmbeddingsOptions
+    {
+        /// <summary>
+        /// Optional. Whether to log requests to the console.
+        /// </summary>
+        /// <remarks>
+        /// This is useful for debugging prompts.
+        /// The default value is `false`.
+        /// </remarks>
+        public bool? LogRequests { get; set; }
+
+        /// <summary>
+        /// Optional. Retry policy to use when calling the OpenAI API.
+        /// </summary>
+        /// <remarks>
+        /// The default retry policy is `{ TimeSpan.FromMilliseconds(2000), TimeSpan.FromMilliseconds(5000) }`
+        /// which means that the first retry will be after 2 seconds and the second retry will be after 5 seconds.
+        /// </remarks>
+        public List<TimeSpan>? RetryPolicy { get; set; }
+    }
+}
