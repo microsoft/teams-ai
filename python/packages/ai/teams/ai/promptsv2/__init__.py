3abd11e8a870e83c8097fb02635861c488623075
Chaoyi Yuan <blackchoey@gmail.com>
2024-02-01 09:52:48 +0800

    [PY] feat: add remaining sections and messages (#1224)
    ## Linked issues
    
    closes: #1065
    
    ## Details
    
    1. Add remaining sections and messages besides `UserInputMessage`.
    
    ## Attestation Checklist
    
    - [x] My code follows the style guidelines of this project
    
    - I have checked for/fixed spelling, linting, and other errors
    - I have commented my code for clarity
    - I have made corresponding changes to the documentation (updating the
    doc strings in the code is sufficient)
    - My changes generate no new warnings
    - I have added tests that validates my changes, and provides sufficient
    test coverage. I have tested with:
      - Local testing
      - E2E testing in Teams
    - New and existing unit tests pass locally with my changes

--- python/packages/ai/teams/ai/promptsv2/__init__.py
+++ python/packages/ai/teams/ai/promptsv2/__init__.py
@@ -3,11 +3,19 @@ Copyright (c) Microsoft Corporation. All rights reserved.
 Licensed under the MIT License.
 """
 
+from .assistant_message import AssistantMessage
+from .conversation_history import ConversationHistory
+from .data_source_section import DataSourceSection
 from .function_call import FunctionCall
+from .function_call_message import FunctionCallMessage
+from .function_response_message import FunctionResponseMessage
+from .group_section import GroupSection
 from .layout_engine import LayoutEngine
 from .message import ImageContentPart, Message, TextContentPart
 from .prompt_section import PromptFunctions, PromptSection
 from .prompt_section_base import PromptSectionBase
 from .rendered_prompt_section import RenderedPromptSection
+from .system_message import SystemMessage
 from .template_section import TemplateSection
 from .text_section import TextSection
+from .user_message import UserMessage
