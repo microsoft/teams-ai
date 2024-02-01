5d5d426b38adeb88504636d38c2c0c3de15a232b
Chaoyi Yuan <chyuan@microsoft.com>
2024-02-01 16:54:51 +0800

    feat: add prompt and user input message

--- python/packages/ai/teams/ai/promptsv2/__init__.py
+++ python/packages/ai/teams/ai/promptsv2/__init__.py
@@ -11,11 +11,12 @@ from .function_call_message import FunctionCallMessage
 from .function_response_message import FunctionResponseMessage
 from .group_section import GroupSection
 from .layout_engine import LayoutEngine
-from .message import ImageContentPart, Message, TextContentPart
+from .message import ImageContentPart, ImageUrl, Message, TextContentPart
 from .prompt_section import PromptFunctions, PromptSection
 from .prompt_section_base import PromptSectionBase
 from .rendered_prompt_section import RenderedPromptSection
 from .system_message import SystemMessage
 from .template_section import TemplateSection
 from .text_section import TextSection
+from .user_input_message import UserInputMessage
 from .user_message import UserMessage
