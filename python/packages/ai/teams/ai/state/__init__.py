dadf02faf6935de2978b1e2fe2ca93815559fc1a
Alex Acebo <aacebowork@gmail.com>
2024-02-05 10:21:56 -0800

    Update __init__.py

--- python/packages/ai/teams/ai/state/__init__.py
+++ python/packages/ai/teams/ai/state/__init__.py
@@ -5,7 +5,6 @@ Licensed under the MIT License.
 
 from .conversation_history import ConversationHistory
 from .conversation_state import ConversationState
-from .message import Message, MessageRole
 from .temp_state import TempState
 from .turn_state import TurnState
 from .user_state import UserState
