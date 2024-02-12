8ece0de910a007da0d88de0c80f0f11c577b3763
Alex Acebo <aacebowork@gmail.com>
2024-02-12 09:18:26 -0800

    add default moderator

---
+++ python/packages/ai/teams/ai/moderators/__init__.py
@@ -0,0 +1,7 @@
+"""
+Copyright (c) Microsoft Corporation. All rights reserved.
+Licensed under the MIT License.
+"""
+
+from .default_moderator import DefaultModerator
+from .moderator import Moderator
