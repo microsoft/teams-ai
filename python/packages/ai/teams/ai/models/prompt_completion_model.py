3e4dadf7bfd8acc71b1bf74648b3a94591c3f7f3
Alex Acebo <aacebowork@gmail.com>
2024-02-12 16:57:45 -0800

    add openai model

---
+++ python/packages/ai/teams/ai/models/prompt_completion_model.py
@@ -0,0 +1,42 @@
+"""
+Copyright (c) Microsoft Corporation. All rights reserved.
+Licensed under the MIT License.
+"""
+
+from abc import ABC, abstractmethod
+
+from botbuilder.core import TurnContext
+
+from ...state import Memory
+from ..prompts import PromptFunctions, PromptTemplate
+from ..tokenizers import Tokenizer
+from .prompt_response import PromptResponse
+
+
+class PromptCompletionModel(ABC):
+    """
+    An AI model that can be used to complete prompts.
+    """
+
+    @abstractmethod
+    async def complete_prompt(
+        self,
+        context: TurnContext,
+        memory: Memory,
+        functions: PromptFunctions,
+        tokenizer: Tokenizer,
+        template: PromptTemplate,
+    ) -> PromptResponse[str]:
+        """
+        Completes a prompt.
+
+        Args:
+            context (TurnContext): turn context.
+            memory (Memory): the turn state.
+            functions (PromptFunctions): to use when rendering the prompt.
+            tokenizer (Tokenizer): to use when rendering the prompt.
+            template (PromptTemplate): template to complete.
+
+        Returns:
+            PromptResponse[str]: a response with the status and message.
+        """
