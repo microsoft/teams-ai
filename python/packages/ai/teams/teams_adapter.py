2544f5008ba110d0ed31fd479b0e88c6ecda73b0
Alex Acebo <aacebowork@gmail.com>
2024-02-14 13:11:24 -0800

    override process method to add header

--- python/packages/ai/teams/teams_adapter.py
+++ python/packages/ai/teams/teams_adapter.py
@@ -3,10 +3,13 @@ Copyright (c) Microsoft Corporation. All rights reserved.
 Licensed under the MIT License.
 """
 
+from importlib.metadata import version
 from logging import Logger
 from typing import Any, Optional, cast
 
 from aiohttp import ClientResponse, ClientResponseError, ClientSession
+from aiohttp.web import Request, Response, WebSocketResponse
+from botbuilder.core import Bot
 from botbuilder.integration.aiohttp import (
     CloudAdapter,
     ConfigurationBotFrameworkAuthentication,
@@ -24,6 +27,10 @@ class TeamsAdapter(CloudAdapter):
     An adapter that implements the Bot Framework Protocol
     and can be hosted in different cloud environments both public and private.
     """
+
+    @property
+    def user_agent(self) -> str:
+        return f"teamsai-py/{version('teams-ai')}"
 
     def __init__(
         self,
@@ -43,33 +50,59 @@ class TeamsAdapter(CloudAdapter):
                 configuration,
                 credentials_factory=cast(ServiceClientCredentialsFactory, credentials_factory),
                 auth_configuration=cast(AuthenticationConfiguration, auth_configuration),
-                http_client_factory=_TeamsHttpClientFactory(http_client_factory),
+                http_client_factory=_TeamsHttpClientFactory(
+                    self.user_agent, parent=http_client_factory
+                ),
                 logger=cast(Logger, logger),
             )
         )
 
+    async def process(
+        self,
+        request: Request,
+        bot: Bot,
+        ws_response: Optional[WebSocketResponse] = None,
+    ) -> Optional[Response]:
+        res = await super().process(
+            request,
+            bot,
+            cast(WebSocketResponse, ws_response),
+        )
+
+        if res:
+            res.headers.add("User-Agent", self.user_agent)
+
+        return res
+
 
 class _TeamsHttpClientFactory(HttpClientFactory):
+    _user_agent: str
     _parent: Optional[HttpClientFactory]
 
-    def __init__(self, parent: Optional[HttpClientFactory] = None) -> None:
+    def __init__(self, user_agent: str, *, parent: Optional[HttpClientFactory] = None) -> None:
+        self._user_agent = user_agent
         self._parent = parent
 
     def create_client(self) -> HttpClientBase:
-        return _TeamsHttpClient()
+        if self._parent:
+            return _TeamsHttpClient(self._user_agent, parent=self._parent.create_client())
+
+        return _TeamsHttpClient(self._user_agent)
 
 
 class _TeamsHttpClient(HttpClientBase):
+    _user_agent: str
     _parent: Optional[HttpClientBase]
 
-    def __init__(self, parent: Optional[HttpClientBase] = None) -> None:
+    def __init__(self, user_agent: str, *, parent: Optional[HttpClientBase] = None) -> None:
         self._session = ClientSession()
+        self._user_agent = user_agent
         self._parent = parent
 
     async def post(self, *, request: HttpRequest) -> HttpResponseBase:
-        request.headers["User-Agent"] = "teamsai-py/1.0.0"
+        request.headers["User-Agent"] = self._user_agent
 
-        if self._parent is not None:
+        if self._parent:
             return await self._parent.post(request=request)
 
         aio_response = await self._session.post(
