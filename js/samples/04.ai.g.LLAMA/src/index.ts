0631a426635ad47074309774fe795a6e39afd8eb
Corina Gum <>
2024-03-07 16:07:04 -0800

    start of new sample

---
+++ js/samples/04.ai.g.LLAMA/src/index.ts
@@ -0,0 +1,98 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+// Import required packages
+import { config } from 'dotenv';
+import * as path from 'path';
+import * as restify from 'restify';
+
+// Import required bot services.
+// See https://aka.ms/bot-services to learn more about the different parts of a bot.
+import { ActivityTypes, ConfigurationServiceClientCredentialFactory, MemoryStorage, TurnContext } from 'botbuilder';
+
+import { LlamaModel, TurnState, TeamsAdapter, Application } from '@microsoft/teams-ai';
+
+// Read botFilePath and botFileSecret from .env file.
+const ENV_FILE = path.join(__dirname, '..', '.env');
+config({ path: ENV_FILE });
+
+// Create adapter.
+// See https://aka.ms/about-bot-adapter to learn more about how bots work.
+const adapter = new TeamsAdapter(
+    {},
+    new ConfigurationServiceClientCredentialFactory({
+        MicrosoftAppId: process.env.BOT_ID,
+        MicrosoftAppPassword: process.env.BOT_PASSWORD,
+        MicrosoftAppType: 'MultiTenant'
+    })
+);
+
+// Catch-all for errors.
+const onTurnErrorHandler = async (context: TurnContext, error: any) => {
+    // This check writes out errors to console log .vs. app insights.
+    // NOTE: In production environment, you should consider logging this to Azure
+    //       application insights.
+    console.error(`\n [onTurnError] unhandled error: ${error}`);
+    console.log(error);
+
+    // Send a trace activity, which will be displayed in Bot Framework Emulator
+    await context.sendTraceActivity(
+        'OnTurnError Trace',
+        `${error}`,
+        'https://www.botframework.com/schemas/error',
+        'TurnError'
+    );
+
+    // Send a message to the user
+    await context.sendActivity('The bot encountered an error or bug.');
+    await context.sendActivity('To continue to run this bot, please fix the bot source code.');
+};
+
+// Set the onTurnError for the singleton CloudAdapter.
+adapter.onTurnError = onTurnErrorHandler;
+
+// Create HTTP server.
+const server = restify.createServer();
+server.use(restify.plugins.bodyParser());
+
+server.listen(process.env.port || process.env.PORT || 3978, () => {
+    console.log(`\n${server.name} listening to ${server.url}`);
+    console.log('\nGet Bot Framework Emulator: https://aka.ms/botframework-emulator');
+    console.log('\nTo test your bot in Teams, sideload the app manifest.json within Teams Apps.');
+});
+
+interface ConversationState {
+    count: number;
+}
+type ApplicationTurnState = TurnState<ConversationState>;
+// Create AI components
+const model = new LlamaModel({
+    // Llama Support
+    apiKey: process.env.API_KEY!
+});
+
+// Define storage and application
+// - Note that we're not passing a prompt for our AI options as we won't be chatting with the app.
+const storage = new MemoryStorage();
+const app = new Application<ApplicationTurnState>({
+    storage
+});
+
+// Listen for ANY message to be received. MUST BE AFTER ANY OTHER MESSAGE HANDLERS
+app.activity(ActivityTypes.Message, async (context: TurnContext, state: ApplicationTurnState) => {
+    // Increment count state
+    let count = state.conversation.count ?? 0;
+    state.conversation.count = ++count;
+
+    // Echo back users request
+    await context.sendActivity(`[${count}] you said: ${context.activity.text}`);
+});
+
+// Listen for incoming server requests.
+server.post('/api/messages', async (req, res) => {
+    // Route received a request to adapter for processing
+    await adapter.process(req, res as any, async (context) => {
+        // Dispatch to application for routing
+        await app.run(context);
+    });
+});
