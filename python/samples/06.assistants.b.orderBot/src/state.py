fc0e38db0b280f2eed074cec0efef633aba68288
Lily Du <lilydu@microsoft.com>
2024-04-01 10:06:02 -0700

    [PY] feat: OrderBot  (#1465)
    ## Linked issues
    
    closes: #1253
    
    ## Details
    
    - Added OrderBot into PY samples

---
+++ python/samples/06.assistants.b.orderBot/src/state.py
@@ -0,0 +1,19 @@
+"""
+Copyright (c) Microsoft Corporation. All rights reserved.
+Licensed under the MIT License.
+"""
+
+from typing import Optional
+
+from botbuilder.core import Storage, TurnContext
+from teams.state import ConversationState, TempState, TurnState, UserState
+
+
+class AppTurnState(TurnState[ConversationState, UserState, TempState]):
+    @classmethod
+    async def load(cls, context: TurnContext, storage: Optional[Storage] = None) -> "AppTurnState":
+        return cls(
+            conversation=await ConversationState.load(context, storage),
+            user=await UserState.load(context, storage),
+            temp=await TempState.load(context, storage),
+        )
