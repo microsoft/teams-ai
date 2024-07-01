647cfaf5101345a455fb6dbcf94a4520596d90de
Corina Gum <>
2024-07-01 10:52:55 -0700

    Refactor CompletionConfig into types folder

---
+++ js/packages/teams-ai/src/types/index.ts
@@ -0,0 +1,10 @@
+/**
+ * @module teams-ai
+ */
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+export * from './CompletionConfig';
+export * from './OpenAIFuncion';
