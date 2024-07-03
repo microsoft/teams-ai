96495df8a419ffc24ee13c8bd786a21529870e25
kavin <115390646+singhk97@users.noreply.github.com>
2024-07-03 09:44:52 -0700

    [PY] fix: Citations UI was not showing (#1799)
    ## Linked issues
    
    closes: #minor
    
    ## Details
    
    The `Activity` object onto which the citations-related fields were being
    added to weren't being properly deserialized into json payload.
    
    
    #### Change details
    * added `_attributes_map` which is required for `msrest.serialization`
    to deserialize the defined fields.
    * introduced `AIEntity` which extends `Entity`, as it is in the input
    type to `Activity.entities`
    
    **code snippets**:
    
    **screenshots**:
    
    ## Attestation Checklist
    
    - [x] My code follows the style guidelines of this project
    
    - I have checked for/fixed spelling, linting, and other errors
    - I have commented my code for clarity
    - I have made corresponding changes to the documentation (updating the
    doc strings in the code is sufficient)
    - My changes generate no new warnings
    - I have added tests that validates my changes, and provides sufficient
    test coverage. I have tested with:
      - Local testing
      - E2E testing in Teams
    - New and existing unit tests pass locally with my changes
    
    ### Additional information
    * Verified it works in Teams

--- python/packages/ai/teams/ai/citations/__init__.py
+++ python/packages/ai/teams/ai/citations/__init__.py
@@ -3,6 +3,12 @@ Copyright (c) Microsoft Corporation. All rights reserved.
 Licensed under the MIT License.
 """
 
-from .citations import Appearance, ClientCitation, Pattern, SensitivityUsageInfo
+from .citations import (
+    AIEntity,
+    Appearance,
+    ClientCitation,
+    Pattern,
+    SensitivityUsageInfo,
+)
 
-__all__ = ["ClientCitation", "Appearance", "SensitivityUsageInfo", "Pattern"]
+__all__ = ["ClientCitation", "Appearance", "SensitivityUsageInfo", "Pattern", "AIEntity"]
