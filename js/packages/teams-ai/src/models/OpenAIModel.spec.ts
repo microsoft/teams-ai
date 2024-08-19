3b29133ec184f7b755fb8f1109d245a7e7f883b2
Corina Gum <>
2024-08-19 15:22:49 -0700

    Add a few OpenaiModel tests

--- js/packages/teams-ai/src/models/OpenAIModel.spec.ts
+++ js/packages/teams-ai/src/models/OpenAIModel.spec.ts
@@ -1,4 +1,5 @@
 import { strict as assert } from 'assert';
+import sinon from 'sinon';
 import { OpenAIModel } from './OpenAIModel';
 import { EventEmitter } from 'stream';
 
@@ -41,6 +42,36 @@ describe('OpenAIModel', () => {
                 `Model created with an invalid endpoint of '${badEndpoint}'. The endpoint must be a valid HTTPS url.`
             ),
             'Unexpected error thrown from OpenAIModel constructor.'
+        );
+    });
+
+    it('should trim an endpoint with trailing `/`"', () => {
+        const trailingForwardSlashEndpoint = 'https://test-endpoint.com/';
+        const model = new OpenAIModel({
+            azureApiKey: 'test-azure-api-key',
+            azureEndpoint: trailingForwardSlashEndpoint,
+            azureDefaultDeployment: GPT35_MODEL
+        });
+
+        assert(model instanceof OpenAIModel);
+        assert.equal(model['_client']['baseURL'], 'https://test-endpoint.com/openai');
+    });
+
+    it('should warn when requestConfig is used', () => {
+        const consoleWarnSpy = sinon.spy(console, 'warn');
+        const model = new OpenAIModel({
+            apiKey: 'test-api-key',
+            endpoint: goodEndpoint,
+            defaultModel: GPT35_MODEL,
+            retryPolicy: [1, 2],
+            requestConfig: { timeout: 1000 }
+        });
+        assert(model instanceof OpenAIModel);
+        assert(consoleWarnSpy.called);
+        assert(
+            consoleWarnSpy.calledWith(
+                `OpenAIModel: The 'requestConfig' option is deprecated. Use 'clientOptions' instead.`
+            )
         );
     });
 });
