1b6f1897b825eccfc3aa2c244f055c3e7c874fd1
Lily Du <lilydu@microsoft.com>
2024-09-16 16:29:46 -0700

    [JS] fix: separated chefbot samples (#2027)
    ## Linked issues
    
    closes: #minor
    
    ## Details
    
    - separated chefbot samples 
    
    ## Attestation Checklist
    
    - [x] My code follows the style guidelines of this project
    
    - I have checked for/fixed spelling, linting, and other errors
    - I have commented my code for clarity
    - I have made corresponding changes to the documentation (updating the
    doc strings in the code is sufficient)
    - My changes generate no new warnings
    - I have added tests that validates my changes, and provides sufficient
    test coverage. I have tested with:
      - Local testing
      - E2E testing in Teams
    - New and existing unit tests pass locally with my changes

---
+++ js/samples/04.ai-apps/i.teamsChefBot-streaming/src/index.ts
@@ -0,0 +1,158 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+// Import required packages
+import { config } from 'dotenv';
+import * as path from 'path';
+import * as restify from 'restify';
+
+// Import required bot services.
+// See https://aka.ms/bot-services to learn more about the different parts of a bot.
+import { ConfigurationServiceClientCredentialFactory, MemoryStorage, TurnContext } from 'botbuilder';
+
+import {
+    AI,
+    Application,
+    ActionPlanner,
+    OpenAIModel,
+    PromptManager,
+    TurnState,
+    TeamsAdapter
+} from '@microsoft/teams-ai';
+
+import { addResponseFormatter } from './responseFormatter';
+import { VectraDataSource } from './VectraDataSource';
+
+// Read botFilePath and botFileSecret from .env file.
+const ENV_FILE = path.join(__dirname, '..', '.env');
+config({ path: ENV_FILE });
+
+// Create adapter.
+// See https://aka.ms/about-bot-adapter to learn more about how bots work.
+const adapter = new TeamsAdapter(
+    {},
+    new ConfigurationServiceClientCredentialFactory({
+        MicrosoftAppType: process.env.BOT_TYPE,
+        MicrosoftAppId: process.env.BOT_ID,
+        MicrosoftAppPassword: process.env.BOT_PASSWORD,
+        MicrosoftAppTenantId: process.env.BOT_TENANT_ID
+    })
+);
+
+// Catch-all for errors.
+const onTurnErrorHandler = async (context: TurnContext, error: any) => {
+    // This check writes out errors to console log .vs. app insights.
+    // NOTE: In production environment, you should consider logging this to Azure
+    //       application insights.
+    console.error(`\n [onTurnError] unhandled error: ${error}`);
+    console.log(error);
+
+    // Send a trace activity, which will be displayed in Bot Framework Emulator
+    await context.sendTraceActivity(
+        'OnTurnError Trace',
+        `${error}`,
+        'https://www.botframework.com/schemas/error',
+        'TurnError'
+    );
+
+    // Send a message to the user
+    await context.sendActivity('The bot encountered an error or bug.');
+    await context.sendActivity('To continue to run this bot, please fix the bot source code.');
+};
+
+// Set the onTurnError for the singleton CloudAdapter.
+adapter.onTurnError = onTurnErrorHandler;
+
+// Create HTTP server.
+const server = restify.createServer();
+server.use(restify.plugins.bodyParser());
+
+server.listen(process.env.port || process.env.PORT || 3978, () => {
+    console.log(`\n${server.name} listening to ${server.url}`);
+    console.log('\nGet Bot Framework Emulator: https://aka.ms/botframework-emulator');
+    console.log('\nTo test your bot in Teams, sideload the app manifest.json within Teams Apps.');
+});
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface ConversationState {}
+type ApplicationTurnState = TurnState<ConversationState>;
+
+if (!process.env.OPENAI_KEY && !process.env.AZURE_OPENAI_KEY) {
+    throw new Error('Missing environment variables - please check that OPENAI_KEY or AZURE_OPENAI_KEY is set.');
+}
+
+// Create AI components
+const model = new OpenAIModel({
+    // OpenAI Support
+    apiKey: process.env.OPENAI_KEY!,
+    defaultModel: 'gpt-4o',
+    stream: true,
+    project: process.env.OPENAI_PROJECT_KEY!,
+    clientOptions: { apiKey: process.env.OPENAI_KEY! },
+
+    // Azure OpenAI Support
+    // azureApiKey: process.env.AZURE_OPENAI_KEY!,
+    // azureDefaultDeployment: 'gpt-3.5-turbo',
+    // azureEndpoint: process.env.AZURE_OPENAI_ENDPOINT!,
+    // azureApiVersion: '2023-03-15-preview',
+
+    // Request logging
+    logRequests: true
+});
+
+const prompts = new PromptManager({
+    promptsFolder: path.join(__dirname, '../src/prompts')
+});
+
+const planner = new ActionPlanner({
+    model,
+    prompts,
+    defaultPrompt: 'default',
+    startStreamingMessage: 'Loading stream results...'
+});
+
+// Define storage and application
+const storage = new MemoryStorage();
+const app = new Application<ApplicationTurnState>({
+    storage,
+    ai: {
+        planner
+    }
+});
+
+// Register your data source with planner
+planner.prompts.addDataSource(
+    new VectraDataSource({
+        name: 'teams-ai',
+        apiKey: process.env.OPENAI_KEY!,
+        azureApiKey: process.env.AZURE_OPENAI_KEY!,
+        azureEndpoint: process.env.AZURE_OPENAI_ENDPOINT!,
+        indexFolder: path.join(__dirname, '../index')
+    })
+);
+
+// Add a custom response formatter to convert markdown code blocks to <pre> tags
+addResponseFormatter(app);
+
+// Register other AI actions
+app.ai.action(
+    AI.FlaggedInputActionName,
+    async (context: TurnContext, state: ApplicationTurnState, data: Record<string, any>) => {
+        await context.sendActivity(`I'm sorry your message was flagged: ${JSON.stringify(data)}`);
+        return AI.StopCommandName;
+    }
+);
+
+app.ai.action(AI.FlaggedOutputActionName, async (context: TurnContext, state: ApplicationTurnState, data: any) => {
+    await context.sendActivity(`I'm not allowed to talk about such things.`);
+    return AI.StopCommandName;
+});
+
+// Listen for incoming server requests.
+server.post('/api/messages', async (req, res) => {
+    // Route received a request to adapter for processing
+    await adapter.process(req, res as any, async (context) => {
+        // Dispatch to application for routing
+        await app.run(context);
+    });
+});
