c5a97c3d06fcbc58d846f57953d4a032322f6002
Corina Gum <>
2024-09-30 14:07:05 -0700

    Refactor AiEntity, ClientCitation, SensitivityUsageInfo, TooManyStepsParameters

--- js/packages/teams-ai/src/types/index.ts
+++ js/packages/teams-ai/src/types/index.ts
@@ -7,5 +7,9 @@
  */
 
 export * from './ActionCall';
+export * from './AIEntity';
+export * from './ClientCitation';
 export * from './CompletionConfig';
 export * from './PromptResponse';
+export * from './SensitivityUsageInfo';
+export * from './TooManyStepsParameters';
