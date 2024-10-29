590f8966b96dbf514a9db27fd19ca5504317acf1
Kavin Singh <kavinsingh@microsoft.com>
2024-10-29 07:47:53 -0700

    update twenty questions & o1 model fix

--- python/samples/04.ai.a.twentyQuestions/src/bot.py
+++ python/samples/04.ai.a.twentyQuestions/src/bot.py
@@ -41,14 +41,14 @@ model: OpenAIModel
 
 if config.OPENAI_KEY:
     model = OpenAIModel(
-        OpenAIModelOptions(api_key=config.OPENAI_KEY, default_model="gpt-3.5-turbo")
+        OpenAIModelOptions(api_key=config.OPENAI_KEY, default_model="gpt-4o")
     )
 elif config.AZURE_OPENAI_KEY and config.AZURE_OPENAI_ENDPOINT:
     model = OpenAIModel(
         AzureOpenAIModelOptions(
             api_key=config.AZURE_OPENAI_KEY,
-            default_model="gpt-35-turbo",
-            api_version="2023-03-15-preview",
+            default_model="gpt-4o",
+            api_version="2024-08-01-preview",
             endpoint=config.AZURE_OPENAI_ENDPOINT,
         )
     )
