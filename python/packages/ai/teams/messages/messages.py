e0b2b11d83f4508ec3406799bf78e5f8e396d791
Alex Acebo <aacebowork@gmail.com>
2025-01-22 10:37:00 -0800

    fix messages handler and tests

--- python/packages/ai/teams/messages/messages.py
+++ python/packages/ai/teams/messages/messages.py
@@ -5,11 +5,17 @@ Licensed under the MIT License.
 
 from __future__ import annotations
 
-from typing import Awaitable, Callable, Generic, List, TypeVar
+from typing import Awaitable, Callable, Generic, List, TypeVar, Union, cast
 
-from botbuilder.core import TurnContext
-from botbuilder.core.serializer_helper import serializer_helper
-from botbuilder.schema import ActivityTypes
+from botbuilder.core import InvokeResponse, TurnContext
+from botbuilder.core.serializer_helper import deserializer_helper, serializer_helper
+from botbuilder.schema import Activity, ActivityTypes
+from botbuilder.schema.teams import (
+    TaskModuleContinueResponse,
+    TaskModuleMessageResponse,
+    TaskModuleResponse,
+    TaskModuleTaskInfo,
+)
 
 from teams.route import Route
 from teams.state import TurnState
@@ -25,9 +31,9 @@ class Messages(Generic[StateT]):
     def __init__(self, routes: List[Route[StateT]]) -> None:
         self._routes = routes
 
-    def fetch(self) -> Callable[
-        [Callable[[TurnContext, StateT, dict], Awaitable[None]]],
-        Callable[[TurnContext, StateT, dict], Awaitable[None]],
+    def fetch_task(self) -> Callable[
+        [Callable[[TurnContext, StateT, dict], Awaitable[Union[TaskModuleTaskInfo, str]]]],
+        Callable[[TurnContext, StateT, dict], Awaitable[Union[TaskModuleTaskInfo, str]]],
     ]:
         """
         Adds a route for handling the message fetch task activity.
@@ -35,13 +41,13 @@ class Messages(Generic[StateT]):
 
         ```python
         # Use this method as a decorator
-        @app.messages.fetch()
-        async def fetch(context: TurnContext, state: TurnState, data: dict):
+        @app.messages.fetch_task()
+        async def fetch_task(context: TurnContext, state: TurnState, data: dict):
             print(f"Execute with data: {data}")
             return True
 
         # Pass a function to this method
-        app.messages.fetch()(fetch)
+        app.messages.fetch_task()(fetch_task)
         ```
         """
 
@@ -53,15 +59,38 @@ class Messages(Generic[StateT]):
             )
 
         def __call__(
-            func: Callable[[TurnContext, StateT, dict], Awaitable[None]],
-        ) -> Callable[[TurnContext, StateT, dict], Awaitable[None]]:
-            async def __handler__(context: TurnContext, state: StateT):
-                if not context.activity.value:
-                    return False
-                await func(context, state, context.activity.value)
+            func: Callable[
+                [TurnContext, StateT, dict],
+                Awaitable[Union[TaskModuleTaskInfo, str]],
+            ],
+        ) -> Callable[
+            [TurnContext, StateT, dict],
+            Awaitable[Union[TaskModuleTaskInfo, str]],
+        ]:
+            async def __invoke__(context: TurnContext, state: StateT):
+                res = await func(context, state, cast(dict, context.activity.value))
+                await self._invoke_task_response(context, res)
                 return True
 
-            self._routes.append(Route[StateT](__selector__, __handler__))
+            self._routes.append(Route[StateT](__selector__, __invoke__, True))
             return func
 
         return __call__
+
+    async def _invoke_task_response(
+        self, context: TurnContext, body: Union[TaskModuleTaskInfo, str]
+    ):
+        if context._INVOKE_RESPONSE_KEY in context.turn_state:
+            return
+
+        response = TaskModuleResponse(task=TaskModuleContinueResponse(value=body))
+
+        if isinstance(body, str):
+            response = TaskModuleResponse(task=TaskModuleMessageResponse(value=body))
+
+        await context.send_activity(
+            Activity(
+                type=ActivityTypes.invoke_response,
+                value=InvokeResponse(body=serializer_helper(response), status=200),
+            )
+        )
--- python/packages/ai/tests/test_messages.py
+++ python/packages/ai/tests/test_messages.py
@@ -8,40 +8,62 @@ from unittest import IsolatedAsyncioTestCase, mock
 import pytest
 from botbuilder.core import TurnContext
 from botbuilder.schema import Activity, ChannelAccount, ConversationAccount
+from botbuilder.schema.teams import TaskModuleTaskInfo
 
 from teams import Application
 from tests.utils import SimpleAdapter
 
 
 class TestMessages(IsolatedAsyncioTestCase):
-    application: Application
+    app: Application
 
     @pytest.fixture(autouse=True)
     def before_each(self):
-        self.application = Application()
+        self.app = Application()
         yield
 
     @pytest.mark.asyncio
-    async def test_fetch(self):
-        handler = mock.AsyncMock()  # mock handler
-        self.application.messages.fetch()(handler)
-        self.assertEqual(len(self.application._routes), 1)
+    async def test_fetch_task(self):
+        handler = mock.AsyncMock()
+        handler.return_value = TaskModuleTaskInfo()
+        self.app.messages.fetch_task()(handler)
+        self.assertEqual(len(self.app._routes), 1)
 
-        # The handler will be tirggered
-        await self.application.on_turn(
+        await self.app.on_turn(
+            TurnContext(
+                SimpleAdapter(),
+                Activity(
+                    id="1234",
+                    type="invoke",
+                    text="test",
+                    from_property=ChannelAccount(id="user", name="User Name"),
+                    recipient=ChannelAccount(id="bot", name="Bot Name"),
+                    conversation=ConversationAccount(id="convo", name="Convo Name"),
+                    channel_id="UnitTest",
+                    locale="en-uS",
+                    service_url="https://example.org",
+                    members_added=[ChannelAccount(id="user-2", name="User Name 2")],
+                ),
+            )
+        )
+
+        await self.app.on_turn(
             TurnContext(
                 SimpleAdapter(),
                 Activity(
                     id="1234",
                     type="invoke",
                     name="message/fetchTask",
-                    value={"hello": "world"},
-                    from_property=ChannelAccount(id="user", name="Task Modules Test User"),
-                    recipient=ChannelAccount(id="bot", name="Task Modules Test Bot"),
-                    conversation=ConversationAccount(id="convo", name="Task Modules Test Convo"),
+                    text="test",
+                    value={},
+                    from_property=ChannelAccount(id="user", name="User Name"),
+                    recipient=ChannelAccount(id="bot", name="Bot Name"),
+                    conversation=ConversationAccount(id="convo", name="Convo Name"),
                     channel_id="UnitTest",
                     locale="en-uS",
                     service_url="https://example.org",
                 ),
             )
         )
+
+        handler.assert_called_once()
