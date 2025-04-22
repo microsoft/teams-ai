129f17822194f66adc5b3123eec5385cbb8632ba
heyitsaamir <ajawaid191@gmail.com>
2025-04-22 16:32:13 -0700

    Fix up docs

--- book/generated-snippets/ts/index.snippet.multiple-actions-card.ts
+++ book/generated-snippets/ts/index.snippet.multiple-actions-card.ts
@@ -1,5 +1,14 @@
+/**
+ * import {
+ *  Card,
+ *  ExecuteAction,
+ *  OpenUrlAction,
+ *  ActionSet,
+ * } from "@microsoft/teams.cards";
+ */
 new ActionSet(
   new ExecuteAction({ title: 'Submit Feedback' })
     .withData({ action: 'submit_feedback' })
     .withAssociatedInputs('auto'),
   new OpenUrlAction('https://adaptivecards.microsoft.com').withTitle('Learn More')
+)
--- book/generated-snippets/ts/index.snippet.single-action.ts
+++ book/generated-snippets/ts/index.snippet.single-action.ts
@@ -1,3 +1,4 @@
+/** import { ExecuteAction } from "@microsoft/teams.cards"; */
 new ExecuteAction({ title: 'Submit Feedback' })
   .withData({ action: 'submit_feedback' })
   .withAssociatedInputs('auto'),
