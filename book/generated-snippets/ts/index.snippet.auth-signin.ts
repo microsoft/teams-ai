997f39399c3e70e47cae87ee8ce54bd52beec935
kavins14 <kavinsingh@hotmail.com>
2025-05-01 03:03:01 +0700

    Add `user authentication` section to docs (#2411)
    ## Linked issues
    
    closes: #minor
    
    ## Details
    
    Add `User Authentication` section
    
    <img width="1454" alt="Screenshot 2025-04-25 at 6 10 08 AM"
    src="https://github.com/user-attachments/assets/4e0ac220-8228-44ee-9ae9-fbf310f508b4"
    />
    
    ---------
    
    Co-authored-by: Kavin <115390646+singhk97@users.noreply.github.com>

---
+++ book/generated-snippets/ts/index.snippet.auth-signin.ts
@@ -0,0 +1,14 @@
+app.on('message', async ({ log, signin, api, isSignedIn }) => {
+  if (!isSignedIn) { // no need to sign in if already signed in...
+    await signin(); // call signin for your auth connection...
+    return;
+  }
+
+  const me = await api.user.me.get();
+  log.info(`user "${me.displayName}" already signed in!`);
+});
+
+app.event('signin', async ({ send, api, token }) => {
+  const me = await api.user.me.get();
+  await send(`user "${me.displayName}" signed in. Here's the token: ${token}`);
+});
