83ee5d570b38f1e864c00c19173858f22dddb44a
kavins14 <kavinsingh@hotmail.com>
2025-05-01 02:54:48 +0700

    Message extension docs (#2418)
    ## Linked issues
    
    closes: #minor
    
    ## Details
    - Added message extension docs
    - Added message extension test
    
    <img width="1440" alt="Screenshot 2025-04-29 at 4 43 52 AM"
    src="https://github.com/user-attachments/assets/0f5d1c2f-736f-4034-ae80-afd68cd603af"
    />
    
    
    ## Attestation Checklist
    
    - [x] My code follows the style guidelines of this project
    
    - I have checked for/fixed spelling, linting, and other errors
    - I have commented my code for clarity
    - I have made corresponding changes to the documentation (updating the
    doc strings in the code is sufficient)
    - My changes generate no new warnings
    - I have added tests that validates my changes, and provides sufficient
    test coverage. I have tested with:
      - Local testing
      - E2E testing in Teams
    - New and existing unit tests pass locally with my changes
    
    ---------
    
    Co-authored-by: Kavin <115390646+singhk97@users.noreply.github.com>

---
+++ book/generated-snippets/ts/index.snippet.message-ext-submit.ts
@@ -0,0 +1,22 @@
+app.on('message.ext.submit', async ({ activity }) => {
+  const { commandId } = activity.value;
+  let card: ICard;
+
+  if (commandId === 'createCard') {
+    // activity.value.commandContext == "compose"
+    card = createCard(activity.value.data);
+  } else if (commandId === 'getMessageDetails' && activity.value.messagePayload) {
+    // activity.value.commandContext == "message"
+    card = createMessageDetailsCard(activity.value.messagePayload);
+  } else {
+    throw new Error(`Unknown commandId: ${commandId}`);
+  }
+
+  return {
+    composeExtension: {
+      type: 'result',
+      attachmentLayout: 'list',
+      attachments: [cardAttachment('adaptive', card)],
+    },
+  };
+});
