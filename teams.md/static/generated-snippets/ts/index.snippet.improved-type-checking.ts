49ad199ef900149367bb750609abc93ae202b711
heyitsaamir <ajawaid191@gmail.com>
2025-10-09 14:03:57 -0700

    Remove generated snippets

--- teams.md/static/generated-snippets/ts/index.snippet.improved-type-checking.ts
+++
@@ -1,2 +0,0 @@
-// @ts-expect-error: "huge" is not a valid size for TextBlock
-const textBlock = new TextBlock('Valid', { size: 'huge' });
--- teams.md/static/generated-snippets/ts/index.snippet.proactive-messaging-send.ts
+++
@@ -1,8 +0,0 @@
-const sendProactiveNotification = async (userId: string) => {
-  const conversationId = myConversationIdStorage.get(userId);
-  if (!conversationId) {
-    return;
-  }
-  const activity = new MessageActivity('Hey! It\'s been a while. How are you?');
-  await app.send(conversationId, activity);
-};
